3ddd6bc571b8669df170644afb88543957ac0c9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:59 +0000

    Add package snowflake-cli-labs/0.1.7/snowflake_cli_labs-0.1.7.tar.gz

---
+++ packages/snowflake-cli-labs/snowflake_cli_labs-0.1.7.tar.gz/snowflake_cli_labs-0.1.7/src/templates/default_function/app.py
@@ -0,0 +1,13 @@
+import sys
+
+
+def hello() -> str:
+    return 'Hello World!'
+
+
+# For local debugging. Be aware you may need to type-convert arguments if you add input parameters
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        print(hello(sys.argv[1:]))  # type: ignore
+    else:
+        print(hello()) # type: ignore
