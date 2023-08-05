6e4f00b59647f8e0fe1a7dd5ba5dd6c93e44d9a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:12:43 +0000

    Add package jupyter-scheduler/1.1.0/jupyter_scheduler-1.1.0.tar.gz

---
+++ packages/jupyter-scheduler/jupyter_scheduler-1.1.0.tar.gz/jupyter_scheduler-1.1.0/src/components/job-definition-row.tsx
@@ -0,0 +1,140 @@
+import React from 'react';
+
+import cronstrue from 'cronstrue';
+
+import { JupyterFrontEnd } from '@jupyterlab/application';
+import { PathExt } from '@jupyterlab/coreutils';
+
+import PauseIcon from '@mui/icons-material/Pause';
+import PlayArrowIcon from '@mui/icons-material/PlayArrow';
+import { IconButton } from '@mui/material';
+import Stack from '@mui/material/Stack';
+import TableRow from '@mui/material/TableRow';
+import TableCell from '@mui/material/TableCell';
+
+import { Scheduler, SchedulerService } from '../handler';
+import { useTranslator } from '../hooks';
+import { TranslationBundle } from '@jupyterlab/translation';
+import { ConfirmDeleteButton } from './confirm-buttons';
+
+function CreatedAt(props: {
+  job: Scheduler.IDescribeJobDefinition;
+}): JSX.Element | null {
+  const create_date: Date | null = props.job.create_time
+    ? new Date(props.job.create_time)
+    : null;
+  const create_display_date: string | null = create_date
+    ? create_date.toLocaleString()
+    : null;
+
+  return <>{create_display_date}</>;
+}
+
+function ScheduleSummary(props: {
+  schedule: string | undefined;
+}): JSX.Element | null {
+  if (props.schedule === undefined) {
+    return null;
+  }
+
+  return <>{cronstrue.toString(props.schedule)}</>;
+}
+
+function PauseButton(props: {
+  jobDef: Scheduler.IDescribeJobDefinition;
+  clickHandler: () => void;
+}): JSX.Element | null {
+  const trans = useTranslator('jupyterlab');
+
+  if (!props.jobDef.active) {
+    return null;
+  }
+
+  const buttonTitle = props.jobDef.name
+    ? trans.__('Pause "%1"', props.jobDef.name)
+    : trans.__('Pause job definition');
+
+  return (
+    <IconButton onClick={props.clickHandler} title={buttonTitle}>
+      <PauseIcon fontSize="small" />
+    </IconButton>
+  );
+}
+
+function ResumeButton(props: {
+  jobDef: Scheduler.IDescribeJobDefinition;
+  clickHandler: () => void;
+}): JSX.Element | null {
+  const trans = useTranslator('jupyterlab');
+
+  if (props.jobDef.active) {
+    return null;
+  }
+
+  const buttonTitle = props.jobDef.name
+    ? trans.__('Resume "%1"', props.jobDef.name)
+    : trans.__('Resume job definition');
+
+  return (
+    <IconButton onClick={props.clickHandler} title={buttonTitle}>
+      <PlayArrowIcon fontSize="small" />
+    </IconButton>
+  );
+}
+
+export function buildJobDefinitionRow(
+  jobDef: Scheduler.IDescribeJobDefinition,
+  app: JupyterFrontEnd,
+  openJobDefinitionDetail: (jobDefId: string) => unknown,
+  deleteRow: (
+    id: Scheduler.IDescribeJobDefinition['job_definition_id']
+  ) => void,
+  forceReload: () => void,
+  trans: TranslationBundle,
+  ss: SchedulerService
+): JSX.Element {
+  const cellContents: React.ReactNode[] = [
+    // name
+    <a onClick={() => openJobDefinitionDetail(jobDef.job_definition_id)}>
+      {jobDef.name}
+    </a>,
+    PathExt.basename(jobDef.input_filename),
+    <CreatedAt job={jobDef} />,
+    <ScheduleSummary schedule={jobDef.schedule} />,
+    jobDef.active ? trans.__('Active') : trans.__('Paused'),
+    <Stack spacing={1} direction="row">
+      <PauseButton
+        jobDef={jobDef}
+        clickHandler={async () => {
+          await ss.pauseJobDefinition(jobDef.job_definition_id);
+          forceReload();
+        }}
+      />
+      <ResumeButton
+        jobDef={jobDef}
+        clickHandler={async () => {
+          await ss.resumeJobDefinition(jobDef.job_definition_id);
+          forceReload();
+        }}
+      />
+      <ConfirmDeleteButton
+        name={jobDef.name}
+        clickHandler={async () => {
+          await ss.deleteJobDefinition(jobDef.job_definition_id);
+
+          deleteRow(jobDef.job_definition_id);
+        }}
+      />
+    </Stack>
+  ];
+
+  return (
+    <TableRow key={jobDef.job_definition_id}>
+      {cellContents.map((cellContent, idx) => (
+        <TableCell key={`${jobDef.job_definition_id}-${idx}`}>
+          {cellContent}
+        </TableCell>
+      ))}
+    </TableRow>
+  );
+}
---
+++ packages/jupyter-scheduler/jupyter_scheduler-1.1.0.tar.gz/jupyter_scheduler-1.1.0/src/notebook-jobs-panel.tsx
@@ -0,0 +1,171 @@
+import React from 'react';
+
+import { ThemeProvider } from '@mui/material/styles';
+
+import { JupyterFrontEnd } from '@jupyterlab/application';
+import { VDomRenderer } from '@jupyterlab/apputils';
+import { ITranslator } from '@jupyterlab/translation';
+import { LabIcon } from '@jupyterlab/ui-components';
+
+import { calendarMonthIcon } from './components/icons';
+import TranslatorContext from './context';
+import { CreateJob } from './mainviews/create-job';
+import { NotebookJobsList } from './mainviews/list-jobs';
+import {
+  defaultScheduleFields,
+  ICreateJobModel,
+  IJobDefinitionModel,
+  JobsModel,
+  JobsView
+} from './model';
+import { getJupyterLabTheme } from './theme-provider';
+import { Scheduler } from './tokens';
+import { DetailView } from './mainviews/detail-view';
+import { CreateJobFromDefinition } from './mainviews/create-job-from-definition';
+import { EditJobDefinition } from './mainviews/edit-job-definition';
+
+export class NotebookJobsPanel extends VDomRenderer<JobsModel> {
+  readonly _title?: string;
+  readonly _description?: string;
+  readonly _app: JupyterFrontEnd;
+  readonly _translator: ITranslator;
+  readonly _advancedOptions: React.FunctionComponent<Scheduler.IAdvancedOptionsProps>;
+
+  constructor(options: NotebookJobsPanel.IOptions) {
+    super(
+      options.model ||
+        new JobsModel({
+          onModelUpdate: () => {
+            // allow us to invoke private parent method
+            // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+            // @ts-ignore
+            this.renderDOM();
+          }
+        })
+    );
+    this.addClass('jp-notebook-jobs-panel');
+    const trans = options.translator.load('jupyterlab');
+
+    this.title.icon = options.titleIcon ?? calendarMonthIcon;
+    this.title.caption = options.title ?? trans.__('Notebook Jobs');
+    this._description = options.description ?? trans.__('Job Runs');
+    this._app = options.app;
+    this._translator = options.translator;
+    this._advancedOptions = options.advancedOptions;
+
+    this.node.setAttribute('role', 'region');
+    this.node.setAttribute('aria-label', trans.__('Notebook Jobs'));
+  }
+
+  showListView(view: JobsView.ListJobs | JobsView.ListJobDefinitions): void {
+    this.model.jobsView = view;
+  }
+
+  showDetailView(jobId: string): void {
+    this.model.jobsView = JobsView.JobDetail;
+    this.model.jobDetailModel.id = jobId;
+  }
+
+  showJobDefinitionDetail(jobDefId: string): void {
+    this.model.jobsView = JobsView.JobDefinitionDetail;
+    this.model.jobDetailModel.id = jobDefId;
+  }
+
+  editJobDefinition(jobDef: IJobDefinitionModel): void {
+    this.model.jobsView = JobsView.EditJobDefinition;
+    this.model.updateJobDefinitionModel = {
+      definitionId: jobDef.definitionId,
+      name: jobDef.name,
+      ...defaultScheduleFields,
+      // TODO: should these properties really be optional?
+      schedule: jobDef.schedule || '* * * * *',
+      timezone: jobDef.timezone || 'UTC',
+      scheduleInterval: 'custom'
+    };
+  }
+
+  render(): JSX.Element {
+    const showCreateJob = (newModel: ICreateJobModel) => {
+      this.model.createJobModel = newModel;
+      this.model.jobsView = JobsView.CreateForm;
+    };
+
+    return (
+      <ThemeProvider theme={getJupyterLabTheme()}>
+        <TranslatorContext.Provider value={this._translator}>
+          {this.model.jobsView === JobsView.CreateForm && (
+            <CreateJob
+              key={this.model.createJobModel.key}
+              model={this.model.createJobModel}
+              handleModelChange={newModel =>
+                (this.model.createJobModel = newModel)
+              }
+              showListView={this.showListView.bind(this)}
+              advancedOptions={this._advancedOptions}
+            />
+          )}
+          {this.model.jobsView === JobsView.CreateFromJobDescriptionForm && (
+            <CreateJobFromDefinition
+              key={this.model.createJobModel.key}
+              model={this.model.createJobModel}
+              handleModelChange={newModel =>
+                (this.model.createJobModel = newModel)
+              }
+              showListView={this.showListView.bind(this)}
+              advancedOptions={this._advancedOptions}
+            />
+          )}
+          {(this.model.jobsView === JobsView.ListJobs ||
+            this.model.jobsView === JobsView.ListJobDefinitions) && (
+            <NotebookJobsList
+              app={this._app}
+              listView={this.model.jobsView}
+              showListView={this.showListView.bind(this)}
+              showCreateJob={showCreateJob}
+              showJobDetail={this.showDetailView.bind(this)}
+              showJobDefinitionDetail={this.showJobDefinitionDetail.bind(this)}
+            />
+          )}
+          {(this.model.jobsView === JobsView.JobDetail ||
+            this.model.jobsView === JobsView.JobDefinitionDetail) && (
+            <DetailView
+              app={this._app}
+              model={this.model.jobDetailModel}
+              setCreateJobModel={newModel =>
+                (this.model.createJobModel = newModel)
+              }
+              jobsView={this.model.jobsView}
+              setJobsView={view => (this.model.jobsView = view)}
+              showCreateJob={showCreateJob}
+              showJobDetail={this.showDetailView.bind(this)}
+              editJobDefinition={this.editJobDefinition.bind(this)}
+              advancedOptions={this._advancedOptions}
+            />
+          )}
+          {this.model.jobsView === JobsView.EditJobDefinition && (
+            <EditJobDefinition
+              model={this.model.updateJobDefinitionModel}
+              handleModelChange={newModel =>
+                (this.model.updateJobDefinitionModel = newModel)
+              }
+              showListView={this.showListView.bind(this)}
+              showJobDefinitionDetail={this.showJobDefinitionDetail.bind(this)}
+            />
+          )}
+        </TranslatorContext.Provider>
+      </ThemeProvider>
+    );
+  }
+}
+
+namespace NotebookJobsPanel {
+  export interface IOptions {
+    title?: string;
+    titleIcon?: LabIcon;
+    description?: string;
+    app: JupyterFrontEnd;
+    translator: ITranslator;
+    advancedOptions: Scheduler.IAdvancedOptions;
+    model?: JobsModel;
+  }
+}
