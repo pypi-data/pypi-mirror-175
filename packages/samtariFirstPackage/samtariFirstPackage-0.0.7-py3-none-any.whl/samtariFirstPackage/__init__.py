83ae4f9b0b51eed686c06d0ebe26ab922a9d6bd8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:01:50 +0000

    Add package samtariFirstPackage/0.0.7/samtariFirstPackage-0.0.7-py3-none-any.whl

---
+++ packages/samtariFirstPackage/samtariFirstPackage-0.0.7-py3-none-any.whl/samtariFirstPackage/__init__.py
@@ -0,0 +1,8 @@
+# from add import add
+# from subtract import subtract
+
+def add(x, y):
+    return x+y
+
+def sub(x, y):
+    return x-y
---
+++ packages/samtariFirstPackage/samtariFirstPackage-0.0.7-py3-none-any.whl/samtariFirstPackage/extras/__init__.py
@@ -0,0 +1,8 @@
+# import multiply
+# import divide
+
+def divide(x, y):
+    return x/y
+    
+def multiply(x, y):
+    return x*y
