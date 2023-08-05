9792104506bff63a8a0779d41fafbed9a39140c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:02:21 +0000

    Add package opensarlab-controlbtn/1.0.0/opensarlab_controlbtn-1.0.0.tar.gz

---
+++ packages/opensarlab-controlbtn/opensarlab_controlbtn-1.0.0.tar.gz/opensarlab_controlbtn-1.0.0/src/index.ts
@@ -0,0 +1,29 @@
+import {
+  JupyterFrontEnd,
+  JupyterFrontEndPlugin
+} from '@jupyterlab/application';
+
+import { ITopBar } from 'jupyterlab-topbar';
+
+import { ToolbarButton } from '@jupyterlab/apputils';
+
+const plugin: JupyterFrontEndPlugin<void> = {
+  id: 'opensarlab-topbar-serverbtn',
+  autoStart: true,
+  requires: [ITopBar],
+  activate: (app: JupyterFrontEnd, topBar: ITopBar) => {
+    const serverBtn = new ToolbarButton({
+      className: 'hub-server-button',
+      label: 'Shutdown and Logout Page',
+      onClick: () => {
+        let base_url = '/lab' + window.location.pathname.split('/')[2];
+        window.location.href = base_url + '/hub/home';
+      },
+      tooltip: 'Hub Control Panel: A place to stop the server and logout'
+    });
+
+    topBar.addItem('server-btn', serverBtn);
+  }
+};
+
+export default plugin;
