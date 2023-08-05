e75de88c3a90c4644eb00fef6493710ac9142786
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:03 +0000

    Add package queueplus/0.5.0/queueplus-0.5.0.tar.gz

---
+++ packages/queueplus/queueplus-0.5.0.tar.gz/queueplus-0.5.0/src/queueplus/__init__.py
@@ -0,0 +1,6 @@
+__version__ = '0.5.0'
+__author__ = 'Mitchell Lisle'
+__email__ = 'm.lisle90@gmail.com'
+
+from queueplus.aioqueue import AioQueue, DataT, TypedAioQueue
+from queueplus.violations import DiscardOnViolation, RaiseOnViolation, ViolationStrategy
