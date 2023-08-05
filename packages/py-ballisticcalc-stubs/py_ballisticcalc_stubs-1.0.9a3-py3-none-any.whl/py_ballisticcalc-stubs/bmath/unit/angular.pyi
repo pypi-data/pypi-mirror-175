e5ff3ba4119f8caeaba718bfc3f7d291a4d8a352
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:53 +0000

    Add package py-ballisticcalc-stubs/1.0.9a3/py_ballisticcalc_stubs-1.0.9a3-py3-none-any.whl

---
+++ packages/py-ballisticcalc-stubs/py_ballisticcalc_stubs-1.0.9a3-py3-none-any.whl/py_ballisticcalc-stubs/bmath/unit/angular.pyi
@@ -0,0 +1,70 @@
+# imports
+import builtins as __builtins__ # <module 'builtins' (built-in)>
+
+# Variables with simple values
+
+AngularCmPer100M = 7
+AngularDegree = 1
+AngularInchesPer100Yd = 6
+AngularMil = 3
+AngularMOA = 2
+AngularMRad = 4
+AngularRadian = 0
+AngularThousand = 5
+
+# functions
+
+def __pyx_unpickle_Angular(*args, **kwargs): # real signature unknown
+    pass
+
+# classes
+
+class Angular(object):
+    # no doc
+    def convert(self, *args, **kwargs): # real signature unknown
+        pass
+
+    def get_in(self, *args, **kwargs): # real signature unknown
+        pass
+
+    def units(self, *args, **kwargs): # real signature unknown
+        pass
+
+    def value(self, *args, **kwargs): # real signature unknown
+        pass
+
+    def __init__(self, *args, **kwargs): # real signature unknown
+        pass
+
+    @staticmethod # known case of __new__
+    def __new__(*args, **kwargs): # real signature unknown
+        """ Create and return a new object.  See help(type) for accurate signature. """
+        pass
+
+    def __reduce_cython__(self, *args, **kwargs): # real signature unknown
+        pass
+
+    def __reduce__(self, *args, **kwargs): # real signature unknown
+        pass
+
+    def __setstate_cython__(self, *args, **kwargs): # real signature unknown
+        pass
+
+    def __setstate__(self, *args, **kwargs): # real signature unknown
+        pass
+
+    def __str__(self, *args, **kwargs): # real signature unknown
+        """ Return str(self). """
+        pass
+
+    __pyx_vtable__ = None # (!) real value is '<capsule object NULL at 0x000001ABF86059B0>'
+
+
+# variables with complex values
+
+__loader__ = None # (!) real value is '<_frozen_importlib_external.ExtensionFileLoader object at 0x000001ABF6E11790>'
+
+__spec__ = None # (!) real value is "ModuleSpec(name='py_ballisticcalc.bmath.unit.angular', loader=<_frozen_importlib_external.ExtensionFileLoader object at 0x000001ABF6E11790>, origin='C:\\\\Users\\\\Sergey\\\\PycharmProjects\\\\archer_ballistics_constructor\\\\venv11\\\\Lib\\\\site-packages\\\\py_ballisticcalc\\\\bmath\\\\unit\\\\angular.cp311-win_amd64.pyd')"
+
+__test__ = {}
+
---
+++ packages/py-ballisticcalc-stubs/py_ballisticcalc_stubs-1.0.9a3-py3-none-any.whl/py_ballisticcalc-stubs/bmath/unit/distance.pyi
@@ -0,0 +1,72 @@
+# imports
+import builtins as __builtins__ # <module 'builtins' (built-in)>
+
+# Variables with simple values
+
+DistanceCentimeter = 16
+DistanceFoot = 11
+DistanceInch = 10
+DistanceKilometer = 18
+DistanceLine = 19
+DistanceMeter = 17
+DistanceMile = 13
+DistanceMillimeter = 15
+DistanceNauticalMile = 14
+DistanceYard = 12
+
+# functions
+
+def __pyx_unpickle_Distance(*args, **kwargs): # real signature unknown
+    pass
+
+# classes
+
+class Distance(object):
+    # no doc
+    def convert(self, *args, **kwargs): # real signature unknown
+        pass
+
+    def get_in(self, *args, **kwargs): # real signature unknown
+        pass
+
+    def units(self, *args, **kwargs): # real signature unknown
+        pass
+
+    def value(self, *args, **kwargs): # real signature unknown
+        pass
+
+    def __init__(self, *args, **kwargs): # real signature unknown
+        pass
+
+    @staticmethod # known case of __new__
+    def __new__(*args, **kwargs): # real signature unknown
+        """ Create and return a new object.  See help(type) for accurate signature. """
+        pass
+
+    def __reduce_cython__(self, *args, **kwargs): # real signature unknown
+        pass
+
+    def __reduce__(self, *args, **kwargs): # real signature unknown
+        pass
+
+    def __setstate_cython__(self, *args, **kwargs): # real signature unknown
+        pass
+
+    def __setstate__(self, *args, **kwargs): # real signature unknown
+        pass
+
+    def __str__(self, *args, **kwargs): # real signature unknown
+        """ Return str(self). """
+        pass
+
+    __pyx_vtable__ = None # (!) real value is '<capsule object NULL at 0x0000012BDDFF57A0>'
+
+
+# variables with complex values
+
+__loader__ = None # (!) real value is '<_frozen_importlib_external.ExtensionFileLoader object at 0x0000012BDC830A50>'
+
+__spec__ = None # (!) real value is "ModuleSpec(name='py_ballisticcalc.bmath.unit.distance', loader=<_frozen_importlib_external.ExtensionFileLoader object at 0x0000012BDC830A50>, origin='C:\\\\Users\\\\Sergey\\\\PycharmProjects\\\\archer_ballistics_constructor\\\\venv11\\\\Lib\\\\site-packages\\\\py_ballisticcalc\\\\bmath\\\\unit\\\\distance.cp311-win_amd64.pyd')"
+
+__test__ = {}
+
