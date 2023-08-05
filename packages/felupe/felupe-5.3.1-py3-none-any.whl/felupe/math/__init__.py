e9737c206acab1cdbedce6b03b015cd719902965
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:03:37 +0000

    Add package felupe/5.3.1/felupe-5.3.1-py3-none-any.whl

---
+++ packages/felupe/felupe-5.3.1-py3-none-any.whl/felupe/math/__init__.py
@@ -0,0 +1,36 @@
+from ._tensor import (
+    identity,
+    det,
+    inv,
+    dya,
+    cdya,
+    cdya_ik,
+    cdya_il,
+    dev,
+    cof,
+    eig,
+    eigh,
+    eigvals,
+    eigvalsh,
+    transpose,
+    majortranspose,
+    trace,
+    cross,
+    dot,
+    ddot,
+    tovoigt,
+    sym,
+)
+
+from ._field import (
+    defgrad,
+    strain,
+    extract,
+    values,
+    norm,
+    interpolate,
+    grad,
+)
+
+from ._spatial import rotation_matrix
+from ._math import linsteps
---
+++ packages/felupe/felupe-5.3.1-py3-none-any.whl/felupe/mechanics/__init__.py
@@ -0,0 +1,20 @@
+from ._solidbody import SolidBody
+from ._solidbody_incompressible import (
+    SolidBodyNearlyIncompressible,
+    StateNearlyIncompressible,
+)
+from ._solidbody_pressure import SolidBodyPressure
+from ._solidbody_tensor import SolidBodyTensor
+from ._solidbody_gravity import SolidBodyGravity
+from ._pointload import PointLoad
+from ._step import Step
+from ._job import Job
+from ._curve import CharacteristicCurve
+
+try:
+    from ._multipoint import (
+        MultiPointConstraint,
+        MultiPointContact,
+    )
+except:
+    pass
