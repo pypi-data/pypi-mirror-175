89496d75fae10f4cb8b76dc7af482724a2a6ce10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:10 +0000

    Add package morpion-game/0.6.0.post1/morpion_game-0.6.0.post1.tar.gz

---
+++ packages/morpion-game/morpion_game-0.6.0.post1.tar.gz/morpion_game-0.6.0.post1/setup.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['morpion-game']
+
+package_data = \
+{'': ['*'],
+ 'morpion-game': ['dist/__main__/*',
+                  'dist/__main__/certifi/*',
+                  'dist/__main__/lib-dynload/*']}
+
+install_requires = \
+['art>=5.2,<6.0', 'requests>=2.26.0,<3.0.0', 'termcolor>=1.1.0,<2.0.0']
+
+setup_kwargs = {
+    'name': 'morpion-game',
+    'version': '0.6.0.post1',
+    'description': 'A minimal Morpion game to play in the console or with a graphical user interface',
+    'long_description': 'None',
+    'author': 'PetchouDev',
+    'author_email': 'petchou91d@gmail.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
