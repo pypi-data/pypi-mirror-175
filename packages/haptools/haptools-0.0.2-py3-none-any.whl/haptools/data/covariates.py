60230faa07ac95cd5e4b4a17381ed517a6273503
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:18 +0000

    Add package haptools/0.0.2/haptools-0.0.2-py3-none-any.whl

---
+++ packages/haptools/haptools-0.0.2-py3-none-any.whl/haptools/data/covariates.py
@@ -0,0 +1,38 @@
+from __future__ import annotations
+from csv import reader
+from pathlib import Path
+from collections import namedtuple
+from logging import getLogger, Logger
+from fileinput import hook_compressed
+
+import numpy as np
+
+from .data import Data
+from .phenotypes import Phenotypes
+
+
+class Covariates(Phenotypes):
+    """
+    A class for processing covariates from a file
+
+    Attributes
+    ----------
+    data : np.array
+        The covariates in an n (samples) x m (covariates) array
+    fname : Path | str
+        The path to the read-only file containing the data
+    samples : tuple[str]
+        The names of each of the n samples
+    names : tuple[str]
+        The names of the covariates
+    log: Logger
+        A logging instance for recording debug statements.
+
+    Examples
+    --------
+    >>> covariates = Covariates.load('tests/data/simple.covar')
+    """
+
+    def __init__(self, fname: Path | str, log: Logger = None):
+        super(Phenotypes, self).__init__(fname, log)
+        self._ext = "covar"
---
+++ packages/haptools/haptools-0.0.2-py3-none-any.whl/haptools/data/data.py
@@ -0,0 +1,80 @@
+from __future__ import annotations
+from csv import reader
+from pathlib import Path
+from typing import Iterator
+from collections import namedtuple
+from abc import ABC, abstractmethod
+from logging import getLogger, Logger
+
+import numpy as np
+
+
+class Data(ABC):
+    """
+    Abstract class for accessing read-only data files
+
+    Attributes
+    ----------
+    fname : Path | str
+        The path to the read-only file containing the data
+    data : np.array
+        The contents of the data file, once loaded
+    log: Logger
+        A logging instance for recording debug statements.
+    """
+
+    def __init__(self, fname: Path | str, log: Logger = None):
+        if isinstance(fname, str):
+            fname = Path(fname)
+        self.fname = fname
+        self.data = None
+        self.log = log or getLogger(self.__class__.__name__)
+        super().__init__()
+
+    def __repr__(self):
+        return str(self.fname)
+
+    @classmethod
+    @abstractmethod
+    def load(cls: Data, fname: Path):
+        """
+        Read the file contents and perform any recommended pre-processing
+
+        Parameters
+        ----------
+        fname : Path
+            See documentation for :py:attr:`~.Data.fname`
+        """
+        pass
+
+    def unset(self) -> bool:
+        """
+        Whether the data has been loaded into the object yet
+
+        Returns
+        -------
+        bool
+            True if :py:attr:`~.Data.data` is None else False
+        """
+        return self.data is None
+
+    @abstractmethod
+    def read(self):
+        """
+        Read the raw file contents into the class properties
+        """
+        if not self.unset():
+            self.log.warning("The data has already been loaded. Overriding.")
+
+    @abstractmethod
+    def __iter__(self) -> Iterator[namedtuple]:
+        """
+        Return an iterator over the raw file contents
+
+        Yields
+        ------
+        Iterator[namedtuple]
+            An iterator over each line in the file, where each line is encoded as a
+            namedtuple containing each of the class properties
+        """
+        pass
