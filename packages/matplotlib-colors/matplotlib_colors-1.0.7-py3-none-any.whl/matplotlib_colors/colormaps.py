335f9f4b8494c924326353cf9c93d12e1c961f66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:00:24 +0000

    Add package matplotlib-colors/1.0.7/matplotlib_colors-1.0.7-py3-none-any.whl

---
+++ packages/matplotlib-colors/matplotlib_colors-1.0.7-py3-none-any.whl/matplotlib_colors/colormaps.py
@@ -0,0 +1,22 @@
+import matplotlib.pyplot as plt
+import matplotlib.colors as mcolors
+from matplotlib_colors.colors import colors
+
+
+# Colors maps
+wld = mcolors.ListedColormap([colors['PL_LOSE'], colors['PL_DRAW'], colors['PL_WIN']])
+premier_league = mcolors.ListedColormap([colors['PL_PINK'], colors['PL_BLUE'], colors['PL_GREEN']])
+team = mcolors.ListedColormap([colors['OPPOSITION'], colors['CONTESTED'], colors['TEAM']])
+analyst = mcolors.ListedColormap([colors['OPPOSITION'], colors['CONTESTED'], colors['TEAM']])
+
+
+excluded_vars = {'colormaps', 'colors', 'plt', 'mcolors', 'excluded_vars', 'var', 'register_cmaps'}
+colormaps = {var: eval(var) for var in dir() if not var.startswith('__') and var not in excluded_vars}
+colormap_names = list(colormaps.keys())
+
+def register_cmaps():
+    for var, val in globals().items():
+        if not var.startswith('__') and var not in excluded_vars:
+            val.name = var
+            plt.colormaps.register(val)
+    print(plt.colormaps)
