f8bd48127200e9d354c864cbfa4973fe4b221a64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:03:32 +0000

    Add package sleepyemoji/0.1.7/sleepyemoji-0.1.7-py3-none-any.whl

---
+++ packages/sleepyemoji/sleepyemoji-0.1.7-py3-none-any.whl/toolchain/hands.py
@@ -0,0 +1,11 @@
+handsDict = { 
+# Emoji    Discord             ios
+  '👌' : [':ok_hand:',     'ok hand sign'],
+  '👍' : [':thumbsup:',    'thumbs up'],
+  '✌ ' : [':v:',           'peace sign hand'],
+  '👎' : [':thumbsdown:',  'thumbs down'],
+  '👏' : [':clap:',        'clapping hands'],
+  '👈' : [':point_left:',  '?'],
+  '👉' : [':point_right:', '?'],
+}
+# '' : ['::', ''],
