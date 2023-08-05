f5e38954678bb69f7d443acf3bead727008d311a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:14:41 +0000

    Add package pypony/1.0.0b2/pypony-1.0.0b2.tar.gz

---
+++ packages/pypony/pypony-1.0.0b2.tar.gz/pypony-1.0.0b2/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+with open("requirements.txt", "r") as fp:
+    requirements = fp.readlines()
+
+setup(
+    name='pypony',
+    description='A python utility for contract testing APIs',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='Bandwidth',
+    author_email='letstalk@bandwidth.com',
+    url='https://github.com/Bandwidth/pypony/',
+    version='1.0.0b2',
+    py_modules=['pypony', 'src'],
+    install_requires=requirements,
+    packages=find_packages(exclude=["test"]),
+    include_package_data=True,
+    entry_points='''
+        [console_scripts]
+        pypony=pypony:cli
+    '''
+)
