a6edb8a8625c81651ca1d8a62ee54d1eeb9bc2e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:55 +0000

    Add package om-simple-encoder/0.0.1/om_simple_encoder-0.0.1.tar.gz

---
+++ packages/om-simple-encoder/om_simple_encoder-0.0.1.tar.gz/om_simple_encoder-0.0.1/setup.py
@@ -0,0 +1,34 @@
+import setuptools
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="om_simple_encoder",
+    packages = find_packages(),
+    package_data={'om_simple_encoder': ['*.jpg']},
+    include_package_data=True,
+    version="0.0.1",
+    author="kyusonglee",
+    description="OM Simple Encoder",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://www.soco.ai",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: Free for non-commercial use",
+        "Operating System :: OS Independent",
+    ],
+    install_requires = [
+        "matplotlib",
+        "opencv-python-headless<4.3",
+        "Image",
+        "torch",
+        "pytorch-lightning",
+        "timm",
+        "regex",
+        "ftfy",
+        "tqdm"
+    ]
+)
