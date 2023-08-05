00e09a1914937abcc7219091b783fa07a55f63a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:59:40 +0000

    Add package matplotlib-colors/1.0.3/matplotlib-colors-1.0.3.tar.gz

---
+++ packages/matplotlib-colors/matplotlib-colors-1.0.3.tar.gz/matplotlib-colors-1.0.3/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+long_description = open("README.md").read()
+required = ['matplotlib']
+
+setup(
+    name="matplotlib-colors",
+    version="1.0.3",
+    author="Tom Draper",
+    author_email="tomjdraper1@gmail.com",
+    license="MIT",
+    description="A collection of curated colors and colormaps for matplotlib.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/tom-draper/matplotlib-colors",
+    key_words="visualization color colormap colorset matplotlib",
+    install_requires=required,
+    packages=find_packages(where="matplotlib_colors"),
+    python_requires=">=3.6",
+)
