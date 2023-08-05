ffca23e7ee7414784f5e662117bd0e6bf7b989f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:41 +0000

    Add package pybehavior/0.0.14/pybehavior-0.0.14.tar.gz

---
+++ packages/pybehavior/pybehavior-0.0.14.tar.gz/pybehavior-0.0.14/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pybehavior',
+    version='0.0.14',
+    description='PyBehavior',
+    author='jup014',
+    author_email='jup014@ucsd.edu',
+    url='https://cwphs.ucsd.edu',
+    license='MIT',
+    py_modules=['pybehavior'],
+    python_requires='>=3',
+    install_requires=['numpy', 'pandas', 'pyncei'],
+    packages=['pybehavior']
+)
