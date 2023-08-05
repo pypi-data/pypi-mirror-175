a6ea3e16aeb3dec153a26288a35e39da70987592
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:00:24 +0000

    Add package PyPI-nester/1.0.1/PyPI-nester-1.0.1.tar.gz

---
+++ packages/PyPI-nester/PyPI-nester-1.0.1.tar.gz/PyPI-nester-1.0.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+        name='PyPI-nester',
+        version='1.0.1',
+        py_modules=['nester'],
+        author='SonaM',
+        author_email='sonalimetkar1@gmail.com',
+        description='Printing each items in list (nested or not nested) on new line',
+        long_description='This module also prints the nested items with required tab spaces',
+        url='',
+        install_requires=[],
+     )
