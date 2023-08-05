f208aa50b5c89772b94319547dafb9b1662807be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:54 +0000

    Add package qotto-auth-client/1.1.3/qotto_auth_client-1.1.3-py3-none-any.whl

---
+++ packages/qotto-auth-client/qotto_auth_client-1.1.3-py3-none-any.whl/qottoauth/api/__init__.py
@@ -0,0 +1,10 @@
+from qottoauth.api.base import QottoAuthApi, QottoAuthApiError
+from qottoauth.api.graphene import QottoAuthGrapheneApi
+from qottoauth.api.test import QottoAuthTestApi
+
+__all__ = [
+    'QottoAuthApi',
+    'QottoAuthApiError',
+    'QottoAuthTestApi',
+    'QottoAuthGrapheneApi',
+]
