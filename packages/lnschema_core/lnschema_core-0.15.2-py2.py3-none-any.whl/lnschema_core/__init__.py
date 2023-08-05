64643a9367863ae3e5adb16c951d4e878399d03e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:12:54 +0000

    Add package lnschema_core/0.15.2/lnschema_core-0.15.2-py2.py3-none-any.whl

---
+++ packages/lnschema_core/lnschema_core-0.15.2-py2.py3-none-any.whl/lnschema_core/__init__.py
@@ -0,0 +1,74 @@
+"""Data provenance & flow (`yvzi`).
+
+Import the package::
+
+   import lnschema_core
+
+Data objects & transformations:
+
+.. autosummary::
+   :toctree: .
+
+   DSet
+   DObject
+   DTransform
+   DTransformIn
+
+Users, storage locations, and usage statistics:
+
+.. autosummary::
+   :toctree: .
+
+   User
+   Storage
+   Usage
+
+Data transformations:
+
+.. autosummary::
+   :toctree: .
+
+   Jupynb
+   Pipeline
+   PipelineRun
+
+Project management:
+
+.. autosummary::
+   :toctree: .
+
+   Project
+
+Development tools:
+
+.. autosummary::
+   :toctree: .
+
+   dev
+
+"""
+# This is lnschema-module yvzi.
+_schema_id = "yvzi"
+_name = "core"
+_migration = "98da12fc80a8"
+__version__ = "0.15.2"
+
+from . import _core, dev
+from ._core import (
+    DObject,
+    DSet,
+    DSetDObject,
+    DTransform,
+    DTransformIn,
+    Jupynb,
+    Pipeline,
+    PipelineRun,
+    Project,
+    ProjectDSet,
+    Storage,
+    Usage,
+    User,
+)
+from .dev import id  # backward compat
+
+storage = Storage  # backward compat
