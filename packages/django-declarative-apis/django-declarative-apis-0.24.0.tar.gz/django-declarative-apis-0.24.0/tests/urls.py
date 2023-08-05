39af10ec1c4015b505b61b54fe546a8c72429112
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:33 +0000

    Add package django-declarative-apis/0.24.0/django-declarative-apis-0.24.0.tar.gz

---
+++ packages/django-declarative-apis/django-declarative-apis-0.24.0.tar.gz/django-declarative-apis-0.24.0/tests/urls.py
@@ -0,0 +1,27 @@
+#
+# Copyright (c) 2019, salesforce.com, inc.
+# All rights reserved.
+# SPDX-License-Identifier: BSD-3-Clause
+# For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
+#
+
+from django.urls import re_path
+
+from . import views
+
+from django_declarative_apis.adapters import resource_adapter
+
+UUID4_REGEX = r"[0-9a-fA-F]{8}-([0-9a-fA-F]{4}-){3}[0-9a-fA-F]{12}"
+
+urlpatterns = [
+    re_path(r"^simple$", resource_adapter(get=views.SimpleEndpointDefinition)),
+    re_path(r"^dict$", resource_adapter(get=views.DictEndpointDefinition)),
+    re_path(r"^dictfield$", resource_adapter(post=views.DictFieldEndpointDefinition)),
+    re_path(
+        r"^pydanticfield$", resource_adapter(post=views.PydanticFieldEndpointDefinition)
+    ),
+    re_path(
+        r"^nestedpydanticfield$",
+        resource_adapter(post=views.NestedPydanticFieldEndpointDefinition),
+    ),
+]
