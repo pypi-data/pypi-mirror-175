15c3050ab980fc0becab4190deb0b60027deee3e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:58:47 +0000

    Add package botboy/1.2.0/botboy-1.2.0.tar.gz

---
+++ packages/botboy/botboy-1.2.0.tar.gz/botboy-1.2.0/test.py
@@ -0,0 +1,34 @@
+"""Tests the core module"""
+
+from botboy import BotBoy
+
+bot = BotBoy('Adder', lambda x, y: x + y)
+
+def test_display_information():
+  global bot
+  bot.display_information()
+
+def test_execute():
+  global bot
+  bot.execute(1, 2)
+  bot.set_processing()
+  bot.execute(3, 4)
+  bot.set_processing()
+  bot.execute(5, 6)
+  bot.set_on_file()
+  bot.execute(7, 8)
+  bot.set_on_file()
+
+def test_pause():
+  global bot
+
+  bot.set_wait()
+  bot.execute(100, 10000)
+  bot.set_wait()
+  bot.execute(1, 2)
+  bot.execute(3, 4)
+
+if __name__ == '__main__':
+  test_display_information()
+  test_execute()
+  test_pause()
