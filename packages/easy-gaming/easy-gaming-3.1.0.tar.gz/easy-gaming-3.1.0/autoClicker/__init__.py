b11b5b56cff456ddc70fcbc62d167f303b5fad33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:03:08 +0000

    Add package easy-gaming/3.1.0/easy-gaming-3.1.0.tar.gz

---
+++ packages/easy-gaming/easy-gaming-3.1.0.tar.gz/easy-gaming-3.1.0/autoClicker/__init__.py
@@ -0,0 +1,18 @@
+import mouse
+import time
+from pynput.keyboard import Controller
+
+keyboard = Controller()
+
+def click(button, speed: float = 1, duration: float = 10, delay: float = 0):
+    time.sleep(delay)
+    clicks = int(duration/speed)
+    if button == 'left' or button == 'right' or button == 'middle':
+        for i in range(0, clicks):
+            mouse.click(button)
+            time.sleep(speed)
+    else:
+        for i in range(0, clicks):
+            keyboard.press(button)
+            keyboard.release(button)
+            time.sleep(speed)
