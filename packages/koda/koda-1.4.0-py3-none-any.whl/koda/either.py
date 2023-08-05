c22e921ebe6ae840061792f8c0fe4c24f2eef953
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:58:59 +0000

    Add package koda/1.4.0/koda-1.4.0-py3-none-any.whl

---
+++ packages/koda/koda-1.4.0-py3-none-any.whl/koda/either.py
@@ -0,0 +1,154 @@
+from typing import Any, Callable, Generic, Union
+
+from koda._generics import A, B, C, D, E
+
+
+class First(Generic[A]):
+    __slots__ = ("val",)
+    __match_args__ = ("val",)
+
+    def __init__(self, val: A) -> None:
+        self.val: A = val
+
+    def __eq__(self, other: Any) -> bool:
+        return isinstance(other, First) and other.val == self.val
+
+    def __repr__(self) -> str:
+        return f"First({repr(self.val)})"
+
+    def map_first(self, fn: Callable[[A], B]) -> "First[B]":
+        return First(fn(self.val))
+
+    def map_second(self, fn: Callable[[Any], Any]) -> "First[A]":
+        return self
+
+    def map_third(self, fn: Callable[[Any], Any]) -> "First[A]":
+        return self
+
+    def map_fourth(self, fn: Callable[[Any], Any]) -> "First[A]":
+        return self
+
+    def map_fifth(self, fn: Callable[[Any], Any]) -> "First[A]":
+        return self
+
+
+class Second(Generic[A]):
+    __slots__ = ("val",)
+    __match_args__ = ("val",)
+
+    def __init__(self, val: A) -> None:
+        self.val: A = val
+
+    def __eq__(self, other: Any) -> bool:
+        return isinstance(other, Second) and other.val == self.val
+
+    def __repr__(self) -> str:
+        return f"Second({repr(self.val)})"
+
+    def map_first(self, fn: Callable[[Any], Any]) -> "Second[A]":
+        return self
+
+    def map_second(self, fn: Callable[[A], B]) -> "Second[B]":
+        return Second(fn(self.val))
+
+    def map_third(self, fn: Callable[[Any], Any]) -> "Second[A]":
+        return self
+
+    def map_fourth(self, fn: Callable[[Any], Any]) -> "Second[A]":
+        return self
+
+    def map_fifth(self, fn: Callable[[Any], Any]) -> "Second[A]":
+        return self
+
+
+class Third(Generic[A]):
+    __slots__ = ("val",)
+    __match_args__ = ("val",)
+
+    def __init__(self, val: A) -> None:
+        self.val: A = val
+
+    def __eq__(self, other: Any) -> bool:
+        return isinstance(other, Third) and other.val == self.val
+
+    def __repr__(self) -> str:
+        return f"Third({repr(self.val)})"
+
+    def map_first(self, fn: Callable[[Any], Any]) -> "Third[A]":
+        return self
+
+    def map_second(self, fn: Callable[[Any], Any]) -> "Third[A]":
+        return self
+
+    def map_third(self, fn: Callable[[A], B]) -> "Third[B]":
+        return Third(fn(self.val))
+
+    def map_fourth(self, fn: Callable[[Any], Any]) -> "Third[A]":
+        return self
+
+    def map_fifth(self, fn: Callable[[Any], Any]) -> "Third[A]":
+        return self
+
+
+class Fourth(Generic[A]):
+    __slots__ = ("val",)
+    __match_args__ = ("val",)
+
+    def __init__(self, val: A) -> None:
+        self.val: A = val
+
+    def __eq__(self, other: Any) -> bool:
+        return isinstance(other, Fourth) and other.val == self.val
+
+    def __repr__(self) -> str:
+        return f"Fourth({repr(self.val)})"
+
+    def map_first(self, fn: Callable[[Any], Any]) -> "Fourth[A]":
+        return self
+
+    def map_second(self, fn: Callable[[Any], Any]) -> "Fourth[A]":
+        return self
+
+    def map_third(self, fn: Callable[[Any], Any]) -> "Fourth[A]":
+        return self
+
+    def map_fourth(self, fn: Callable[[A], B]) -> "Fourth[B]":
+        return Fourth(fn(self.val))
+
+    def map_fifth(self, fn: Callable[[Any], Any]) -> "Fourth[A]":
+        return self
+
+
+class Fifth(Generic[A]):
+    __slots__ = ("val",)
+    __match_args__ = ("val",)
+
+    def __init__(self, val: A) -> None:
+        self.val: A = val
+
+    def __eq__(self, other: Any) -> bool:
+        return isinstance(other, Fifth) and other.val == self.val
+
+    def __repr__(self) -> str:
+        return f"Fifth({repr(self.val)})"
+
+    def map_first(self, fn: Callable[[Any], Any]) -> "Fifth[A]":
+        return self
+
+    def map_second(self, fn: Callable[[Any], Any]) -> "Fifth[A]":
+        return self
+
+    def map_third(self, fn: Callable[[Any], Any]) -> "Fifth[A]":
+        return self
+
+    def map_fourth(self, fn: Callable[[Any], Any]) -> "Fifth[A]":
+        return self
+
+    def map_fifth(self, fn: Callable[[A], B]) -> "Fifth[B]":
+        return Fifth(fn(self.val))
+
+
+Either = Union[First[A], Second[B]]
+Either3 = Union[First[A], Second[B], Third[C]]
+Either4 = Union[First[A], Second[B], Third[C], Fourth[D]]
+Either5 = Union[First[A], Second[B], Third[C], Fourth[D], Fifth[E]]
---
+++ packages/koda/koda-1.4.0-py3-none-any.whl/koda/maybe.py
@@ -0,0 +1,108 @@
+from typing import (
+    TYPE_CHECKING,
+    Any,
+    Callable,
+    ClassVar,
+    Final,
+    Generic,
+    Literal,
+    Optional,
+    Union,
+)
+
+from koda._generics import A, B, FailT
+
+if TYPE_CHECKING:  # pragma: no cover
+    from koda.result import Result
+
+
+class Nothing:
+    # singleton
+    _instance: ClassVar[Optional["Nothing"]] = None
+
+    is_just: ClassVar[Literal[False]] = False
+
+    def __new__(cls) -> "Nothing":
+        """
+        Make `Nothing` a singleton, so we can do `is` checks if we want.
+        """
+        if cls._instance is None:
+            cls._instance = super(Nothing, cls).__new__(cls)
+        return cls._instance
+
+    def __eq__(self, other: Any) -> bool:
+        return other is self._instance
+
+    def __repr__(self) -> str:
+        return "Nothing()"
+
+    def get_or_else(self, fallback: A) -> A:
+        return fallback
+
+    def map(self, _: Callable[[Any], B]) -> "Maybe[B]":
+        return self
+
+    def flat_map(self, _: Callable[[Any], "Maybe[B]"]) -> "Maybe[B]":
+        return self
+
+    def apply(self, _: "Maybe[Callable[[Any], B]]") -> "Maybe[B]":
+        return self
+
+    @property
+    def to_optional(self) -> Optional[Any]:
+        """
+        Note that `Just[None]` will return None!
+        """
+        return None
+
+    def to_result(self, fail_obj: FailT) -> "Result[Any, FailT]":
+        from koda.result import Err
+
+        return Err(fail_obj)
+
+
+# just a pre-init-ed instance of nothing.
+nothing: Final[Nothing] = Nothing()
+
+
+class Just(Generic[A]):
+    __match_args__ = ("val",)
+    __slots__ = ("val",)
+
+    is_just: ClassVar[Literal[True]] = True
+
+    def __init__(self, val: A) -> None:
+        self.val: A = val
+
+    def __eq__(self, other: Any) -> bool:
+        return isinstance(other, Just) and other.val == self.val
+
+    def __repr__(self) -> str:
+        return f"Just({repr(self.val)})"
+
+    def get_or_else(self, _: Any) -> A:
+        return self.val
+
+    def map(self, fn: Callable[[A], B]) -> "Maybe[B]":
+        return Just(fn(self.val))
+
+    def flat_map(self, fn: Callable[[A], "Maybe[B]"]) -> "Maybe[B]":
+        return fn(self.val)
+
+    def apply(self, container: "Maybe[Callable[[A], B]]") -> "Maybe[B]":
+        if isinstance(container, Nothing):
+            return nothing
+        else:
+            return Just(container.val(self.val))
+
+    @property
+    def to_optional(self) -> Optional[A]:
+        return self.val
+
+    def to_result(self, fail_obj: FailT) -> "Result[A, FailT]":
+        from koda.result import Ok
+
+        return Ok(self.val)
+
+
+Maybe = Union[Just[A], Nothing]
