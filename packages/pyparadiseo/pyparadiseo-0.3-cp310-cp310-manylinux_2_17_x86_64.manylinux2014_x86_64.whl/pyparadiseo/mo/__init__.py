a8719faad35ffb37eb697ea411b1b534d5dc6e77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:59:35 +0000

    Add package pyparadiseo/0.3/pyparadiseo-0.3-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/pyparadiseo/pyparadiseo-0.3-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/pyparadiseo/mo/__init__.py
@@ -0,0 +1,9 @@
+#abstract base classes?
+
+
+# from .algo import *
+# from .comparator import *
+# from .continuator import *
+# from .eval import *
+# from .explorer import *
+# from .neighborhood import *
