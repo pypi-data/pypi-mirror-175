75c2a3236c8e4b2255c6dd29f1af879a16087797
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:14:24 +0000

    Add package django-jira/0.2.6/django-jira-0.2.6.tar.gz

---
+++ packages/django-jira/django-jira-0.2.6.tar.gz/django-jira-0.2.6/src/django_jira/__init__.py
@@ -0,0 +1,13 @@
+from .tasks import tasks
+
+try:
+    from importlib.metadata import version, PackageNotFoundError  # type: ignore
+except ImportError:  # pragma: no cover
+    from importlib_metadata import version, PackageNotFoundError  # type: ignore
+try:
+    __version__ = version(__name__)
+except PackageNotFoundError:  # pragma: no cover
+    __version__ = "unknown"
+
+
+__all__ = ["tasks"]
