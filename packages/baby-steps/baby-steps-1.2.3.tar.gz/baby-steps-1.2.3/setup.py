48d01f819f40d51a4f3cbc96d407ca227bc1dcff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:18 +0000

    Add package baby-steps/1.2.3/baby-steps-1.2.3.tar.gz

---
+++ packages/baby-steps/baby-steps-1.2.3.tar.gz/baby-steps-1.2.3/setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages, setup
+
+
+setup(
+    name="baby-steps",
+    version="1.2.3",
+    description="Readability Matters",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="Nikita Tsvetkov",
+    author_email="nikitanovosibirsk@yandex.com",
+    python_requires=">=3.7",
+    url="https://github.com/nikitanovosibirsk/baby-steps",
+    license="Apache-2.0",
+    packages=find_packages(exclude=("tests",)),
+    package_data={"baby_steps": ["py.typed"]},
+    install_requires=[],
+    tests_require=[],
+    classifiers=[
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+    ],
+)
