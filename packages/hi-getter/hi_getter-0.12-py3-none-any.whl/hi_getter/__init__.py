6de9f0e3c44de5bfbd083446075f03d8ea9a4a5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:05 +0000

    Add package hi-getter/0.12/hi_getter-0.12-py3-none-any.whl

---
+++ packages/hi-getter/hi_getter-0.12-py3-none-any.whl/hi_getter/__init__.py
@@ -0,0 +1,11 @@
+###################################################################################################
+#                              MIT Licence (C) 2022 Cubicpath@Github                              #
+###################################################################################################
+"""A simple way to get live Halo data straight from Halo Waypoint."""
+# TODO: Add logging functionality
+# TODO: Redo Events implementation
+from ._version import __version__
+from ._version import __version_info__
+
+__author__ = 'Cubicpath@Github <cubicpath@pm.me>'
+"""Author's information."""
