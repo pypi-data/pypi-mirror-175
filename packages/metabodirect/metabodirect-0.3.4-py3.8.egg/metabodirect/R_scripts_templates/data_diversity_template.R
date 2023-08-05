b6453548fcd19e28611acc37771d20416e5727a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:05 +0000

    Add package metabodirect/0.3.4/metabodirect-0.3.4-py3.8.egg

---
+++ packages/metabodirect/metabodirect-0.3.4-py3.8.egg/metabodirect/R_scripts_templates/data_diversity_template.R
@@ -0,0 +1,21 @@
+# #############################################################
+# 
+# MetaboDirect
+# Data Exploration step
+# version 1.0
+# by Christian Ayala
+#     based on scripts and functions by Nathalia Graf Grachet
+# Licensed under the MIT license. See LICENSE.md file.
+# 
+# #############################################################
+
+suppressPackageStartupMessages({
+  library(tidyverse)
+  library(RColorBrewer)
+  library(ggnewscale)
+  library(ggpubr)
+  library(rstatix)
+  library(vegan)
+  library(picante)
+}) 
+
