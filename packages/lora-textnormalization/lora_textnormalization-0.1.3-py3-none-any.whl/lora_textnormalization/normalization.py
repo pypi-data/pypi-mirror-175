a77918323a9c3387775106bde64a0f6d92ae7f15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:15:40 +0000

    Add package lora-textnormalization/0.1.3/lora_textnormalization-0.1.3-py3-none-any.whl

---
+++ packages/lora-textnormalization/lora_textnormalization-0.1.3-py3-none-any.whl/lora_textnormalization/normalization.py
@@ -0,0 +1,32 @@
+import spacy
+
+nlp = spacy.load('en_core_web_sm')
+
+
+def spacy_process(text):
+    doc = nlp(text)
+
+    # Tokenization and lemmatization are done with the spacy nlp pipeline commands
+    lemma_list = []
+    for token in doc:
+        lemma_list.append(token.lemma_)
+
+    # Filter the stopword
+    filtered_sentence = []
+    for word in lemma_list:
+        lexeme = nlp.vocab[word]
+        if lexeme.is_stop == False:
+            filtered_sentence.append(word)
+
+    # Remove punctuation
+    punctuations = "?:!.,,;'()®_--$%^&*#@<>/\\"
+    for word in filtered_sentence:
+        if word in punctuations:
+            filtered_sentence.remove(word)
+
+    rm_special = []
+    for tk in filtered_sentence:
+        tk = tk.strip()
+        if tk != '"' and tk != '-' and len(tk) > 0 and not tk.isnumeric():
+            rm_special.append(tk)
+    return rm_special
