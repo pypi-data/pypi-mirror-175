98868cff27c4f6ac9ea2967d6f891f228ac8f9f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:28 +0000

    Add package csa-nemo-layer/0.0.1/csa_nemo_layer-0.0.1.tar.gz

---
+++ packages/csa-nemo-layer/csa_nemo_layer-0.0.1.tar.gz/csa_nemo_layer-0.0.1/csa_nemo_layer/classes.py
@@ -0,0 +1,21 @@
+class Finding:
+    def __init__(self, **kwargs) -> None:
+        """
+        Class for CSA Nemo project's findings
+        :params:
+        id: string
+        target: string
+        controlId: string
+        Reason: list of strings
+        Identifier: string
+        """
+        self.id = kwargs.get('id')
+        self.target = kwargs.get('target', '')
+        self.controlId = kwargs.get('controlId', '')
+        self.Reason = []
+        self.Identifier = kwargs.get('identifier', '')
+
+    def json(self) -> dict:
+        return self.__dict__
+
+# TO-DO: Add classes for targets
