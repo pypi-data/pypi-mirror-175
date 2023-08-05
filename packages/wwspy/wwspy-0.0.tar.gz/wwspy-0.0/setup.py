910362ac1630f76ea08ef7e72d7b8832aff28427
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:02:22 +0000

    Add package wwspy/0.0/wwspy-0.0.tar.gz

---
+++ packages/wwspy/wwspy-0.0.tar.gz/wwspy-0.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='wwspy',
+      version='0.0',
+      description='Work With Stroke!',
+      packages=['wwspy'],
+      author_email='mironrobloksi@gmail.com',
+      zip_safe=False)
