d907b3e59f42375efe5f0b70edd79388f2f0db76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:00:10 +0000

    Add package cg/22.87.0/cg-22.87.0.tar.gz

---
+++ packages/cg/cg-22.87.0.tar.gz/cg-22.87.0/tests/apps/loqus/conftest.py
@@ -0,0 +1,204 @@
+"""
+    conftest for loqusdb API
+"""
+
+import pytest
+from cg.apps.loqus import LoqusdbAPI
+from cg.models.cg_config import CGConfig, CommonAppConfig
+from tests.mocks.process_mock import ProcessMock
+
+LOQUSDB_OUTPUT = (
+    b"2018-11-29 08:41:38 130-229-8-20-dhcp.local "
+    b"mongo_adapter.client[77135] INFO Connecting to "
+    b"uri:mongodb://None:None@localhost:27017\n"
+    b"2018-11-29 08:41:38 130-229-8-20-dhcp.local "
+    b"mongo_adapter.client[77135] INFO Connection "
+    b"established\n2018-11-29 08:41:38 130-229-8-20-dhcp.local "
+    b"mongo_adapter.adapter[77135] INFO"
+    b" Use database loqusdb\n2018-11-29 08:41:38 130-229-8-20-dhcp.local "
+    b"loqusdb.utils.vcf[77135]"
+    b" INFO Check if vcf is on correct format...\n"
+    b"2018-11-29 08:41:38 130-229-8-20-dhcp.local"
+    b" loqusdb.utils.vcf[77135] INFO Vcf file "
+    b"/Users/mansmagnusson/Projects/loqusdb/tests/fixtures"
+    b"/test.vcf.gz looks fine\n2018-11-29 "
+    b"08:41:38 130-229-8-20-dhcp.local loqusdb.utils.vcf[77135]"
+    b" INFO Nr of variants in vcf: 15\n2018-11-29 "
+    b"08:41:38 130-229-8-20-dhcp.local loqusdb.utils."
+    b"vcf[77135] INFO Type of variants in vcf: snv\n"
+    b"Inserting variants\n2018-11-29 08:41:38 130-22"
+    b"9-8-20-dhcp.local loqusdb.utils.load[77135] "
+    b"INFO Inserted 15 variants of type snv\n2018-11-2"
+    b"9 08:41:38 130-229-8-20-dhcp.local loqusdb.commands.load[77135] "
+    b"INFO Nr variants inserted: "
+    b"15\n2018-11-29 08:41:38 130-229-8-20-dhcp.local "
+    b"loqusdb.commands.load[77135] INFO Time to "
+    b"insert variants: 0:00:00.012648\n2018-11-29 "
+    b"08:41:38 130-229-8-20-dhcp.local loqusdb.plugins."
+    b"mongo.adapter[77135] INFO All indexes exists\n"
+)
+
+
+# Loqusdb fixtures
+@pytest.fixture(name="loqus_config")
+def fixture_loqus_config():
+    """
+    loqusdb config fixture
+    """
+    return {
+        "loqusdb": {"config_path": "loqusdb_config_wes", "binary_path": "loqus_binary"},
+        "loqusdb_wes": {
+            "config_path": "loqusdb_config_wes",
+            "binary_path": "loqusdb_wes_binary",
+        },
+    }
+
+
+@pytest.fixture(name="loqus_config_object")
+def fixture_loqus_config_object(loqus_config: dict, cg_config_object: CGConfig):
+    cg_config_object.loqusdb = CommonAppConfig(**loqus_config["loqusdb"])
+    cg_config_object.loqusdb_wes = CommonAppConfig(**loqus_config["loqusdb-wes"])
+    return cg_config_object
+
+
+@pytest.fixture(scope="function")
+def loqus_binary_path(loqus_config):
+    """
+    loqusdb binary fixture
+    """
+
+    return loqus_config["loqusdb"]["binary_path"]
+
+
+@pytest.fixture(scope="function")
+def loqus_config_path(loqus_config):
+    """
+    loqusdb binary fixture
+    """
+
+    return loqus_config["loqusdb"]["config_path"]
+
+
+@pytest.fixture(scope="function")
+def loqus_process(loqus_binary_path: str, loqus_config_path: str):
+    """
+    Return mocked cg.utils.Process instance
+    """
+
+    return ProcessMock(binary=loqus_binary_path, config=loqus_config_path)
+
+
+@pytest.fixture(scope="function")
+def loqus_process_exception(loqus_binary_path, loqus_config_path):
+    """
+    Return mocked cg.utils.Process instance
+    """
+
+    return ProcessMock(binary=loqus_binary_path, config=loqus_config_path, error=True)
+
+
+@pytest.fixture(name="loqusdbapi")
+def fixture_loqusdbapi(loqus_config: dict, loqus_process) -> LoqusdbAPI:
+    """
+    loqusdb API fixture
+    """
+
+    _loqus_api = LoqusdbAPI(loqus_config)
+    _loqus_api.process = loqus_process
+
+    return _loqus_api
+
+
+@pytest.fixture(scope="function")
+def loqusdbapi_exception(loqus_config, loqus_process_exception):
+    """
+    loqusdb API fixture
+    """
+
+    _loqus_api = LoqusdbAPI(loqus_config)
+    _loqus_api.process = loqus_process_exception
+
+    return _loqus_api
+
+
+@pytest.fixture(name="loqusdb_output")
+def fixture_loqusdb_output() -> bytes:
+    """
+    loqusdb stderr for a successful load
+    """
+    return LOQUSDB_OUTPUT
+
+
+@pytest.fixture(name="loqusdb_case_output")
+def fixture_loqusdb_case_output() -> bytes:
+    """
+    loqusdb output for a 'loqusdb cases -c <case_id> --to-json' command
+    """
+    return (
+        b'[{"_id": "1234", "case_id": "yellowhog", '
+        b'"vcf_path": "test.vcf.gz", "vcf_sv_path": null, "nr_variants": 15, '
+        b'"nr_sv_variants": null, "profile_path": "test.vcf.gz", "individuals": '
+        b'[{"ind_id": "proband", "name": "proband", "case_id": "recessive_trio", '
+        b'"ind_index": 2, "sex": 1, "profile": ["CC", "TT", "TT", "CC", "TT", '
+        b'"AA", "CC", "GG", "GG", "GG", "TT", "GG", "CC", "AA", "GG", "GG", "GG", '
+        b'"AA", "CC", "CC", "CC", "GG", "CC", "CC", "TT", "CC", "GG", "GG", "TT", '
+        b'"AA", "AA", "GG", "CC", "AA", "AA", "AA", "AA", "GG", "GG", "TT", "GG", '
+        b'"AA", "TT", "AA", "GG", "GG", "CC", "AA", "TT", "TT"]}, {"ind_id": '
+        b'"mother", "name": "mother", "case_id": "recessive_trio", "ind_index": 1, '
+        b'"sex": 2, "profile": ["CC", "TT", "TT", "CC", "TT", "AA", "CC", "GG", '
+        b'"GG", "GG", "TT", "GG", "CC", "AA", "GG", "GG", "GG", "AA", "CC", "CC", '
+        b'"CC", "GG", "CC", "CC", "TT", "CC", "GG", "GG", "TT", "AA", "AA", "GG", '
+        b'"CC", "AA", "AA", "AA", "AA", "GG", "GG", "TT", "GG", "AA", "TT", "AA", '
+        b'"GG", "GG", "CC", "AA", "TT", "TT"]}, {"ind_id": "father", "name": '
+        b'"father", "case_id": "recessive_trio", "ind_index": 0, "sex": 1, '
+        b'"profile": ["CC", "TT", "TT", "CC", "TT", "AA", "CC", "GG", "GG", '
+        b'"GG", "TT", "GG", "CC", "AA", "GG", "GG", "GG", "AA", "CC", "CC", '
+        b'"CC", "GG", "CC", "CC", "TT", "CC", "GG", "GG", "TT", "AA", "AA", '
+        b'"GG", "CC", "AA", "AA", "AA", "AA", "GG", "GG", "TT", "GG", "AA", '
+        b'"TT", "AA", "GG", "GG", "CC", "AA", "TT", "TT"]}], "sv_individuals": '
+        b'[], "_inds": {"proband": {"ind_id": "proband", "name": "proband", '
+        b'"case_id": "recessive_trio", "ind_index": 2, "sex": 1}, "mother": '
+        b'{"ind_id": "mother", "name": "mother", "case_id": "recessive_trio", '
+        b'"ind_index": 1, "sex": 2}, "father": {"ind_id": "father", "name": '
+        b'"father", "case_id": "recessive_trio", "ind_index": 0, "sex": 1}}, '
+        b'"_sv_inds": {}}]\n'
+    )
+
+
+@pytest.fixture(name="loqusdb_duplicate_output")
+def fixture_loqusdb_duplicate_output() -> bytes:
+    """loqusdb output for a 'loqusdb profile --check-vcf' call"""
+    return (
+        b'{"ind_id": "proband", "name": "proband", "case_id": "recessive_trio_test", '
+        b'"ind_index": 4, "sex": 1, "profile": ["TT", "CC", "CC", "CC", "TT", "AA",'
+        b' "CC", "GG", "GG", "GG", "TT", "GG", "CC", "AA", "GG", "GG", "GG", "AA", '
+        b'"CC", "CC", "CC", "GG", "CC", "CC", "TT", "CC", "AA", "GA", "TC", "AG", "AC", '
+        b'"GT", "CC", "AC", "AG", "AG", "AA", "GA", "AA", "CC", "GG", "AA", "TT", "AA", '
+        b'"GG", "GG", "CC", "AA", "TT", "TT"]}'
+    )
+
+
+@pytest.fixture(name="loqusdb_delete_stderr")
+def fixture_loqusdb_delete_stderr() -> bytes:
+    """
+    Loqusdb STDERR for a successful delete.
+    """
+    return b"""2022-09-22 12:30:07 usernam loqusdb.commands.cli[20689] INFO Running loqusdb version 2.6.9
+2022-09-22 12:30:07 usernam mongo_adapter.client[20689] INFO Connecting to uri:mongodb://None:None@localhost:27017
+2022-09-22 12:30:07 usernam mongo_adapter.client[20689] INFO Connection established
+2022-09-22 12:30:07 usernam mongo_adapter.adapter[20689] INFO Use database loqusdb
+2022-09-22 12:30:07 usernam loqusdb.plugins.mongo.case[20689] INFO Removing case yellowhog from database
+2022-09-22 12:30:07 usernam loqusdb.utils.delete[20689] INFO deleting variants
+2022-09-22 12:30:07 usernam loqusdb.utils.delete[20689] INFO Start deleting chromosome 1"""
+
+
+@pytest.fixture(name="loqusdb_delete_nonexisting_stderr")
+def fixture_loqusdb_delete_nonexisting_stderr() -> bytes:
+    """
+    Loqusdb delete STDERR for non existing case.
+    """
+    return b"""2022-09-22 11:40:04 usernam loqusdb.commands.cli[19944] INFO Running loqusdb version 2.6.9
+2022-09-22 11:40:04 usernam mongo_adapter.client[19944] INFO Connecting to uri:mongodb://None:None@localhost:27017
+2022-09-22 11:40:04 usernam mongo_adapter.client[19944] INFO Connection established
+2022-09-22 11:40:04 usernam mongo_adapter.adapter[19944] INFO Use database loqusdb
+2022-09-22 11:40:04 usernam loqusdb.commands.delete[19944] WARNING Case yellowhog does not exist in database"""
---
+++ packages/cg/cg-22.87.0.tar.gz/cg-22.87.0/tests/cli/upload/test_cli_upload.py
@@ -0,0 +1,71 @@
+"""Test CG CLI upload module."""
+from datetime import datetime, timedelta
+
+from cg.cli.upload.base import upload
+from cg.constants.sequencing import SequencingMethod
+from cg.models.cg_config import CGConfig
+from cg.store import Store, models
+from click.testing import CliRunner
+
+from cg.store.api.link import LinkHelper
+from tests.store_helpers import StoreHelpers
+
+
+def test_all_samples_are_non_tumor(analysis_store: Store, case_id: str):
+    """Test that all samples are non tumor."""
+
+    case_obj = analysis_store.family(case_id)
+    assert LinkHelper.is_all_samples_non_tumour(case_obj.links)
+
+
+def test_all_samples_list_analyses(analysis_store: Store, case_id: str):
+    """Test that all samples have an analysis type."""
+
+    # GIVEN case obj where each sample is wgs analysis
+    case_obj = analysis_store.family(case_id)
+
+    # WHEN looking up the analysis type for the samples in the case
+    analysis_types = LinkHelper.get_analysis_type_for_each_link(case_obj.links)
+
+    # THEN all the samples should have analysis type 'wgs'
+    assert len(set(analysis_types)) == 1 and analysis_types[0] == SequencingMethod.WGS
+
+
+def test_upload_started_long_time_ago_raises_exception(
+    cli_runner: CliRunner,
+    base_context: CGConfig,
+    helpers: StoreHelpers,
+):
+    """Test that an upload for a missing case does fail hard."""
+
+    # GIVEN an analysis that is already uploading since a week ago
+    disk_store: Store = base_context.status_db
+    case = helpers.add_case(disk_store)
+    case_id = case.internal_id
+    today = datetime.now()
+    upload_started = today - timedelta(hours=100)
+    helpers.add_analysis(disk_store, case=case, uploading=True, upload_started=upload_started)
+
+    # WHEN trying to upload an analysis that was started a long time ago
+    result = cli_runner.invoke(upload, ["-f", case_id], obj=base_context)
+
+    # THEN an exception should have be thrown
+    assert result.exit_code != 0
+    assert result.exception
+
+
+def test_upload_force_restart(cli_runner: CliRunner, base_context: CGConfig, helpers: StoreHelpers):
+    """Test that a case that is already uploading can be force restarted."""
+
+    # GIVEN an analysis that is already uploading
+    disk_store: Store = base_context.status_db
+    case: models.Family = helpers.add_case(disk_store)
+    case_id: str = case.internal_id
+
+    helpers.add_analysis(disk_store, case=case, uploading=True)
+
+    # WHEN trying to upload it again with the force restart flag
+    result = cli_runner.invoke(upload, ["-f", case_id, "-r"], obj=base_context)
+
+    # THEN it tries to restart the upload
+    assert "already started" not in result.output
