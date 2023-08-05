41ff9c14887a399ea6ddae3558a84c43f2da16c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:13:17 +0000

    Add package simpleschema/0.3.0/simpleschema-0.3.0-py3-none-any.whl

---
+++ packages/simpleschema/simpleschema-0.3.0-py3-none-any.whl/simpleschema/__init__.py
@@ -0,0 +1,4 @@
+
+from simpleschema.validate import validateSchema as validate, validateItem
+from simpleschema.validate import is_valid, logger
+from simpleschema.helper_classes import ObjectSchema
