d9faedd4fa0ec66e860f851efa5b45919844fa85
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:16 +0000

    Add package astro-run-dag/0.1.4a0/astro_run_dag-0.1.4a0.tar.gz

---
+++ packages/astro-run-dag/astro_run_dag-0.1.4a0.tar.gz/astro_run_dag-0.1.4a0/README.md
@@ -0,0 +1,39 @@
+# Astro SQL CLI
+
+Empower analysts to build workflows to transform data using SQL.
+
+
+## Getting started
+
+### Install
+
+```bash
+    pip install astro-sql-cli
+```
+
+### Try it out
+
+```bash
+    flow version
+```
+
+
+## Development
+
+Setup your local environment:
+
+```bash
+    make setup
+```
+
+Try your own version of the SQL CLI locally:
+
+```bash
+    poetry run flow version
+```
+
+Run the tests:
+
+```bash
+    make test
+```
---
+++ packages/astro-run-dag/astro_run_dag-0.1.4a0.tar.gz/astro_run_dag-0.1.4a0/sql_cli/__main__.py
@@ -0,0 +1,252 @@
+import logging
+from pathlib import Path
+
+import typer
+from dotenv import load_dotenv
+from rich import print as rprint
+
+import sql_cli
+from sql_cli.constants import DEFAULT_AIRFLOW_HOME, DEFAULT_DAGS_FOLDER
+
+load_dotenv()
+app = typer.Typer(add_completion=False, context_settings={"help_option_names": ["-h", "--help"]})
+
+
+airflow_logger = logging.getLogger("airflow")
+airflow_logger.setLevel(logging.CRITICAL)
+airflow_logger.propagate = False
+
+
+@app.command(help="Print the SQL CLI version.")
+def version() -> None:
+    rprint("Astro SQL CLI", sql_cli.__version__)
+
+
+@app.command(help="Print additional information about the project.")
+def about() -> None:
+    rprint("Find out more: https://github.com/astronomer/astro-sdk/sql-cli")
+
+
+@app.command(help="Generate the Airflow DAG from a directory of SQL files.")
+def generate(
+    workflow_name: str = typer.Argument(
+        default=...,
+        show_default=False,
+        help="name of the workflow directory within workflows directory.",
+    ),
+    env: str = typer.Option(
+        default="default",
+        help="environment to run in",
+    ),
+    project_dir: Path = typer.Option(
+        None, dir_okay=True, metavar="PATH", help="(Optional) Default: current directory.", show_default=False
+    ),
+) -> None:
+    from sql_cli import cli
+    from sql_cli.project import Project
+
+    project_dir_absolute = project_dir.resolve() if project_dir else Path.cwd()
+    project = Project(project_dir_absolute)
+    project.load_config(env)
+
+    cli.generate_dag(project, env, workflow_name)
+
+
+@app.command(
+    help="""
+    Validate Airflow connection(s) provided in the configuration file for the given environment.
+    """
+)
+def validate(
+    project_dir: Path = typer.Argument(
+        None, dir_okay=True, metavar="PATH", help="(Optional) Default: current directory.", show_default=False
+    ),
+    env: str = typer.Option(
+        default="default",
+        help="(Optional) Environment used to declare the connections to be validated",
+    ),
+    connection: str = typer.Option(
+        default=None,
+        help="(Optional) Identifier of the connection to be validated. By default checks all the env connections.",
+    ),
+) -> None:
+    from sql_cli.connections import validate_connections
+    from sql_cli.project import Project
+    from sql_cli.utils.airflow import retrieve_airflow_database_conn_from_config, set_airflow_database_conn
+
+    project_dir_absolute = project_dir.resolve() if project_dir else Path.cwd()
+    project = Project(project_dir_absolute)
+    project.load_config(environment=env)
+
+    # Since we are using the Airflow ORM to interact with connections, we need to tell Airflow to use our airflow.db
+    # The usual route is to set $AIRFLOW_HOME before Airflow is imported. However, in the context of the SQL CLI, we
+    # decide this during runtime, depending on the project path and SQL CLI configuration.
+    airflow_meta_conn = retrieve_airflow_database_conn_from_config(project.directory / project.airflow_home)
+    set_airflow_database_conn(airflow_meta_conn)
+
+    rprint(f"Validating connection(s) for environment '{env}'")
+    validate_connections(connections=project.connections, connection_id=connection)
+
+
+@app.command(
+    help="""
+    Run a workflow locally. This task assumes that there is a local airflow DB (can be a SQLite file), that has been
+    initialized with Airflow tables.
+    """
+)
+def run(
+    workflow_name: str = typer.Argument(
+        default=...,
+        show_default=False,
+        help="name of the workflow directory within workflows directory.",
+    ),
+    env: str = typer.Option(
+        metavar="environment",
+        default="default",
+        help="environment to run in",
+    ),
+    project_dir: Path = typer.Option(
+        None, dir_okay=True, metavar="PATH", help="(Optional) Default: current directory.", show_default=False
+    ),
+    verbose: bool = typer.Option(False, help="Whether to show airflow logs", show_default=True),
+) -> None:
+    from sql_cli import cli
+    from sql_cli.project import Project
+    from sql_cli.utils.airflow import (
+        get_dag,
+        retrieve_airflow_database_conn_from_config,
+        set_airflow_database_conn,
+    )
+
+    project_dir_absolute = project_dir.resolve() if project_dir else Path.cwd()
+    project = Project(project_dir_absolute)
+    project.update_config(environment=env)
+    project.load_config(env)
+
+    # Since we are using the Airflow ORM to interact with connections, we need to tell Airflow to use our airflow.db
+    # The usual route is to set $AIRFLOW_HOME before Airflow is imported. However, in the context of the SQL CLI, we
+    # decide this during runtime, depending on the project path and SQL CLI configuration.
+    airflow_meta_conn = retrieve_airflow_database_conn_from_config(project.directory / project.airflow_home)
+    set_airflow_database_conn(airflow_meta_conn)
+
+    dag_file = cli.generate_dag(project, env, workflow_name)
+    dag = get_dag(dag_id=workflow_name, subdir=dag_file.parent.as_posix(), include_examples=False)
+    cli.run_dag(project, env, dag, verbose)
+
+
+@app.command(
+    help="""
+    Run a workflow locally. This task assumes that there is a local airflow DB (can be a SQLite file), that has been
+    initialized with Airflow tables.
+    """
+)
+def test_dag(
+    dag_file: str = typer.Argument(
+        default=...,
+        show_default=False,
+        help="path to DAG file or directory",
+    ),
+    dag_id: str = typer.Argument(
+        default=...,
+        show_default=False,
+        help="file where the dag is",
+    ),
+    connections_file_path: str = typer.Argument(
+        default=None,
+        show_default=False,
+        help="path to a file containing a list of connection objects",
+    ),
+    variables_file_path: str = typer.Argument(
+        default=None,
+        show_default=False,
+        help="path to a file containing a list of variable objects",
+    ),
+) -> None:
+    from sql_cli.utils.airflow import (
+        get_dag,
+    )
+    from sql_cli.run_dag import run_dag
+    from sql_cli.exceptions import ConnectionFailed
+    from typer import Exit
+    from airflow.models.dagbag import DagBag
+    from airflow.utils.cli import process_subdir
+
+    import_errors = DagBag(process_subdir(str(dag_file))).import_errors
+    if import_errors:
+        all_errors = "\n\n".join(list(import_errors.values()))
+        rprint(f"[bold red]Workflow failed to render[/bold red]\n errors found:\n\n {all_errors}")
+        raise Exit(code=1)
+    dag = get_dag(dag_id=dag_id, subdir=dag_file, include_examples=False)
+    try:
+        dr = run_dag(dag, conn_file_path=connections_file_path, variable_file_path=variables_file_path)
+    except ConnectionFailed as connection_failed:
+        rprint(
+            f"  [bold red]{connection_failed}[/bold red] using connection [bold]{connection_failed.conn_id}[/bold]"
+        )
+        raise Exit(code=1)
+    except Exception as exception:
+        rprint(f"  [bold red]{exception}[/bold red]")
+        raise Exit(code=1)
+    rprint(f"Completed running the workflow {dr.dag_id}. 🚀")
+    elapsed_seconds = (dr.end_date - dr.start_date).microseconds / 10**6
+    rprint(f"Total elapsed time: [bold blue]{elapsed_seconds:.2}s[/bold blue]")
+
+
+@app.command(
+    help="""
+    Initialise a project structure to write workflows using SQL files.
+
+    \b\n
+    Examples of usage:
+    \b\n
+    $ flow init
+    \b\n
+    $ flow init .
+    \b\n
+    $ flow init project_name
+
+
+    \b\n
+    By default, the project structure includes:
+
+    ├── config: withholds configuration, e.g. database connections, within each environment directory
+    \b\n
+    ├── data: directory which contains datasets, including SQLite databases used by the examples
+    \b\n
+    └── workflows: directory where SQL workflows are declared, by default has two examples of workflow
+
+    \b\n
+    Next steps:
+    \b\n
+    * Update the file `config/default/configuration.yaml` to declare database connections.
+    \b\n
+    * Create SQL workflows within the `workflows` folder.
+    """
+)
+def init(
+    project_dir: Path = typer.Argument(
+        None, dir_okay=True, metavar="PATH", help="(Optional) Default: current directory.", show_default=False
+    ),
+    airflow_home: Path = typer.Option(
+        None,
+        dir_okay=True,
+        help=f"(Optional) Set the Airflow Home. Default: {DEFAULT_AIRFLOW_HOME}",
+        show_default=False,
+    ),
+    airflow_dags_folder: Path = typer.Option(
+        None,
+        dir_okay=True,
+        help=f"(Optional) Set the DAGs Folder. Default: {DEFAULT_DAGS_FOLDER}",
+        show_default=False,
+    ),
+) -> None:
+    from sql_cli.project import Project
+
+    project_dir_absolute = project_dir.resolve() if project_dir else Path.cwd()
+    project = Project(project_dir_absolute, airflow_home, airflow_dags_folder)
+    project.initialise()
+    rprint("Initialized an Astro SQL project at", project.directory)
+
+
+if __name__ == "__main__":  # pragma: no cover
+    app()
