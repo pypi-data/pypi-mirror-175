d4ed3560ab2816a0e55f2ecc31764e121a02a99d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:59:11 +0000

    Add package libreria-correlaciones/0.10/libreria_correlaciones-0.10.tar.gz

---
+++ packages/libreria-correlaciones/libreria_correlaciones-0.10.tar.gz/libreria_correlaciones-0.10/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup
+from pathlib import Path
+directory = Path(__file__).parent
+long_description = (directory / "README.md").read_text()
+setup(
+  name = 'libreria_correlaciones',         # How you named your package folder (MyLib)
+  packages = ['libreria_correlaciones'],   # Chose the same as "name"
+  version = '0.10',      # Start with a small number and increase it with every change you make
+  license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
+  description = 'Esta librería sirve para la deteccion de correlaciones de cara a limpiar los datos para un modelo de clasificacion binaria.',   # Give a short description about your library
+  author = 'Paula',                   # Type in your name
+  author_email = 'paula.hermosilla@alumni.mondragon.edu',      # Type in your E-Mail
+  url = 'https://github.com/PaulaHermosilla/libreria_correlaciones',   # Provide either the link to your github or to your website
+  download_url = 'https://github.com/PaulaHermosilla/libreria_correlaciones/archive/refs/tags/v_10.tar.gz',    # I explain this later on
+  keywords = ['CORRELACIONES', 'CLASIFICACION_BINARIA', 'HISTOGRAMAS'],   # Keywords that define your package best
+  long_description=long_description,
+  long_description_content_type="text/markdown",
+  install_requires=[            # I get to this in a second
+          'pandas',
+          'seaborn',
+          'matplotlib',
+          'numpy',
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+    'Intended Audience :: Developers',      # Define that your audience are developers
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',   # Again, pick a license
+    'Programming Language :: Python :: 3.9',      #Specify which pyhton versions that you want to support
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
