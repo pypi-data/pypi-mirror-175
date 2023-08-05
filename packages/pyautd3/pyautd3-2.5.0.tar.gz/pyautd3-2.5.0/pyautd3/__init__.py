daeb7a67fd2cb18f1deacd4bb472ab86cf928107
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:05:57 +0000

    Add package pyautd3/2.5.0/pyautd3-2.5.0.tar.gz

---
+++ packages/pyautd3/pyautd3-2.5.0.tar.gz/pyautd3-2.5.0/pyautd3/__init__.py
@@ -0,0 +1,33 @@
+'''
+File: __init__.py
+Project: pyautd3
+Created Date: 24/05/2021
+Author: Shun Suzuki
+-----
+Last Modified: 01/11/2022
+Modified By: Shun Suzuki (suzuki@hapis.k.u-tokyo.ac.jp)
+-----
+Copyright (c) 2022 Shun Suzuki. All rights reserved.
+
+'''
+
+
+from pyautd3.autd import SilencerConfig
+from pyautd3.autd import Controller
+from pyautd3.autd import Amplitudes, ModDelayConfig
+from pyautd3.autd import NUM_TRANS_IN_UNIT, NUM_TRANS_X, NUM_TRANS_Y, TRANS_SPACING, DEVICE_HEIGHT, DEVICE_WIDTH
+
+__all__ = [
+    'SilencerConfig',
+    'Controller',
+    'Amplitudes',
+    'ModDelayConfig',
+    'NUM_TRANS_IN_UNIT',
+    'NUM_TRANS_X',
+    'NUM_TRANS_Y',
+    'TRANS_SPACING',
+    'DEVICE_WIDTH',
+    'DEVICE_HEIGHT'
+]
+
+__version__ = '2.5.0'
