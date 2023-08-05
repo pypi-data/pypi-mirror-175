b6f835fbc8edbe8b2032c01d6ab4bd37c02a8291
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:10 +0000

    Add package jmapc/0.2.5/jmapc-0.2.5-py3-none-any.whl

---
+++ packages/jmapc/jmapc-0.2.5-py3-none-any.whl/jmapc/models/__init__.py
@@ -0,0 +1,75 @@
+from .email import (
+    Email,
+    EmailBodyPart,
+    EmailBodyValue,
+    EmailHeader,
+    EmailQueryFilter,
+    EmailQueryFilterCondition,
+    EmailQueryFilterOperator,
+)
+from .email_submission import (
+    Address,
+    Delivered,
+    DeliveryStatus,
+    Displayed,
+    EmailSubmission,
+    EmailSubmissionQueryFilter,
+    EmailSubmissionQueryFilterCondition,
+    EmailSubmissionQueryFilterOperator,
+    Envelope,
+    UndoStatus,
+)
+from .event import Event, StateChange, TypeState
+from .identity import Identity
+from .mailbox import (
+    Mailbox,
+    MailboxQueryFilter,
+    MailboxQueryFilterCondition,
+    MailboxQueryFilterOperator,
+)
+from .models import (
+    AddedItem,
+    Comparator,
+    EmailAddress,
+    ListOrRef,
+    Operator,
+    SetError,
+    StrOrRef,
+)
+from .thread import Thread
+
+__all__ = [
+    "AddedItem",
+    "Address",
+    "Comparator",
+    "Delivered",
+    "DeliveryStatus",
+    "Displayed",
+    "Email",
+    "EmailAddress",
+    "EmailBodyPart",
+    "EmailBodyValue",
+    "EmailHeader",
+    "EmailQueryFilter",
+    "EmailQueryFilterCondition",
+    "EmailQueryFilterOperator",
+    "EmailSubmission",
+    "EmailSubmissionQueryFilter",
+    "EmailSubmissionQueryFilterCondition",
+    "EmailSubmissionQueryFilterOperator",
+    "Envelope",
+    "Event",
+    "Identity",
+    "ListOrRef",
+    "Mailbox",
+    "MailboxQueryFilter",
+    "MailboxQueryFilterCondition",
+    "MailboxQueryFilterOperator",
+    "Operator",
+    "SetError",
+    "StateChange",
+    "StrOrRef",
+    "Thread",
+    "TypeState",
+    "UndoStatus",
+]
