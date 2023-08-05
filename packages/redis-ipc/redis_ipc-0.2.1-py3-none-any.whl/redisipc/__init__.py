7df90b868690af0cf4046bf0527ad044644a6f51
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:39 +0000

    Add package redis-ipc/0.2.1/redis_ipc-0.2.1-py3-none-any.whl

---
+++ packages/redis-ipc/redis_ipc-0.2.1-py3-none-any.whl/redisipc/__init__.py
@@ -0,0 +1,35 @@
+"""
+Copyright (C) 2021-present  AXVin
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published
+by the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
+from typing import NamedTuple
+from redisipc.ipc import *
+
+__title__ = 'redis-ipc'
+__author__ = 'AXVin'
+__license__ = 'AGPL v3'
+__copyright__ = 'Copyright 2021-present AXVin'
+__version__ = "0.2.1"
+
+
+class VersionInfo(NamedTuple):
+    major: int
+    minor: int
+    micro: int
+    releaselevel: str
+    serial: int
+
+
+version_info = VersionInfo(major=0, minor=2, micro=1, releaselevel='final', serial=0)
