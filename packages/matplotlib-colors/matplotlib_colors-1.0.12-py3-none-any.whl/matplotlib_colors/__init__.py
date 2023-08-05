de06eb44ea4ee5f1ca3e30d1a503ff06b3f75c80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:01:32 +0000

    Add package matplotlib-colors/1.0.12/matplotlib_colors-1.0.12-py3-none-any.whl

---
+++ packages/matplotlib-colors/matplotlib_colors-1.0.12-py3-none-any.whl/matplotlib_colors/__init__.py
@@ -0,0 +1,2 @@
+from matplotlib_colors.colors import colors, color_names
+from matplotlib_colors.colormaps import colormaps, colormap_names, register_cmaps
---
+++ packages/matplotlib-colors/matplotlib_colors-1.0.12-py3-none-any.whl/matplotlib_colors/colormaps.py
@@ -0,0 +1,27 @@
+import matplotlib.pyplot as plt
+import matplotlib.colors as mcolors
+from matplotlib_colors.colors import colors
+
+
+# Colors maps
+wld = mcolors.ListedColormap([colors['PL_LOSE'], colors['PL_DRAW'], colors['PL_WIN']])
+premier_league = mcolors.ListedColormap([colors['PL_PINK'], colors['PL_BLUE'], colors['PL_GREEN']])
+team = mcolors.ListedColormap([colors['OPPOSITION'], colors['CONTESTED'], colors['TEAM']])
+analyst = mcolors.ListedColormap([colors['OPPOSITION'], colors['CONTESTED'], colors['TEAM']])
+
+
+excluded_vars = {'colormaps', 'colors', 'plt', 'mcolors', 'excluded_vars', 
+                 'colormap_names', 'color_names', 'var', 'cmap', 'register_cmaps'}
+
+colormaps = {}
+for var in dir():
+    if not var.startswith('__') and var not in excluded_vars:
+        cmap = eval(var)
+        cmap.name = var  # Assign variable name as colormap name
+        colormaps[var] = cmap
+colormap_names = list(colormaps.keys())
+
+def register_cmaps():
+    for var, val in globals().items():
+        if not var.startswith('__') and var not in excluded_vars:
+            plt.colormaps.register(val)
