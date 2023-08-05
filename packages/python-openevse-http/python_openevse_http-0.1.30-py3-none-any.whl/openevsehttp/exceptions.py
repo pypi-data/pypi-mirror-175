4d89b3e9187220cc04960d7db5e0d78f9dff755c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:12:41 +0000

    Add package python-openevse-http/0.1.30/python_openevse_http-0.1.30-py3-none-any.whl

---
+++ packages/python-openevse-http/python_openevse_http-0.1.30-py3-none-any.whl/openevsehttp/exceptions.py
@@ -0,0 +1,25 @@
+"""Exceptions."""
+
+
+class AuthenticationError(Exception):
+    """Exception for authentication errors."""
+
+
+class ParseJSONError(Exception):
+    """Exception for JSON parsing errors."""
+
+
+class UnknownError(Exception):
+    """Exception for Unknown errors."""
+
+
+class MissingMethod(Exception):
+    """Exception for missing method variable."""
+
+
+class AlreadyListening(Exception):
+    """Exception for already listening websocket."""
+
+
+class MissingSerial(Exception):
+    """Exception for missing serial number."""
