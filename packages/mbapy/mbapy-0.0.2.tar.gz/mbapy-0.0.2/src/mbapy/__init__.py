4ad39f6a71590cddd402016522606d6f0f22cb0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:59:27 +0000

    Add package mbapy/0.0.2/mbapy-0.0.2.tar.gz

---
+++ packages/mbapy/mbapy-0.0.2.tar.gz/mbapy-0.0.2/src/mbapy/__init__.py
@@ -0,0 +1,13 @@
+from . import __version__, base, file, plot, web
+
+# def main():
+#     pass
+"""
+var naming:
+
+constant : CONSTANT_VAR_NAME
+variable : var_name
+func : func_name
+global var: globalVarName
+class : ClassName
+"""
