c027a43283752133bcf65f819438fffaed857755
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:05:58 +0000

    Add package Geode-ExplicitModeling/2.1.11/Geode_ExplicitModeling-2.1.11-py3-none-any.whl

---
+++ packages/Geode-ExplicitModeling/Geode_ExplicitModeling-2.1.11-py3-none-any.whl/geode_explicitmodeling/explicit_modeling2d.py
@@ -0,0 +1,10 @@
+#
+# Copyright (c) 2019 - 2022 Geode-solutions. All rights reserved.
+#
+
+import opengeode
+import geode_common
+import geode_conversion
+
+from .geode_explicitmodeling_py_explicit_modeling2d import *
+ExplicitModeling2d.initialize()
