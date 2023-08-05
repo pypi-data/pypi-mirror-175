b02b395d9a24172794515a823567bbf9b03b5b0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:03:33 +0000

    Add package samtariFirstPackage/0.1.7/samtariFirstPackage-0.1.7.tar.gz

---
+++ packages/samtariFirstPackage/samtariFirstPackage-0.1.7.tar.gz/samtariFirstPackage-0.1.7/samtariFirstPackage/main.py
@@ -0,0 +1,5 @@
+# from add import add
+# from sub import sub
+
+x = add.add(1,2)
+print(x)
---
+++ packages/samtariFirstPackage/samtariFirstPackage-0.1.7.tar.gz/samtariFirstPackage-0.1.7/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.1.7' 
+DESCRIPTION = 'My first Python package'
+LONG_DESCRIPTION = 'My first Python package with a slightly longer description'
+
+# setup
+setup(
+       # name
+        name="samtariFirstPackage", 
+        version=VERSION,
+        author="Sam Yang",
+        author_email="samtari@163.com",
+        description=DESCRIPTION,
+        long_description=LONG_DESCRIPTION,
+        packages=find_packages(),
+        install_requires=[], # add any additional packages that 
+        # Packages needed，例如：'caer'
+        
+        keywords=['python', 'first package'],
+        classifiers= [
+            "Development Status :: 3 - Alpha",
+            "Intended Audience :: Education",
+            "Programming Language :: Python :: 2",
+            "Programming Language :: Python :: 3",
+            "Operating System :: MacOS :: MacOS X",
+            "Operating System :: Microsoft :: Windows",
+        ]
+)
