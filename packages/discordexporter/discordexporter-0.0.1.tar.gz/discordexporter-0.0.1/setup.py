66ef0c812d4138e550976ded7624b9d146d41e84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:27 +0000

    Add package discordexporter/0.0.1/discordexporter-0.0.1.tar.gz

---
+++ packages/discordexporter/discordexporter-0.0.1.tar.gz/discordexporter-0.0.1/setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+
+setuptools.setup(
+    name="discordexporter",
+    version="0.0.1",
+    description="",
+    long_description="",
+    url="https://www.example.com",
+    author="me",
+    author_email="me@example.com",
+    packages=[],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+    ],
+)
