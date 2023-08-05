005621b8d69e8a20b79af9ebec283768fc73a531
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:05:41 +0000

    Add package imgStampTools/1.0/imgStampTools-1.0.tar.gz

---
+++ packages/imgStampTools/imgStampTools-1.0.tar.gz/imgStampTools-1.0/setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+setuptools.setup(
+    name="imgStampTools",
+    version="1.0",
+    author="yazheng",
+    author_email="981638732@qq.com",
+    description="lz's utils",
+    long_description="lz's utils",
+    long_description_content_type="text/markdown",
+    url="https://pypi.org/project/imgStampTools/",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+    ],  # 这个文件适用的python版本，安全等等，我这里只写入了版本
+)
