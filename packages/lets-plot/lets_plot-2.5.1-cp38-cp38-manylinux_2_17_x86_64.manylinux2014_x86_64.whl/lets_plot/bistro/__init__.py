5fd1f1e806e80571bf77fcff4ee3889a0da98672
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:58 +0000

    Add package lets-plot/2.5.1/lets_plot-2.5.1-cp38-cp38-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/lets-plot/lets_plot-2.5.1-cp38-cp38-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/lets_plot/bistro/__init__.py
@@ -0,0 +1,10 @@
+#  Copyright (c) 2020. JetBrains s.r.o.
+#  Use of this source code is governed by the MIT license that can be found in the LICENSE file.
+
+from .corr import *
+from .im import *
+from .qq import *
+
+__all__ = (im.__all__ +
+           corr.__all__ +
+           qq.__all__)
