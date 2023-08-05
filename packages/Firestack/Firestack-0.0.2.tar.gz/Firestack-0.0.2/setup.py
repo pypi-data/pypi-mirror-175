7f7162594f787e7abd079a138663244dd3f0331f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:11:58 +0000

    Add package Firestack/0.0.2/Firestack-0.0.2.tar.gz

---
+++ packages/Firestack/Firestack-0.0.2.tar.gz/Firestack-0.0.2/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='Firestack',
+    version='0.0.2',
+    license='MIT',
+    author="Thandden",
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    url='https://github.com/Thandden/Firestack',
+    keywords='Authenticate Python apps with Firebase.',
+    install_requires=[
+          'pydantic',
+          'requests'
+      ],
+
+)
