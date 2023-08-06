8d86d495b42f237d5502d769547a82f304ecddf2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:15:16 +0000

    Add package norma2/0.0.1a8/norma2-0.0.1a8.tar.gz

---
+++ packages/norma2/norma2-0.0.1a8.tar.gz/norma2-0.0.1a8/norma2/config/from_gitignore.py
@@ -0,0 +1,17 @@
+import os
+
+from igittigitt import IgnoreParser
+from rich.console import Console
+
+from norma2.config.config_class import Config
+
+
+def from_gitignore(console: Console, conf_path: str = ".") -> Config:
+    full_path = os.path.join(conf_path, ".gitignore")
+    full_path = os.path.abspath(full_path)
+    parser = IgnoreParser()
+    parser.parse_rule_file(full_path)
+    matches = parser.match
+    conf = Config(console)
+    conf.gitignore_matches = matches
+    return conf
