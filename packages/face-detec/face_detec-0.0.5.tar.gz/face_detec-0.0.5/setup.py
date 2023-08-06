085d1aa8891f2133fcd0e77c4651bfb09d9b96d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:31 +0000

    Add package face-detec/0.0.5/face_detec-0.0.5.tar.gz

---
+++ packages/face-detec/face_detec-0.0.5.tar.gz/face_detec-0.0.5/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='face_detec',
+    version='0.0.5',
+    author="sontung",
+    author_email='209tungns@gmail.com',
+    packages=find_packages(),
+    # package_dir={},
+    url='https://github.com/209sontung/NEU-ID',
+    keywords=['face_detection', 'face_alignment'],
+    install_requires=[
+          'scikit-learn',
+          'opencv-python',
+          'mediapipe',
+          'numpy'
+      ],
+
+)
