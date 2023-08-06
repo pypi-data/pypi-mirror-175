bd8ed9909cd24c2bfe6b613aaf78776d7b41a4b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:59:08 +0000

    Add package multigrate/0.0.1/multigrate-0.0.1.tar.gz

---
+++ packages/multigrate/multigrate-0.0.1.tar.gz/multigrate-0.0.1/src/multigrate/__init__.py
@@ -0,0 +1,7 @@
+from importlib.metadata import version
+
+from . import data, distributions, model, module, nn
+
+__all__ = ["data", "distributions", "model", "module", "nn"]
+
+__version__ = version("multigrate")
