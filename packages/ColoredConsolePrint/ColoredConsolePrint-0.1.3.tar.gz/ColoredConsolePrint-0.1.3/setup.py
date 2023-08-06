fab26daa2cb84138dc7962a25cc2da06ed6345f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:20:09 +0000

    Add package ColoredConsolePrint/0.1.3/ColoredConsolePrint-0.1.3.tar.gz

---
+++ packages/ColoredConsolePrint/ColoredConsolePrint-0.1.3.tar.gz/ColoredConsolePrint-0.1.3/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='ColoredConsolePrint',
+    version='0.1.3',
+    description='a package to print colorful text in the console',
+    url='https://github.com/Nico1806/CCPrint',
+    author='Nico F.',
+    author_email='nicofisseler@gmail.com',
+    license='MIT',
+    packages=['package'],
+    classifiers=[
+        'Programming Language :: Python :: 3.9',
+    ],
+)
