c40da56f9528f0931c4ecca6306b40ef17db500e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:20:50 +0000

    Add package nithesh-house-prices/0.0.3/nithesh_house_prices-0.0.3.tar.gz

---
+++ packages/nithesh-house-prices/nithesh_house_prices-0.0.3.tar.gz/nithesh_house_prices-0.0.3/src/house_prices/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+setup(
+    name="nithesh_house_prices",
+    version="0.0.3",
+    packages=['train', 'preprocess', 'inference'],
+    author="Nithesh Kumar MANIMARAN",
+)
