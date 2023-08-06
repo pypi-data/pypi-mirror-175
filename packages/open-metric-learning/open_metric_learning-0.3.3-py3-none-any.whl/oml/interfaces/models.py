abf34ca7d202e57574b7d73efc41c1f43cdf6cda
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:15:46 +0000

    Add package open-metric-learning/0.3.3/open_metric_learning-0.3.3-py3-none-any.whl

---
+++ packages/open-metric-learning/open_metric_learning-0.3.3-py3-none-any.whl/oml/interfaces/models.py
@@ -0,0 +1,20 @@
+from abc import ABC
+
+from torch import Tensor, nn
+
+
+class IExtractor(nn.Module, ABC):
+    """
+    Models have to inherit this interface to be comparable with the rest of the library.
+
+    """
+
+    def extract(self, x: Tensor) -> Tensor:
+        return self.forward(x)
+
+    @property
+    def feat_dim(self) -> int:
+        raise NotImplementedError()
+
+
+__all__ = ["IExtractor"]
