992ba97850511ef19f6d2967acc7ab9be38038c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:10:46 +0000

    Add package st-dataframe-component/0.0.3/st_dataframe_component-0.0.3-py3-none-any.whl

---
+++ packages/st-dataframe-component/st_dataframe_component-0.0.3-py3-none-any.whl/st_dataframe_component/__init__.py
@@ -0,0 +1,20 @@
+import os
+
+import streamlit.components.v1 as components
+
+_RELEASE = True
+
+if not _RELEASE:
+	_component_func = components.declare_component(
+	    "st_dataframe_component",
+	    url="http://localhost:3001",
+	)
+else:
+	parent_dir = os.path.dirname(os.path.abspath(__file__))
+	build_dir = os.path.join(parent_dir, "frontend/dist")
+	_component_func = components.declare_component("st_dataframe_component", path=build_dir)
+
+
+def st_custom_dataframe(data) -> int:
+	component_value = _component_func(data=data)
+	return component_value
