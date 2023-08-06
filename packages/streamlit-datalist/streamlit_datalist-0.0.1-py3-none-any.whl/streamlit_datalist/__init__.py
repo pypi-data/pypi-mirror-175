c6f1247d269fbc09fa1c0b4809cbf45ad26630cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:12:32 +0000

    Add package streamlit-datalist/0.0.1/streamlit_datalist-0.0.1-py3-none-any.whl

---
+++ packages/streamlit-datalist/streamlit_datalist-0.0.1-py3-none-any.whl/streamlit_datalist/__init__.py
@@ -0,0 +1,36 @@
+import streamlit.components.v1 as components
+import streamlit as st
+import os
+
+_RELEASE = True
+
+if _RELEASE:
+    root_dir = os.path.dirname(os.path.abspath(__file__))
+    build_dir = os.path.join(root_dir, 'frontend/build')
+
+    _streamlit_datalist = components.declare_component(
+        "stDatalist",
+        path=build_dir
+    )
+
+else:
+    _streamlit_datalist = components.declare_component(
+        "stDatalist",
+        url="http://localhost:3001"
+    )
+
+
+def stDatalist(label:str, options:list, index:int=0, key=None):
+    return _streamlit_datalist(label=label, options=options, index=index, key=key, default=None)
+
+
+if not _RELEASE:
+ 
+    my_sel = stDatalist('This datalist is...', ["great", "cool", "neat"], key='1')
+
+    st.write('')
+    st.write('')
+    st.write('')
+    st.write('')
+    st.write('')
+    st.write('The value you selected is: ', my_sel)
