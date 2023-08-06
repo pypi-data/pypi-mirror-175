38958100cae2135296223a4862701fd7b71d9161
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:31:19 +0000

    Add package streamlit-keycloak/0.1.6/streamlit_keycloak-0.1.6-py3-none-any.whl

---
+++ packages/streamlit-keycloak/streamlit_keycloak-0.1.6-py3-none-any.whl/streamlit_keycloak/frontend/build/precache-manifest.58bd8094ec9445f17dd5c211fae6d6c7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c646c38c4e6d69be965977696d2cd6a1",
+    "url": "./index.html"
+  },
+  {
+    "revision": "87a79cd534aad5f02a5a",
+    "url": "./static/js/2.f5ae4790.chunk.js"
+  },
+  {
+    "revision": "9fb993bbd978ed97d00146e2dfc5286c",
+    "url": "./static/js/2.f5ae4790.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ab4756eb181cf1eacf3",
+    "url": "./static/js/main.0f12263a.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "./static/js/main.0f12263a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "abf035dd39300f62a9ce",
+    "url": "./static/js/runtime-main.27025a60.js"
+  }
+]);
