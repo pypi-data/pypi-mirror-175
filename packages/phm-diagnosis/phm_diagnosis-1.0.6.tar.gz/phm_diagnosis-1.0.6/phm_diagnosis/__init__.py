e067ffc5a1f7c438203eb1d68e94e4aee5c26302
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:59:50 +0000

    Add package phm-diagnosis/1.0.6/phm_diagnosis-1.0.6.tar.gz

---
+++ packages/phm-diagnosis/phm_diagnosis-1.0.6.tar.gz/phm_diagnosis-1.0.6/phm_diagnosis/__init__.py
@@ -0,0 +1,20 @@
+#!
+from .causal import why_builder
+from .causal import get_cause_graph_from_why
+from .causal import causal_model_build
+from .causal import plot_causal_graph
+from .causal import causal_discovery
+
+'''
+from .phm_feature import enable_parallel
+from .phm_feature import disable_parallel
+from .phm_feature import feature_t
+#from .phm_feature import feature_f
+from .phm_feature import fft
+from .phm_feature import power
+from .phm_feature import ifft
+from .phm_feature import cepstrum
+from .phm_feature import envelope
+from .phm_feature import window
+from .phm_feature import divide
+'''
---
+++ packages/phm-diagnosis/phm_diagnosis-1.0.6.tar.gz/phm_diagnosis-1.0.6/phm_diagnosis/database/base.py
@@ -0,0 +1,39 @@
+#!
+from abc import ABCMeta, abstractmethod
+import re
+import happybase
+import pandas as pd
+import traceback
+import pdb
+import time
+from functools import wraps
+from datetime import datetime
+
+p = pdb.set_trace
+
+'''
+interface of db client
+formular the database client class with interface class
+puporse for make the code formuler, but it's hard to define the function to match all time and status
+'''
+
+
+class DBInterface(metaclass=ABCMeta):
+
+    '''
+    @abstractmethod
+    def connect(self, **keargs):
+        raise NotImplementedError
+
+    @abstractmethod
+    def exec(self, **keargs):
+        raise NotImplementedError
+
+    @abstractmethod
+    def disconnect(self, **keargs):
+        raise NotImplementedError
+    '''
+
+    @abstractmethod
+    def read(self, **keargs):
+        raise NotImplementedError
