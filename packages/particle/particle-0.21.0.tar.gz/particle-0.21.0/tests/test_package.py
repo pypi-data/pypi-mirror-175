7d0d069caeb9d4d3bdbbabc737abe97ffb151f6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:59:26 +0000

    Add package particle/0.21.0/particle-0.21.0.tar.gz

---
+++ packages/particle/particle-0.21.0.tar.gz/particle-0.21.0/tests/test_package.py
@@ -0,0 +1,12 @@
+# Copyright (c) 2018-2022, Eduardo Rodrigues and Henry Schreiner.
+#
+# Distributed under the 3-clause BSD license, see accompanying file LICENSE
+# or https://github.com/scikit-hep/particle for details.
+
+from __future__ import annotations
+
+import particle
+
+
+def test_package_import():
+    assert particle is not None
