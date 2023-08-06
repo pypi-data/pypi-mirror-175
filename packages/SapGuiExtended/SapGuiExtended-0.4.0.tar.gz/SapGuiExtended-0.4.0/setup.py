bc976ad4c529a7539f1c6694429e3c561d3a4541
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:07 +0000

    Add package SapGuiExtended/0.4.0/SapGuiExtended-0.4.0.tar.gz

---
+++ packages/SapGuiExtended/SapGuiExtended-0.4.0.tar.gz/SapGuiExtended-0.4.0/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+import pathlib
+
+setup(
+    name='SapGuiExtended',
+    version='0.4.0',
+    description='SOVOS SAP GUI Library for Robot Framework',
+    url='https://SovosGvatLatam@dev.azure.com/SovosGvatLatam/"SAP%20Labs"/_git/SovosSAPLibrary',
+    long_description='Library to run Robot Framework on SAP GUI',
+    long_description_content_type="text/markdown",
+    author='Daniela Silva',
+    author_email='daniela.silva@sovos.com',
+    license="MIT",
+    packages=['SAPGuiExtended'],
+    include_package_data=True,
+
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Programming Language :: Python',
+    ],
+
+
+    )
