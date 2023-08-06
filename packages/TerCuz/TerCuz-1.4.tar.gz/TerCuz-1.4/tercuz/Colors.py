51dedd0f88c31c8c912a2b9492ae661c1c5575a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:12:31 +0000

    Add package TerCuz/1.4/TerCuz-1.4.tar.gz

---
+++ packages/TerCuz/TerCuz-1.4.tar.gz/TerCuz-1.4/tercuz/Colors.py
@@ -0,0 +1,48 @@
+# THIS CLASS CONTAINS ALL BG NAD FR COLOR VALUES.
+class Colors:  # ESC [ <n> m	SGR	Set Graphics Rendition	Set the format of the screen and text as specified by <n>
+
+
+    # BACKGROUND NORMAL COLORS
+
+    BG_Black = '\x1b[40m'
+    BG_Red = '\x1b[41m'
+    BG_Green = '\x1b[42m'
+    BG_Yellow = '\x1b[43m'
+    BG_Blue = '\x1b[44m'
+    BG_Magenta = '\x1b[45m'
+    BG_Cyan = '\x1b[46m'
+    BG_White = '\x1b[47m'
+
+    Bg_Reset = '\x1b[49m'
+
+    BG_B_Black = "\x1b[100m"
+    BG_B_Red = "\x1b[101m"
+    BG_B_Green = "\x1b[102m"
+    BG_B_Yellow = "\x1b[103m"
+    BG_B_Blue = "\x1b[104m"
+    BG_B_Magenta = "\x1b[105m"
+    BG_B_Cyan = "\x1b[106m"
+    BG_B_White = "\x1b[107m"
+
+    # FOREGROUND COLORS FOR TEXT
+
+    FR_Black = "\x1b[30m"
+    FR_Red = "\x1b[31m"
+    FR_Green = "\x1b[32m"
+    FR_Yellow = "\x1b[33m"
+    FR_Blue = "\x1b[34m"
+    FR_Magenta = "\x1b[35m"
+    FR_Cyan = "\x1b[36m"
+    FR_White = "\x1b[37m"
+
+    Reset_Fr = '\x1b[39m'
+
+    # TEXT BRIGHT COLORS
+    FR_B_Black = "\x1b[90m"
+    FR_B_Red = "\x1b[91m"
+    FR_B_Green = "\x1b[92m"
+    FR_B_Yellow = "\x1b[93m"
+    FR_B_Blue = "\x1b[94m"
+    FR_B_Magenta = "\x1b[95m"
+    FR_B_Cyan = "\x1b[96m"
+    FR_B_White = "\x1b[97m"
