f0f1028447bbd114a61349b3c195ba2fc5daed09
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:54 +0000

    Add package installrequirestest3/0.1/installrequirestest3-0.1.tar.gz

---
+++ packages/installrequirestest3/installrequirestest3-0.1.tar.gz/installrequirestest3-0.1/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+import sys
+
+print(sys.argv)
+print('test')
+
+setup(
+  name = 'installrequirestest3',
+  packages = ['installrequirestest3'],
+  version = '0.1'
+)
