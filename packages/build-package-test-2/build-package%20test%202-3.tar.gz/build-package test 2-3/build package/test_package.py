d3752e71ad2561299c6061a9fe03adadfc3fd7dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:13:31 +0000

    Add package build-package-test-2/3/build-package%20test%202-3.tar.gz

---
+++ packages/build-package-test-2/build-package%20test%202-3.tar.gz/build-package test 2-3/build package/test_package.py
@@ -0,0 +1,6 @@
+def print_hello():
+    print("hello")
+
+
+def print_world_with_name(name):
+    print("world %s" % name)
---
+++ packages/build-package-test-2/build-package%20test%202-3.tar.gz/build-package test 2-3/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+VERSION = "3"
+DESCRIPTION = "Python package for the project"
+LONG_DESCRIPTION = "Testing how to build python packages"
+
+# Setting up
+setup(
+    name="build-package test 2",
+    version=VERSION,
+    author="Ntwanano Rikhotso",
+    author_email="sanele.rikhotso.99@gmail.com",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(),
+    install_requires=[],
+    keywords=["python", "test"],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ],
+)
