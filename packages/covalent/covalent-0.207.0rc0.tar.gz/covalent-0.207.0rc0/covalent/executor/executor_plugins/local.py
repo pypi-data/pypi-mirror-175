f1eb4382a21f1b46fcce30892ac59dd6e5d748e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:08:29 +0000

    Add package covalent/0.207.0rc0/covalent-0.207.0rc0.tar.gz

---
+++ packages/covalent/covalent-0.207.0rc0.tar.gz/covalent-0.207.0rc0/covalent/executor/executor_plugins/local.py
@@ -0,0 +1,56 @@
+# Copyright 2021 Agnostiq Inc.
+#
+# This file is part of Covalent.
+#
+# Licensed under the GNU Affero General Public License 3.0 (the "License").
+# A copy of the License may be obtained with this software package or at
+#
+#      https://www.gnu.org/licenses/agpl-3.0.en.html
+#
+# Use of this file is prohibited except in compliance with the License. Any
+# modifications or derivative works of this file must retain this copyright
+# notice, and modified files must contain a notice indicating that they have
+# been altered from the originals.
+#
+# Covalent is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE. See the License for more details.
+#
+# Relief from the License may be granted by purchasing a commercial license.
+
+"""
+Module for defining a local executor that directly invokes the input python function.
+
+This is a plugin executor module; it is loaded if found and properly structured.
+"""
+
+import os
+from typing import Callable, Dict, List
+
+# Relative imports are not allowed in executor plugins
+from covalent._shared_files import logger
+from covalent.executor import BaseExecutor, wrapper_fn  # nopycln: import
+
+# The plugin class name must be given by the executor_plugin_name attribute:
+EXECUTOR_PLUGIN_NAME = "LocalExecutor"
+
+app_log = logger.app_log
+log_stack_info = logger.log_stack_info
+
+_EXECUTOR_PLUGIN_DEFAULTS = {
+    "log_stdout": "stdout.log",
+    "log_stderr": "stderr.log",
+    "cache_dir": os.path.join(
+        os.environ.get("XDG_CACHE_HOME") or os.path.join(os.environ["HOME"], ".cache"), "covalent"
+    ),
+}
+
+
+class LocalExecutor(BaseExecutor):
+    """
+    Local executor class that directly invokes the input function.
+    """
+
+    def run(self, function: Callable, args: List, kwargs: Dict, task_metadata: Dict):
+        app_log.debug(f"Running function {function} locally")
+        return function(*args, **kwargs)
