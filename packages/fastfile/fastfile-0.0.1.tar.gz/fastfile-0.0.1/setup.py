94a5ae2b46f7c5824e94faf65837570daab8ea3f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:47 +0000

    Add package fastfile/0.0.1/fastfile-0.0.1.tar.gz

---
+++ packages/fastfile/fastfile-0.0.1.tar.gz/fastfile-0.0.1/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+VERSION = '0.0.1'
+DESCRIPTION = 'Writing to files fast.'
+
+# Setting up
+setup(
+    name="fastfile",
+    version=VERSION,
+    author="Fritz Forster",
+    author_email="fritzforster@protonmail.com",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['python', 'file', 'write file', 'fast witing', 'simple'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+    ]
+)
