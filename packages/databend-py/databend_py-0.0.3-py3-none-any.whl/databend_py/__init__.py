82b8f86ce005b7fc8bee49062b0cd2c3c037225f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:04:22 +0000

    Add package databend-py/0.0.3/databend_py-0.0.3-py3-none-any.whl

---
+++ packages/databend-py/databend_py-0.0.3-py3-none-any.whl/databend_py/__init__.py
@@ -0,0 +1,9 @@
+from .util.helper import asbool, Helper
+from .util.escape import escape_params
+from .client import Client
+from .connection import Connection
+
+VERSION = (0, 0, 3)
+__version__ = '.'.join(str(x) for x in VERSION)
+
+__all__ = ['Client', 'Connection']
