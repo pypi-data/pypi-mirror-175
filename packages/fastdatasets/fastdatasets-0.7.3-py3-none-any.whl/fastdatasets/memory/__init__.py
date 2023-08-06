4c1ba3a8cbd9eed5e19971ff7a712ade8a395ddf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:01:51 +0000

    Add package fastdatasets/0.7.3/fastdatasets-0.7.3-py3-none-any.whl

---
+++ packages/fastdatasets/fastdatasets-0.7.3-py3-none-any.whl/fastdatasets/memory/__init__.py
@@ -0,0 +1,5 @@
+# @Time    : 2022/10/27 18:13
+# @Author  : tk
+from ..common.iterable_dataset import IterableDatasetBase
+from ..common.random_dataset import RandomDatasetBase
+from .dataset import *
