cb963c9541601b428d13597a4aaa4633bbae6af9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:30:11 +0000

    Add package foca/0.10.0/foca-0.10.0-py3-none-any.whl

---
+++ packages/foca/foca-0.10.0-py3-none-any.whl/foca/foca.py
@@ -0,0 +1,170 @@
+"""Class for setting up and initializing a FOCA-based microservice."""
+
+import logging
+from pathlib import Path
+from typing import Optional
+
+from connexion import App
+
+from foca.security.access_control.register_access_control import (
+    register_access_control
+)
+from foca.security.access_control.constants import (
+    DEFAULT_SPEC_CONTROLLER,
+    DEFAULT_ACCESS_CONTROL_DB_NAME,
+    DEFAULT_ACESS_CONTROL_COLLECTION_NAME,
+)
+from foca.api.register_openapi import register_openapi
+from foca.config.config_parser import ConfigParser
+from foca.database.register_mongodb import register_mongodb
+from foca.errors.exceptions import register_exception_handler
+from foca.factories.connexion_app import create_connexion_app
+from foca.factories.celery_app import create_celery_app
+from foca.security.cors import enable_cors
+
+# Get logger instance
+logger = logging.getLogger(__name__)
+
+
+class Foca:
+
+    def __init__(
+        self,
+        config_file: Optional[Path] = None,
+        custom_config_model: Optional[str] = None,
+    ) -> None:
+        """Instantiate FOCA class.
+
+            Args:
+                config_file: Path to application configuration file in YAML
+                    format.  Cf. :py:class:`foca.models.config.Config` for
+                    required file structure.
+                custom_config_model: Path to model to be used for custom config
+                    parameter validation, supplied in "dot notation", e.g.,
+                    ``myapp.config.models.CustomConfig`, where ``CustomConfig``
+                    is the actual importable name of a `pydantic` model for
+                    your custom configuration, deriving from ``BaseModel``.
+                    FOCA will attempt to instantiate the model with the values
+                    passed to the ``custom`` section in the application's
+                    configuration, if present. Wherever possible, make sure
+                    that default values are supplied for each config
+                    parameters, so as to make it easier for others to
+                    write/modify their app configuration.
+
+            Attributes:
+                config_file: Path to application configuration file in YAML
+                    format.  Cf. :py:class:`foca.models.config.Config` for
+                    required file structure.
+                custom_config_model: Path to model to be used for custom config
+                    parameter validation, supplied in "dot notation", e.g.,
+                    ``myapp.config.models.CustomConfig`, where ``CustomConfig``
+                    is the actual importable name of a `pydantic` model for
+                    your custom configuration, deriving from ``BaseModel``.
+                    FOCA will attempt to instantiate the model with the values
+                    passed to the ``custom`` section in the application's
+                    configuration, if present. Wherever possible, make sure
+                    that default values are supplied for each config
+                    parameters, so as to make it easier for others to
+                    write/modify their app configuration.
+        """
+        self.config_file: Optional[Path] = Path(
+            config_file
+        ) if config_file is not None else None
+        self.custom_config_model: Optional[str] = custom_config_model
+
+    def create_app(self) -> App:
+        """Set up and initialize FOCA-based microservice.
+
+        Returns:
+            Connexion application instance.
+        """
+
+        # Parse config parameters and format logging
+        conf = ConfigParser(
+            config_file=self.config_file,
+            custom_config_model=self.custom_config_model,
+            format_logs=True,
+        ).config
+        logger.info("Log formatting configured.")
+        if self.config_file is not None:
+            logger.info(f"Configuration file '{self.config_file}' parsed.")
+        else:
+            logger.info("Default app configuration used.")
+
+        # Create Connexion app
+        cnx_app = create_connexion_app(conf)
+        logger.info("Connexion app created.")
+
+        # Register error handlers
+        cnx_app = register_exception_handler(cnx_app)
+        logger.info("Error handler registered.")
+
+        # Enable cross-origin resource sharing
+        if(conf.security.cors.enabled is True):
+            enable_cors(cnx_app.app)
+            logger.info("CORS enabled.")
+        else:
+            logger.info("CORS not enabled.")
+
+        # Register OpenAPI specs
+        if conf.api.specs:
+            cnx_app = register_openapi(
+                app=cnx_app,
+                specs=conf.api.specs,
+            )
+        else:
+            logger.info("No OpenAPI specifications provided.")
+
+        # Register MongoDB
+        if conf.db:
+            cnx_app.app.config.foca.db = register_mongodb(
+                app=cnx_app.app,
+                conf=conf.db,
+            )
+            logger.info("Database registered.")
+        else:
+            logger.info("No database support configured.")
+
+        # Register permission management and casbin enforcer
+        if conf.security.auth.required:
+            if (
+                conf.security.access_control.api_specs is None or
+                conf.security.access_control.api_controllers is None
+            ):
+                conf.security.access_control.api_controllers = (
+                    DEFAULT_SPEC_CONTROLLER
+                )
+
+            if conf.security.access_control.db_name is None:
+                conf.security.access_control.db_name = (
+                    DEFAULT_ACCESS_CONTROL_DB_NAME
+                )
+
+            if conf.security.access_control.collection_name is None:
+                conf.security.access_control.collection_name = (
+                    DEFAULT_ACESS_CONTROL_COLLECTION_NAME
+                )
+
+            cnx_app = register_access_control(
+                cnx_app=cnx_app,
+                mongo_config=conf.db,
+                access_control_config=conf.security.access_control
+            )
+        else:
+            if (
+                conf.security.access_control.api_specs or
+                conf.security.access_control.api_controllers
+            ):
+                logger.error(
+                    "Please enable security config to register "
+                    "access control."
+                )
+
+        # Create Celery app
+        if conf.jobs:
+            create_celery_app(cnx_app.app)
+            logger.info("Support for background tasks set up.")
+        else:
+            logger.info("No support for background tasks configured.")
+
+        return cnx_app
---
+++ packages/foca/foca-0.10.0-py3-none-any.whl/tests/test_foca.py
@@ -0,0 +1,154 @@
+"""Tests for `foca.py` module."""
+
+from pathlib import Path
+import pytest
+import shutil
+
+from connexion import App
+from pydantic import ValidationError
+from pymongo.collection import Collection
+from pymongo.database import Database
+from yaml import (
+    safe_load,
+    safe_dump,
+)
+
+from foca import Foca
+
+DIR = Path(__file__).parent / "test_files"
+PATH_SPECS_2_YAML_ORIGINAL = str(DIR / "openapi_2_petstore.original.yaml")
+PATH_SPECS_2_YAML_MODIFIED = str(DIR / "openapi_2_petstore.modified.yaml")
+PATH_SPECS_INVALID_OPENAPI = str(DIR / "invalid_openapi_2.yaml")
+EMPTY_CONF = str(DIR / "empty_conf.yaml")
+INVALID_CONF = str(DIR / "invalid_conf.yaml")
+VALID_CONF = str(DIR / "conf_valid.yaml")
+VALID_DB_CONF = str(DIR / "conf_db.yaml")
+INVALID_DB_CONF = str(DIR / "invalid_conf_db.yaml")
+INVALID_LOG_CONF = str(DIR / "conf_invalid_log_level.yaml")
+JOBS_CONF = str(DIR / "conf_jobs.yaml")
+INVALID_JOBS_CONF = str(DIR / "conf_invalid_jobs.yaml")
+API_CONF = str(DIR / "conf_api.yaml")
+VALID_CORS_CONF_DISABLED = str(DIR / "conf_valid_cors_disabled.yaml")
+VALID_CORS_CONF_ENABLED = str(DIR / "conf_valid_cors_enabled.yaml")
+INVALID_ACCESS_CONTROL_CONF = str(DIR / "conf_invalid_access_control.yaml")
+VALID_ACCESS_CONTROL_CONF = str(DIR / "conf_valid_access_control.yaml")
+
+
+def create_modified_api_conf(path, temp_dir, api_specs_in, api_specs_out):
+    """Create a copy of a configuration YAML file"""
+    temp_path = Path(temp_dir) / 'temp_test.yaml'
+    shutil.copy2(path, temp_path)
+    with open(temp_path, 'r+') as conf_file:
+        conf = safe_load(conf_file)
+        conf["api"]["specs"][0]["path"] = api_specs_in
+        conf["api"]["specs"][0]["path_out"] = api_specs_out
+        conf_file.seek(0)
+        safe_dump(conf, conf_file)
+        conf_file.truncate()
+    return temp_path
+
+
+def test_foca_output_defaults():
+    """Ensure foca() returns a Connexion app instance; defaults only."""
+    foca = Foca()
+    app = foca.create_app()
+    assert isinstance(app, App)
+
+
+def test_foca_empty_conf():
+    """Ensure foca() require non-empty configuration parameters (if a field is
+    present)"""
+    foca = Foca(config_file=EMPTY_CONF)
+    with pytest.raises(ValidationError):
+        foca.create_app()
+
+
+def test_foca_invalid_structure():
+    """Test foca(); invalid configuration file structure."""
+    foca = Foca(config_file=INVALID_CONF)
+    with pytest.raises(ValueError):
+        foca.create_app()
+
+
+def test_foca_invalid_log():
+    """Test foca(); invalid log field"""
+    foca = Foca(config_file=INVALID_LOG_CONF)
+    with pytest.raises(ValidationError):
+        foca.create_app()
+
+
+def test_foca_jobs():
+    """Ensure foca() returns a Connexion app instance; valid jobs field"""
+    foca = Foca(config_file=JOBS_CONF)
+    app = foca.create_app()
+    assert isinstance(app, App)
+
+
+def test_foca_invalid_jobs():
+    """Test foca(); invalid jobs field"""
+    foca = Foca(config_file=INVALID_JOBS_CONF)
+    with pytest.raises(ValidationError):
+        foca.create_app()
+
+
+def test_foca_api(tmpdir):
+    """Ensure foca() returns a Connexion app instance; valid api field"""
+    temp_file = create_modified_api_conf(
+        API_CONF,
+        tmpdir,
+        PATH_SPECS_2_YAML_ORIGINAL,
+        PATH_SPECS_2_YAML_MODIFIED,
+    )
+    foca = Foca(config_file=temp_file)
+    app = foca.create_app()
+    assert isinstance(app, App)
+
+
+def test_foca_db():
+    """Ensure foca() returns a Connexion app instance; valid db field"""
+    foca = Foca(config_file=VALID_DB_CONF)
+    app = foca.create_app()
+    my_db = app.app.config.foca.db.dbs['my-db']
+    my_coll = my_db.collections['my-col-1']
+    assert isinstance(my_db.client, Database)
+    assert isinstance(my_coll.client, Collection)
+    assert isinstance(app, App)
+
+
+def test_foca_invalid_db():
+    """Test foca(); invalid db field"""
+    foca = Foca(config_file=INVALID_DB_CONF)
+    with pytest.raises(ValidationError):
+        foca.create_app()
+
+
+def test_foca_CORS_enabled():
+    """Ensures CORS is enabled for Microservice"""
+    foca = Foca(config_file=VALID_CORS_CONF_ENABLED)
+    app = foca.create_app()
+    assert app.app.config.foca.security.cors.enabled is True
+
+
+def test_foca_CORS_disabled():
+    """Ensures CORS is disabled if user explicitly mentions"""
+    foca = Foca(config_file=VALID_CORS_CONF_DISABLED)
+    app = foca.create_app()
+    assert app.app.config.foca.security.cors.enabled is False
+
+
+def test_foca_invalid_access_control():
+    """Ensures access control is not enabled if auth is disabled"""
+    foca = Foca(config_file=INVALID_ACCESS_CONTROL_CONF)
+    app = foca.create_app()
+    assert app.app.config.foca.db is None
+
+
+def test_foca_valid_access_control():
+    """Ensures access control enabled."""
+    foca = Foca(config_file=VALID_ACCESS_CONTROL_CONF)
+    app = foca.create_app()
+    my_db = app.app.config.foca.db.dbs['test_db']
+    my_coll = my_db.collections['test_collection']
+    assert isinstance(my_db.client, Database)
+    assert isinstance(my_coll.client, Collection)
+    assert isinstance(app, App)
