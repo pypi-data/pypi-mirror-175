a2f78c0b6567e387bdb9598adecb34b93b2fe2bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:15:04 +0000

    Add package example-package-ah-heakl/0.0.1/example_package_ah_heakl-0.0.1.tar.gz

---
+++ packages/example-package-ah-heakl/example_package_ah_heakl-0.0.1.tar.gz/example_package_ah_heakl-0.0.1/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+VERSION = "0.0.1"
+DESCRIPTION = "Basic library add_one"
+LONG_DESCRIPTION = "basic library for testing pypi"
+
+# Setting up
+setup(
+    name="example_package_ah_heakl",
+    version=VERSION,
+    author="Ahmed Heakl",
+    author_email="ahmed.heakl@ejust.edu.eg",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages("example_package_ah_heakl"),
+    install_requires=[],
+    keywords=["python", "add_one"],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ],
+)
