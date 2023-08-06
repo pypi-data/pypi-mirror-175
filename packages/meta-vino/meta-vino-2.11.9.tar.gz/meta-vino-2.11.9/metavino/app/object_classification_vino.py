8ee2764aae0392359fd172b2b7f92510111f3174
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:02:56 +0000

    Add package meta-vino/2.11.9/meta-vino-2.11.9.tar.gz

---
+++ packages/meta-vino/meta-vino-2.11.9.tar.gz/meta-vino-2.11.9/metavino/app/object_classification_vino.py
@@ -0,0 +1,10 @@
+class ObjectClassificationVino:
+    def __init__(self):
+        pass
+
+    def predict(self, image, class_names):
+        pass
+
+    @staticmethod
+    def show(image, result):
+        pass
