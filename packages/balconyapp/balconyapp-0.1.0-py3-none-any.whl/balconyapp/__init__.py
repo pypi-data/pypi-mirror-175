f8e2021f56b133ac28c472afd4952f7d76b298d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:13:45 +0000

    Add package balconyapp/0.1.0/balconyapp-0.1.0-py3-none-any.whl

---
+++ packages/balconyapp/balconyapp-0.1.0-py3-none-any.whl/balconyapp/__init__.py
@@ -0,0 +1,6 @@
+"""
+Note: All custom Balcony App definitions must be exported here.
+Balcony dynamically imports the statements on this file, on runtime.
+"""
+
+from .example_app import ExampleBalconyApplication
