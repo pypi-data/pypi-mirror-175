5984cac5f416eee3b5e19afbea214b1298d1e89c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:14:16 +0000

    Add package ktdg/0.1.17/ktdg-0.1.17.tar.gz

---
+++ packages/ktdg/ktdg-0.1.17.tar.gz/ktdg-0.1.17/src/ktdg/generation/__init__.py
@@ -0,0 +1,5 @@
+from .generation import Config, Data, add_comments, generate, save_data  # noqa
+from .answers import Answer  # noqa
+from .students import Student  # noqa
+from .skills import Skill  # noqa
+from .questions import Question  # noqa
