44a1e6aae2a23def264765702aec807f62cceb4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:33 +0000

    Add package coramin/0.2.4/coramin-0.2.4.tar.gz

---
+++ packages/coramin/coramin-0.2.4.tar.gz/coramin-0.2.4/coramin/utils/pyomo_utils.py
@@ -0,0 +1,32 @@
+import pyomo.environ as pe
+from pyomo.core.expr.sympy_tools import sympyify_expression, sympy2pyomo_expression
+from pyomo.core.expr.numvalue import is_fixed
+
+
+def get_objective(m):
+    """
+    Assert that there is only one active objective in m and return it.
+
+    Parameters
+    ----------
+    m: pyomo.core.base.block._BlockData
+
+    Returns
+    -------
+    obj: pyomo.core.base.objective._ObjectiveData
+    """
+    obj = None
+    for o in m.component_data_objects(pe.Objective, descend_into=True, active=True, sort=True):
+        if obj is not None:
+            raise ValueError('Found multiple active objectives')
+        obj = o
+    return obj
+
+
+def simplify_expr(expr):
+    if is_fixed(expr):
+        return pe.value(expr)
+    else:
+        om, se = sympyify_expression(expr)
+        se = se.simplify()
+        return sympy2pyomo_expression(se, om)
---
+++ packages/coramin/coramin-0.2.4.tar.gz/coramin-0.2.4/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+from distutils.core import Extension
+
+
+setup(name='coramin',
+      version='0.2.4',
+      packages=find_packages(),
+      ext_modules=[],
+      description='Coramin: Pyomo tools for MINLP',
+      long_description=open('README.md').read(),
+      long_description_content_type="text/markdown",
+      author='Coramin Developers',
+      maintainer_email='mlbynum@sandia.gov',
+      license='Revised BSD',
+      url='https://github.com/Coramin/Coramin',
+      install_requires=['pyomo>=6.4.1', 'numpy', 'scipy', 'networkx'],
+      include_package_data=True,
+      scripts=[],
+      python_requires='>=3.7',
+      classifiers=["Programming Language :: Python :: 3",
+                   "Programming Language :: Python :: 3.7",
+                   "Programming Language :: Python :: 3.8",
+                   "Programming Language :: Python :: 3.9",
+                   "License :: OSI Approved :: BSD License",
+                   "Operating System :: OS Independent"])
