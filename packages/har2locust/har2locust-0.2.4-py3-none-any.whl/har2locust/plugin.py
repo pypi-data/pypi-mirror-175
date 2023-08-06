5466d613ae205e463c6a814b41a794991b7a8fba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:26:02 +0000

    Add package har2locust/0.2.4/har2locust-0.2.4-py3-none-any.whl

---
+++ packages/har2locust/har2locust-0.2.4-py3-none-any.whl/har2locust/plugin.py
@@ -0,0 +1,18 @@
+from typing import List, Dict
+from typing import Callable
+
+# For an example of how to write a plugin, see plugins/
+
+
+class entriesprocessor:
+    processors: List[Callable[[List[Dict]], None]] = []
+
+    def __init__(self, func: Callable[[List[Dict]], None]):
+        self.processors.append(func)
+
+
+class valuesprocessor:
+    processors: List[Callable[[Dict], None]] = []
+
+    def __init__(self, func: Callable[[Dict], None]):
+        self.processors.append(func)
