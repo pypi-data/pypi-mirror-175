17f61bcd681cc8204fc221f1dbf89509698f9fd1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:19:19 +0000

    Add package mmrotate/1.0.0rc0/mmrotate-1.0.0rc0-py3-none-any.whl

---
+++ packages/mmrotate/mmrotate-1.0.0rc0-py3-none-any.whl/mmrotate/datasets/__init__.py
@@ -0,0 +1,6 @@
+# Copyright (c) OpenMMLab. All rights reserved.
+from .dota import DOTADataset  # noqa: F401, F403
+from .hrsc import HRSCDataset  # noqa: F401, F403
+from .transforms import *  # noqa: F401, F403
+
+__all__ = ['DOTADataset', 'HRSCDataset']
---
+++ packages/mmrotate/mmrotate-1.0.0rc0-py3-none-any.whl/mmrotate/version.py
@@ -0,0 +1,4 @@
+# Copyright (c) OpenMMLab. All rights reserved.
+
+__version__ = '1.0.0rc0'
+short_version = __version__
