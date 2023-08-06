052d85bba7be76568983f89d1f8ef1ea7338556c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:20:48 +0000

    Add package neosctl/0.1.1/neosctl-0.1.1-py3-none-any.whl

---
+++ packages/neosctl/neosctl-0.1.1-py3-none-any.whl/neosctl/constant.py
@@ -0,0 +1,14 @@
+import pathlib
+from enum import Enum
+
+
+PROFILE_FILENAME = ".neosctl"
+PROFILE_FILEPATH = pathlib.Path().home() / PROFILE_FILENAME
+
+
+DEFAULT_PROFILE = "default"
+
+
+class AuthFlow(Enum):
+    basic = "basic"
+    keycloak = "keycloak"
