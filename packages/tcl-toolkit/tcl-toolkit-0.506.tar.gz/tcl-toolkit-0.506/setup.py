948194d60f2ef71cc456dce42714f8c862642c91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:30:18 +0000

    Add package tcl-toolkit/0.506/tcl-toolkit-0.506.tar.gz

---
+++ packages/tcl-toolkit/tcl-toolkit-0.506.tar.gz/tcl-toolkit-0.506/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+from tentacle import __version__
+
+
+with open('docs/README.md', 'r') as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name='tcl-toolkit',
+    version=__version__,
+    author='Ryan Simpson',
+    author_email='m3trik@outlook.com',
+    description='A Python3/PySide2 marking menu style toolkit for Maya, 3ds Max, and Blender.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/m3trik/tentacle',
+    packages=setuptools.find_packages(),
+    classifiers=(
+        'Programming Language :: Python :: 3',
+        # 'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ),
+    data_files=(
+        ('ui/0', ['ui/0/init.ui']),
+        ('ui/1', ['ui/1/cameras.ui', 'ui/1/editors.ui', 'ui/1/main.ui']),
+    ),
+)
