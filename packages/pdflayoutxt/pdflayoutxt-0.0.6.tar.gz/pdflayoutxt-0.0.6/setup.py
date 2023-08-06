96199c03de93f318424fe5795ef2e84d24b9b90c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:24:49 +0000

    Add package pdflayoutxt/0.0.6/pdflayoutxt-0.0.6.tar.gz

---
+++ packages/pdflayoutxt/pdflayoutxt-0.0.6.tar.gz/pdflayoutxt-0.0.6/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+setup(
+    name='pdflayoutxt',
+    version='0.0.6',
+    author='Sumeet Suman',
+    author_email='sumeetsuman83@gmail.com',
+    description='This library helps in extracting text from searchable pdf files by keeping the layout intact.',
+    keywords='pdftotext,pdflayout,setuptools',
+    python_requires='>=3.6, <4',
+    install_requires=['pdfplumber','cleantext','autocorrect'],
+    
+)
