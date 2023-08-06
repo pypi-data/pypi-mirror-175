a471013ba45e05a47e4242bfd9f4beedc74b6127
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:53:30 +0000

    Add package scikit-build/0.16.2/scikit-build-0.16.2.tar.gz

---
+++ packages/scikit-build/scikit-build-0.16.2.tar.gz/scikit-build-0.16.2/tests/test_logging.py
@@ -0,0 +1,31 @@
+from types import SimpleNamespace
+
+import pytest
+
+from skbuild.utils import distribution_hide_listing, distutils_log
+
+setuptools_logging = pytest.importorskip("setuptools.logging")
+
+
+def test_hide_listing(caplog):
+    setuptools_logging.configure()
+
+    distribution = SimpleNamespace()
+    distribution.hide_listing = True
+
+    with distribution_hide_listing(distribution):
+        distutils_log.info("This is hidden")
+
+    assert "This is hidden" not in caplog.text
+
+
+def test_no_hide_listing(caplog):
+    setuptools_logging.configure()
+
+    distribution = SimpleNamespace()
+    distribution.hide_listing = False
+
+    with distribution_hide_listing(distribution):
+        distutils_log.info("This is not hidden")
+
+    assert "This is not hidden" in caplog.text
