9a48944307e32d6d83522c5d0f81e89cc51f298c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:03 +0000

    Add package zigzag-dse/2.0.1/zigzag-dse-2.0.1.tar.gz

---
+++ packages/zigzag-dse/zigzag-dse-2.0.1.tar.gz/zigzag-dse-2.0.1/inputs/nokia/mapping/new/batched/8ex_mapping_4_core.py
@@ -0,0 +1,7 @@
+mapping = {
+    "default": {
+        "core_allocation": 1,
+        "spatial_mapping": {'D1': ('B', 32), 'D2': ('C', 32)},
+        "memory_operand_links": {'O': 'O', 'B': 'I2', 'A': 'I1'}
+    },
+}
