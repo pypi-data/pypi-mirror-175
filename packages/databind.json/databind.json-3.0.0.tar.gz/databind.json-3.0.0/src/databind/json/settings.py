93aea7bc290d8cec44c9543c595c1653a21fc0b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:26 +0000

    Add package databind.json/3.0.0/databind.json-3.0.0.tar.gz

---
+++ packages/databind.json/databind.json-3.0.0.tar.gz/databind.json-3.0.0/src/databind/json/settings.py
@@ -0,0 +1,43 @@
+import typing as t
+
+from databind.core.converter import Converter
+from databind.core.settings import ClassDecoratorSetting
+
+ConverterSupplier = t.Callable[[], Converter]
+
+
+class JsonConverter(ClassDecoratorSetting):
+    """Use this setting to decorate a class or to annotate a type hint to inform the JSON module to use the
+    specified convert when deserialize the type instead of any converter that would otherwise match the type.
+
+    Example:
+
+    ```py
+    from databind.core.converter import Converter
+    from databind.json.direction import Direction
+    from databind.json.settings import JsonConverter
+    from typing import Any
+
+    class MyCustomConverter(JsonConverter.Base):
+        def __init__(self, direction: Direction) -> None:
+            self.direction = direction
+        def convert(self, ctx: Context) -> Any:
+            ...
+
+    @JsonConverter(MyCustomConverter)
+    class MyCustomType:
+        ...
+
+    # Or
+
+    Annotated[MyCustomType, JsonConverter(MyCustomConverter)]
+    ```"""
+
+    supplier: ConverterSupplier
+
+    def __init__(self, supplier: t.Union[ConverterSupplier, Converter]) -> None:
+        super().__init__()
+        if isinstance(supplier, Converter):
+            self.supplier = lambda: t.cast(Converter, supplier)
+        else:
+            self.supplier = supplier
