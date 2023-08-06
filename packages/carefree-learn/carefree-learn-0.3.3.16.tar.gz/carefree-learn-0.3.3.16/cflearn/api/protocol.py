dc0ed83dabc49797d02c5f265cba8f8c1f938622
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:05:13 +0000

    Add package carefree-learn/0.3.3.16/carefree-learn-0.3.3.16.tar.gz

---
+++ packages/carefree-learn/carefree-learn-0.3.3.16.tar.gz/carefree-learn-0.3.3.16/cflearn/api/protocol.py
@@ -0,0 +1,26 @@
+import numpy as np
+
+from typing import List
+from typing import NamedTuple
+
+
+class ImageFolderLatentResponse(NamedTuple):
+    latent: np.ndarray
+    img_paths: List[str]
+
+
+class IImageExtractor:
+    def get_folder_latent(
+        self,
+        src_folder: str,
+        *,
+        batch_size: int,
+        num_workers: int = 0,
+        use_tqdm: bool = True,
+    ) -> ImageFolderLatentResponse:
+        pass
+
+
+__all__ = [
+    "IImageExtractor",
+]
