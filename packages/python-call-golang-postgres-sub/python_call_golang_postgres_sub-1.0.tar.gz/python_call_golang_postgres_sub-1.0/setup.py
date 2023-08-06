351ed689925d845af860fd0aa13cbf339de397aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:01:22 +0000

    Add package python-call-golang-postgres-sub/1.0/python_call_golang_postgres_sub-1.0.tar.gz

---
+++ packages/python-call-golang-postgres-sub/python_call_golang_postgres_sub-1.0.tar.gz/python_call_golang_postgres_sub-1.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="python_call_golang_postgres_sub",
+    version="1.0",
+    author="A-bian",
+    author_email="1079942574@qq.com",
+    keywords=['pg', 'sub', 'postgres'],
+    description="通过golang监听postgres的notify",
+    url="https://github.com/bzl19951110/python_call_golang_postgres_sub",
+    packages=find_packages()
+)
