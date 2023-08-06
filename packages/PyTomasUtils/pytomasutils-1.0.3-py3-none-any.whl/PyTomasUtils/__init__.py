8734f7e6464752de986f89ce81248ef33bd5d835
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:19:23 +0000

    Add package PyTomasUtils/1.0.3/pytomasutils-1.0.3-py3-none-any.whl

---
+++ packages/PyTomasUtils/pytomasutils-1.0.3-py3-none-any.whl/PyTomasUtils/__init__.py
@@ -0,0 +1,49 @@
+from functools import cache as optimize
+from time import perf_counter
+import sys
+
+def isfloat(x):
+    try:
+        float(x)
+        return True
+    except:
+        return False
+
+def isstring(x):
+    try:
+        str(x)
+        return True
+    except:
+        return False
+
+def isint(x):
+    try:
+        int(x)
+        return True
+    except:
+        return False
+
+def radice(x):
+    return x**.5
+
+
+def get_time(func):
+
+    def wrapper(*args, **kwargs):
+        start_time = perf_counter()
+
+        func(*args, *kwargs)
+
+        end_time = perf_counter()
+
+        total_time = round(end_time - start_time, 5)
+
+        print('Durata:', total_time, 'secondi.')
+
+    return wrapper
+
+@optimize
+def setup():
+    sys.setrecursionlimit(1000000000)
+    return "Setup Done."
+
