e61f9cb37c9c98942e74f0ad88a0ff5bff0c0229
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:40:10 +0000

    Add package semantha-streamlit-compare/0.1.2/semantha_streamlit_compare-0.1.2.tar.gz

---
+++ packages/semantha-streamlit-compare/semantha_streamlit_compare-0.1.2.tar.gz/semantha_streamlit_compare-0.1.2/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['semantha_streamlit_compare', 'semantha_streamlit_compare.components']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['requests>=2.28.1,<3.0.0', 'streamlit>=1.14.0,<2.0.0']
+
+setup_kwargs = {
+    'name': 'semantha-streamlit-compare',
+    'version': '0.1.2',
+    'description': 'This is the project for a streamlit component which uses semantha semantic compare.',
+    'long_description': "This is the project to build a demo application for semantha's capabilities with streamlit. ",
+    'author': 'thingsTHINKING GmbH',
+    'author_email': 'github@thingsthinking.net',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.10,<4.0',
+}
+
+
+setup(**setup_kwargs)
