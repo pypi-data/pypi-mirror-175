b4ef39b673ca6d3cea550b82a1d114931ad3f3c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:18:54 +0000

    Add package textbytesencoder/0.1.6/textbytesencoder-0.1.6-py3-none-any.whl

---
+++ packages/textbytesencoder/textbytesencoder-0.1.6-py3-none-any.whl/textbytesencoder/setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+project_urls = {
+    'Homepage': 'https://github.com/D1ffic00lt/textbytesencoder'
+}
+
+setuptools.setup(
+    long_description="""# textbytesencoder\nModule encoding and encrypting text by key""",
+    long_description_content_type='text/markdown',
+    project_urls=project_urls
+)
