f1e93cda6453b7ab81cc3d8df18dff1f0a1892cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:59 +0000

    Add package build-package-test/1/build-package-test-1.tar.gz

---
+++ packages/build-package-test/build-package-test-1.tar.gz/build-package-test-1/README.md
@@ -0,0 +1,1 @@
+Testing how to build python packages
---
+++ packages/build-package-test/build-package-test-1.tar.gz/build-package-test-1/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = '1'
+DESCRIPTION = 'Python package for the project'
+LONG_DESCRIPTION = 'Testing how to build python packages'
+
+# Setting up
+setup(
+    name="build-package-test",
+    version=VERSION,
+    author="Ntwanano Rikhotso",
+    author_email="sanele.rikhotso.99@gmail.com",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['python', 'test'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
