ead93f24616ca7f1b9808a5a8d309a9c3f4b15c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:25:15 +0000

    Add package Mensajes-OniNicole/5.0/Mensajes_OniNicole-5.0-py3-none-any.whl

---
+++ packages/Mensajes-OniNicole/Mensajes_OniNicole-5.0-py3-none-any.whl/mensajes/adios/despedidas.py
@@ -0,0 +1,8 @@
+# este es un modulo con funciones que despiden
+def despedir():
+	print("Adios, me estoy despidiendo desde la funcion despedirse del modulo despedidas")
+
+
+class Despedida():
+	def __init__(self):
+		print("Adios, me estoy despidiendo desde el init de la clase Despedida")
