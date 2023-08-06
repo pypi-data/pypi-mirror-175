b81482731f190617a8b00b3add0d38ea55e31438
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:10:02 +0000

    Add package gcb202a22/0.1/gcb202a22-0.1.tar.gz

---
+++ packages/gcb202a22/gcb202a22-0.1.tar.gz/gcb202a22-0.1/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+
+setuptools.setup(
+    name="gcb202a22",
+    version="0.1",
+    author="Francis B. Lavoie",
+    author_email="francis.b.lavoie@usherbrooke.ca",
+    description="Projet GCB202 A22",
+    long_description="Projet GCB202 A22",
+    long_description_content_type="text/markdown",
+    url="https://catalyseur.ca",
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+    ),
+)
