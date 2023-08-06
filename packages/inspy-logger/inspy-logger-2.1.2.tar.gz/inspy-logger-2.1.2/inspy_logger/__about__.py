0544ecc2d66760301e1cbe24d1d8ad98e644a154
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:12:50 +0000

    Add package inspy-logger/2.1.2/inspy-logger-2.1.2.tar.gz

---
+++ packages/inspy-logger/inspy-logger-2.1.2.tar.gz/inspy-logger-2.1.2/inspy_logger/__about__.py
@@ -0,0 +1,19 @@
+"""
+
+File: inspy_logger/__about__.py
+Project: inSPy-Logger
+Description: 
+
+Created: 11/3/22 - 21:05:06
+
+Since:
+    v2.1.1
+
+"""
+
+__prog__ = 'inSPy-Logger'
+__version__ = '2.1.2'
+__authors__ = [
+        ('Inspyre-Softworks', 'https://inspyre.tech'),
+        ('Taylor-Jayde Blackstone', '<t.blackstone@inspyre.tech')
+]
