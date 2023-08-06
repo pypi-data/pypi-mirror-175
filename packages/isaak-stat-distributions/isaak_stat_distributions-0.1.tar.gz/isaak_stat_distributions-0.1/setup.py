ffb05ef8b9bbb99f678fc32c899c83c18d703f27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:37 +0000

    Add package isaak-stat-distributions/0.1/isaak_stat_distributions-0.1.tar.gz

---
+++ packages/isaak-stat-distributions/isaak_stat_distributions-0.1.tar.gz/isaak_stat_distributions-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='isaak_stat_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['isaak_stat_distributions'],
+      author='Isaak Kamau',
+      author_email='isaak2018@students.uonbi.ac.ke',
+      author_tel='+254704205553',
+      zip_safe=False)
