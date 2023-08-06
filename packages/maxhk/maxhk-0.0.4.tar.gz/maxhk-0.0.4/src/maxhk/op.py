baeb7d2aa2ad39d374eab4779aed92a5e0b88084
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:52:04 +0000

    Add package maxhk/0.0.4/maxhk-0.0.4.tar.gz

---
+++ packages/maxhk/maxhk-0.0.4.tar.gz/maxhk-0.0.4/src/maxhk/op.py
@@ -0,0 +1,19 @@
+from collections import namedtuple
+from typing import List
+import json
+import shutil
+import subprocess as sp
+
+def __check_op() -> None:
+    if shutil.which('op') is None:
+        raise RuntimeError('`op` is not installed')
+
+
+def get_item(item_id: str, fields: List[str]) -> namedtuple:
+    __check_op()
+    ResultType = namedtuple('Item', fields)
+    csv_fields = ','.join(fields)
+    data = sp.getoutput(f'op item get {item_id} --format json --fields "{csv_fields}"')
+    return ResultType(*[x['value'] for x in json.loads(data)])
+        
+
