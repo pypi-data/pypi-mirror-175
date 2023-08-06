c56bb8703b440efa6d6b2710fa4b8cb8c3eb1397
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:01:18 +0000

    Add package garpix-user/3.0.1/garpix_user-3.0.1-py3-none-any.whl

---
+++ packages/garpix-user/garpix_user-3.0.1-py3-none-any.whl/garpix_user/serializers/registration_serializer.py
@@ -0,0 +1,132 @@
+from django.conf import settings
+from django.contrib.auth import get_user_model
+from django.db import transaction
+from garpix_utils.string import get_random_string
+from rest_framework import serializers
+
+from garpix_user.models.user_session import UserSession
+from django.utils.translation import ugettext as _
+
+User = get_user_model()
+
+
+class RegistrationSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+    password_2 = serializers.CharField(write_only=True)
+
+    def validate_password(self, value):
+
+        GARPIX_USER_SETTINGS = settings.GARPIX_USER
+
+        min_length = GARPIX_USER_SETTINGS.get('MIN_LENGTH_PASSWORD', 8)
+        min_digits = GARPIX_USER_SETTINGS.get('MIN_DIGITS_PASSWORD', 2)
+        min_chars = GARPIX_USER_SETTINGS.get('MIN_CHARS_PASSWORD', 2)
+        min_uppercase = GARPIX_USER_SETTINGS.get('MIN_UPPERCASE_PASSWORD', 1)
+
+        # check for min length
+        if len(value) < min_length:
+            raise serializers.ValidationError(
+                _('Password must be at least {min_length} characters long.'.format(min_length=min_length))
+            )
+
+        # check for min digits number
+        if sum(c.isdigit() for c in value) < min_digits:
+            raise serializers.ValidationError(
+                _('Password must container at least {min_digits} digits.'.format(min_digits=min_digits))
+            )
+
+        # check for min char number
+        if sum(c.isalpha() for c in value) < min_chars:
+            raise serializers.ValidationError(
+                _('Password must container at least {min_chars} chars.'.format(min_chars=min_chars))
+            )
+
+        # check for uppercase letter
+        if sum(c.isupper() for c in value) < min_uppercase:
+            raise serializers.ValidationError(
+                _('Password must container at least {min_uppercase} uppercase letter.'.format(
+                    min_uppercase=min_uppercase))
+            )
+
+        # check for password = password_2
+        if value != self.initial_data.get('password_2'):
+            raise serializers.ValidationError(_("Passwords do not match"))
+
+        return value
+
+    def validate_email(self, value):
+
+        GARPIX_USER_SETTINGS = settings.GARPIX_USER
+
+        request = self.context.get('request')
+
+        queryset = User.objects.filter(email=value, is_email_confirmed=True).first()
+        if queryset is not None:
+            raise serializers.ValidationError(_("This email is already in use"))
+
+        if GARPIX_USER_SETTINGS.get('USE_PREREGISTRATION_EMAIL_CONFIRMATION', False) and GARPIX_USER_SETTINGS.get(
+                'USE_EMAIL_CONFIRMATION', False):
+            user = UserSession.get_or_create_user_session(request)
+            if not user.is_email_confirmed:
+                raise serializers.ValidationError(_('Email was not confirmed'))
+
+        return value
+
+    def validate_phone(self, value):
+
+        GARPIX_USER_SETTINGS = settings.GARPIX_USER
+
+        request = self.context.get('request')
+
+        queryset = User.objects.filter(phone=value, is_phone_confirmed=True).first()
+        if queryset is not None:
+            raise serializers.ValidationError(_("This phone is already in use"))
+
+        if GARPIX_USER_SETTINGS.get('USE_PREREGISTRATION_PHONE_CONFIRMATION', False) and GARPIX_USER_SETTINGS.get(
+                'USE_PHONE_CONFIRMATION', False):
+            user = UserSession.get_or_create_user_session(request)
+            if not user.is_phone_confirmed:
+                raise serializers.ValidationError(_('Phone number was not confirmed'))
+
+        return value
+
+    def create(self, validated_data):
+
+        GARPIX_USER_SETTINGS = settings.GARPIX_USER
+
+        request = self.context.get('request', None)
+
+        validated_data.pop('password_2')
+
+        user_data = validated_data
+
+        if 'username' not in User.USERNAME_FIELDS and 'username' not in validated_data.keys():
+            user_data.update({'username': get_random_string(25)})
+
+        if GARPIX_USER_SETTINGS.get('USE_PHONE_CONFIRMATION', False) and not GARPIX_USER_SETTINGS.get('USE_PREREGISTRATION_PHONE_CONFIRMATION', False):
+            user_data.update({'is_phone_confirmed': False})
+
+        if GARPIX_USER_SETTINGS.get('USE_EMAIL_CONFIRMATION', False) and not GARPIX_USER_SETTINGS.get('USE_PREREGISTRATION_EMAIL_CONFIRMATION', False):
+            user_data.update({'is_email_confirmed': False})
+
+        with transaction.atomic():
+            user = User.objects.create_user(**user_data)
+            if request:
+                user_session = UserSession.get_or_create_user_session(request)
+                user_session.user = user
+                user_session.recognized = UserSession.UserState.REGISTERED
+                user_session.save()
+
+        return user
+
+    class Meta:
+        model = User
+        fields = ('password', 'password_2',)
+
+    def get_field_names(self, declared_fields, info):
+        expanded_fields = super().get_field_names(declared_fields, info)
+
+        if getattr(User, 'USERNAME_FIELDS', None):
+            return expanded_fields + User.USERNAME_FIELDS
+        else:
+            return expanded_fields
---
+++ packages/garpix-user/garpix_user-3.0.1-py3-none-any.whl/garpix_user/views/restore_password_view.py
@@ -0,0 +1,67 @@
+from drf_spectacular.utils import extend_schema
+from rest_framework import viewsets, status
+from rest_framework.decorators import action
+from rest_framework.exceptions import ValidationError
+from rest_framework.response import Response
+from django.utils.translation import ugettext as _
+from garpix_user.models import UserSession
+from garpix_user.serializers import RestorePasswordSerializer, UserSessionTokenSerializer
+from garpix_user.serializers.restore_passwrod_serializer import RestoreCheckCodeSerializer, RestoreSetPasswordSerializer
+from garpix_user.utils.drf_spectacular import user_session_token_header_parameter
+
+
+@extend_schema(
+    parameters=[
+        user_session_token_header_parameter()
+    ]
+)
+class RestorePasswordView(viewsets.ViewSet):
+
+    def get_serializer_class(self):
+        if self.action == 'send_code':
+            return RestorePasswordSerializer
+        if self.action == 'check_code':
+            return RestoreCheckCodeSerializer
+        return RestoreSetPasswordSerializer
+
+    @extend_schema(summary='Restore password. Step  1')
+    @action(methods=['POST'], detail=False)
+    def send_code(self, request, *args, **kwargs):
+        serializer = self.get_serializer_class()(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        user = UserSession.get_or_create_user_session(request, username=serializer.data['username'])
+
+        result, error = user.send_restore_code(username=serializer.data['username'])
+
+        if not result:
+            error.raise_exception(exception_class=ValidationError)
+        return Response(UserSessionTokenSerializer(user).data)
+
+    @extend_schema(summary='Restore password. Step  2')
+    @action(methods=['POST'], detail=False)
+    def check_code(self, request, *args, **kwargs):
+        user = UserSession.get_or_create_user_session(request)
+
+        serializer = self.get_serializer_class()(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        result, error = user.check_restore_code(
+            restore_password_confirm_code=serializer.data['restore_password_confirm_code'])
+        if not result:
+            error.raise_exception(exception_class=ValidationError)
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+    @extend_schema(summary='Restore password. Step 3')
+    @action(methods=['POST'], detail=False)
+    def set_password(self, request, *args, **kwargs):
+        user = UserSession.get_or_create_user_session(request)
+
+        serializer = self.get_serializer_class()(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        result, error = user.restore_password(field='username', new_password=serializer.data['new_password'])
+
+        if not result:
+            error.raise_exception(exception_class=ValidationError)
+        return Response({"result": _('Password was updated!')})
