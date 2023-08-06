c40f9030f6779087ebc404e9f958fa4f7ac53529
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:32 +0000

    Add package XTLLib/0.8/XTLLib-0.8.tar.gz

---
+++ packages/XTLLib/XTLLib-0.8.tar.gz/XTLLib-0.8/XTLLib/runqol.py
@@ -0,0 +1,4 @@
+def runqol(froms, choose):
+    if choose == "99": exit()
+    if froms != 0 and (choose == "h" or choose == "H"): helpe(froms)
+    else: print("No option named " + choose); sleep(3)
