d02e241081b3990a87b351fe547fb7a187de2b2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:10:39 +0000

    Add package httparse/0.1.1/httparse-0.1.1-cp37-abi3-win_amd64.whl

---
+++ packages/httparse/httparse-0.1.1-cp37-abi3-win_amd64.whl/httparse/__init__.py
@@ -0,0 +1,9 @@
+from __future__ import annotations
+
+from httparse._httparse import Header, ParsedRequest, RequestParser
+
+__all__ = (
+    "RequestParser",
+    "ParsedRequest",
+    "Header",
+)
