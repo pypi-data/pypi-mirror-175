cdb0bfa50e114475d1a2a6a174dbe4744b0e9f21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:54:58 +0000

    Add package otrobopt/0.11.post4/otrobopt-0.11.post4-cp39-cp39-win_amd64.whl

---
+++ packages/otrobopt/otrobopt-0.11.post4-cp39-cp39-win_amd64.whl/otrobopt/__init__.py
@@ -0,0 +1,18 @@
+"""
+    otrobopt --- An OpenTURNS module
+    ==================================
+
+    Contents
+    --------
+      'otrobopt' is a module for OpenTURNS
+
+"""
+
+import sys
+if sys.platform.startswith('win'):
+    # this ensures OT dll is loaded
+    import openturns
+
+from .otrobopt import *
+
+__version__ = '0.11.post4'
