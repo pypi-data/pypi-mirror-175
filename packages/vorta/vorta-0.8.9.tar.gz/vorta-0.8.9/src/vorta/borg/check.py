d0d27c8851f73e39bded0b424741c4f167363892
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:15:05 +0000

    Add package vorta/0.8.9/vorta-0.8.9.tar.gz

---
+++ packages/vorta/vorta-0.8.9.tar.gz/vorta-0.8.9/src/vorta/borg/check.py
@@ -0,0 +1,41 @@
+from typing import Any, Dict
+from .borg_job import BorgJob
+
+
+class BorgCheckJob(BorgJob):
+    def started_event(self):
+        self.app.backup_started_event.emit()
+        self.app.backup_progress_event.emit(self.tr('Starting consistency check…'))
+
+    def finished_event(self, result: Dict[str, Any]):
+        """
+        Process that the job terminated with the given results.
+
+        Parameters
+        ----------
+        result : Dict[str, Any]
+            The (json-like) dictionary containing the job results.
+        """
+        self.app.backup_finished_event.emit(result)
+        self.result.emit(result)
+        if result['returncode'] != 0:
+            self.app.backup_progress_event.emit(self.tr('Repo check failed. See logs for details.'))
+            self.app.check_failed_event.emit(result)
+        else:
+            self.app.backup_progress_event.emit(self.tr('Check completed.'))
+
+    @classmethod
+    def prepare(cls, profile):
+        ret = super().prepare(profile)
+        if not ret['ok']:
+            return ret
+        else:
+            ret['ok'] = False  # Set back to false, so we can do our own checks here.
+
+        cmd = ['borg', 'check', '--info', '--log-json']
+        cmd.append(f'{profile.repo.url}')
+
+        ret['ok'] = True
+        ret['cmd'] = cmd
+
+        return ret
