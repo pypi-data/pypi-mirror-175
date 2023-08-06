0dcea7673f43fb232c29710c8f228b8fa46492a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:00:21 +0000

    Add package f2ai/0.0.3/f2ai-0.0.3.tar.gz

---
+++ packages/f2ai/f2ai-0.0.3.tar.gz/f2ai-0.0.3/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+from pip._internal.req import parse_requirements
+from pip._internal.network.session import PipSession
+
+install_reqs = parse_requirements('requirements.txt', session=PipSession())
+
+setup(
+    name='f2ai',
+    version='0.0.3',
+    description='',
+    long_description='...',
+    url='https://github.com/ai-excelsior/F2AI',
+    long_description_content_type="text/x-rst",
+    author='',
+    license='MIT',
+    zip_safe=False,
+    packages=find_packages(),
+    install_requires=[str(ir.requirement) for ir in install_reqs],
+    # packages=['f2ai'],
+)
