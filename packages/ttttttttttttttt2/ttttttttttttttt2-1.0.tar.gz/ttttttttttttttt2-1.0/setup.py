0b61bce53a9a2ddffa6ce97e8c116c161ad68296
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:21:12 +0000

    Add package ttttttttttttttt2/1.0/ttttttttttttttt2-1.0.tar.gz

---
+++ packages/ttttttttttttttt2/ttttttttttttttt2-1.0.tar.gz/ttttttttttttttt2-1.0/setup.py
@@ -0,0 +1,10 @@
+
+# setup.py
+from distutils.core import setup, Extension
+
+module = Extension('zgx2', sources = ['zgx2.c'])
+
+setup(name = 'ttttttttttttttt2',
+          version = '1.0',
+          description = 'This is a demo package',
+          ext_modules = [module])
