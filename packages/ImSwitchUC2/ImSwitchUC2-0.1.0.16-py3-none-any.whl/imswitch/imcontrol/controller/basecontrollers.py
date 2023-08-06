2d62e3d0008276e42ef62013929cb6eb75fb0cb7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:18:35 +0000

    Add package ImSwitchUC2/0.1.0.16/ImSwitchUC2-0.1.0.16-py3-none-any.whl

---
+++ packages/ImSwitchUC2/ImSwitchUC2-0.1.0.16-py3-none-any.whl/imswitch/imcontrol/controller/basecontrollers.py
@@ -0,0 +1,110 @@
+from imswitch.imcommon.controller import WidgetController, WidgetControllerFactory
+from imswitch.imcontrol.model import InvalidChildClassError
+
+
+class ImConWidgetControllerFactory(WidgetControllerFactory):
+    """ Factory class for creating a ImConWidgetController object. """
+
+    def __init__(self, setupInfo, master, commChannel, moduleCommChannel):
+        super().__init__(setupInfo=setupInfo, master=master, commChannel=commChannel,
+                         moduleCommChannel=moduleCommChannel)
+
+
+class ImConWidgetController(WidgetController):
+    """ Superclass for all ImConWidgetController.
+    All WidgetControllers should have access to the setup information,
+    MasterController, CommunicationChannel and the linked Widget. """
+
+    def __init__(self, setupInfo, commChannel, master, *args, **kwargs):
+        # Protected attributes, which should only be accessed from controller and its subclasses
+        self._setupInfo = setupInfo
+        self._commChannel = commChannel
+        self._master = master
+
+        # Connect to broadcasting signal
+        self._commChannel.sigBroadcast.connect(lambda module, func, params: self._receive(module, func, params))
+
+        # Init superclass
+        super().__init__(*args, **kwargs)
+
+    def _receive(self, module, func, params):
+        if module == type(self).__name__:
+            func = eval("self."+func)
+            if hasattr(func, "_APIExport"):
+                if len(params)==0:
+                    # in case we have no parameters to be transfered
+                    return func()
+                else:
+                    try:
+                        return func(*params)
+                    except:
+                        # not iterable?
+                        return  func(params)
+                
+
+    def broadcast(self, module, func, params):
+        self._commChannel.sigBroadcast.emit(module, func, params)
+
+
+class LiveUpdatedController(ImConWidgetController):
+    """ Superclass for those controllers that will update the widgets with an
+    upcoming frame from the camera.  Should be either active or not, and have
+    an update function. """
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.active = False
+
+    def update(self, detectorName, im, init, isCurrentDetector):
+        pass #self.__ raise NotImplementedError
+
+
+class SuperScanController(ImConWidgetController):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # self._analogParameterDict = None
+        # self._digitalParameterDict = None
+        # Make non-overwritable functions
+        self.isValidScanController = self.__isValidScanController
+        self.isValidChild = self.isValidScanController
+
+    # @property
+    # def stageParameterList(self):
+    #     if self._analogParameterDict is None:
+    #         raise ValueError('Scan controller has no parameters defined')
+    #     else:
+    #         return [*self._analogParameterDict] #makes list of dict keys
+
+    # @property
+    # def TTLParameterList(self):
+    #     if self._digitalParameterDict is None:
+    #         raise ValueError('Scan controller has no parameters defined')
+    #     else:
+    #         return [*self._digitalParameterDict] #makes list of dict keys
+
+    @property
+    def parameterDict(self):
+        return None
+
+    def __isValidScanController(self):
+        if self.parameterDict is None:
+            raise InvalidChildClassError('ScanController needs to return a valid parameterDict')
+        else:
+            return True
+
+
+# Copyright (C) 2020-2021 ImSwitch developers
+# This file is part of ImSwitch.
+#
+# ImSwitch is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# ImSwitch is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
---
+++ packages/ImSwitchUC2/ImSwitchUC2-0.1.0.16-py3-none-any.whl/imswitch/imcontrol/model/__init__.py
@@ -0,0 +1,5 @@
+from .Options import Options
+from .SetupInfo import DeviceInfo, DetectorInfo, LaserInfo, PositionerInfo, ScanInfo, SetupInfo
+from .errors import *
+from .managers import *
+from .signaldesigners import SignalDesignerFactory
