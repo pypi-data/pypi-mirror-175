d0c9ecb048e3ac6831d61246ada88aee794f712c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:02:20 +0000

    Add package zaber-motion/2.15.0/zaber_motion-2.15.0.tar.gz

---
+++ packages/zaber-motion/zaber_motion-2.15.0.tar.gz/zaber_motion-2.15.0/zaber_motion/binary/reply_code.py
@@ -0,0 +1,18 @@
+﻿# ===== THIS FILE IS GENERATED FROM A TEMPLATE ===== #
+# ============== DO NOT EDIT DIRECTLY ============== #
+from enum import Enum
+
+
+class ReplyCode(Enum):
+    """
+    Named constants for all Zaber Binary protocol reply-only command codes.
+    """
+
+    MOVE_TRACKING = 8
+    LIMIT_ACTIVE = 9
+    MANUAL_MOVE_TRACKING = 10
+    MANUAL_MOVE = 11
+    SLIP_TRACKING = 12
+    UNEXPECTED_POSITION = 13
+    DIAGNOSTIC_TRACKING_REPLY = 240
+    ERROR = 255
