07d430718a911af89456f22242deb91a88e5173a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:09 +0000

    Add package hotlib/1.0.23/hotlib-1.0.23-py3-none-any.whl

---
+++ packages/hotlib/hotlib-1.0.23-py3-none-any.whl/hotlib/__init__.py
@@ -0,0 +1,8 @@
+# from .georeference import georeference
+from .inference import predict
+
+# from .postprocessing import vectorize
+
+# from .preprocessing import preprocess
+
+# from .stitch import stitch
