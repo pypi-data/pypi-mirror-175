3a10fee6ab5be7d4e86af641b60b30e842f04997
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:21:12 +0000

    Add package gym-sf/0.0.6/gym-sf-0.0.6.tar.gz

---
+++ packages/gym-sf/gym-sf-0.0.6.tar.gz/gym-sf-0.0.6/gym_sf/four_room/example.py
@@ -0,0 +1,23 @@
+import numpy as np
+import gym
+import gym_sf
+
+env = gym.make("four-room-v0", render_mode='rgb_array', new_step_api=True, max_episode_steps=5000, video=True)
+# env = gym.make("four-room-v0", render_mode='human', new_step_api=True, max_episode_steps=5000)
+terminated = False
+truncated = False
+env.reset()
+
+for _ in range(500):
+    action = env.action_space.sample()
+    if np.random.random() < 0.20:
+        if np.random.random() < 0.50:
+            action = 2
+        else:
+            action = 1
+    next_state, reward, terminated, truncated, _ = env.step(action)
+
+    if terminated or truncated:
+        env.render()
+        env.reset()
+env.close()
