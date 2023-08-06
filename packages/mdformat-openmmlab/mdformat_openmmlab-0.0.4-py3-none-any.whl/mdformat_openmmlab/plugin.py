dc117a40a69ef334500e1b889e83800ee9a4d1e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:33:56 +0000

    Add package mdformat-openmmlab/0.0.4/mdformat_openmmlab-0.0.4-py3-none-any.whl

---
+++ packages/mdformat-openmmlab/mdformat_openmmlab-0.0.4-py3-none-any.whl/mdformat_openmmlab/plugin.py
@@ -0,0 +1,71 @@
+import types
+from typing import Mapping
+
+from markdown_it import MarkdownIt
+from mdformat.renderer.typing import Postprocess, Render
+from mdit_py_plugins.tasklists import tasklists_plugin
+
+from .gfm import (
+    _escape_text,
+    _link_renderer,
+    _list_item_renderer,
+    _postprocess_inline,
+    _strikethrough_renderer,
+)
+from .table import _escape_tables, _render_cell, _render_table
+from .text import text
+
+
+def update_mdit(mdit: MarkdownIt) -> None:
+    """Update the parser"""
+    # Enable table rules
+    mdit.enable("table")
+
+    # Enable linkify-it-py (for GFM autolink extension)
+    mdit.options["linkify"] = True
+    mdit.enable("linkify")
+
+    # Enable strikethrough markdown-it extension
+    mdit.enable("strikethrough")
+
+    # Enable tasklist markdown-it extension
+    mdit.use(tasklists_plugin)
+
+    # Disable normalize link
+    def mock_normalize_link(self, url: str) -> str:
+        return url
+
+    mdit.normalizeLink = types.MethodType(mock_normalize_link, mdit)
+
+
+def add_cli_options(parser):
+    parser.add_argument(
+        "--table-width",
+        type=int,
+        help="The maximum width to pad in tables.",
+    )
+    parser.add_argument(
+        "--disable-escape",
+        action='append',
+        choices=[
+            'backslash', 'asterisk', 'underscore', 'link-enclosure',
+            'uri-enclosure', 'code-span'
+        ],
+        help="To disable the speicifed escape.",
+    )
+
+
+RENDERERS: Mapping[str, Render] = {
+    "table": _render_table,
+    "td": _render_cell,
+    "th": _render_cell,
+    "s": _strikethrough_renderer,
+    "list_item": _list_item_renderer,
+    "link": _link_renderer,
+    "text": text,
+}
+POSTPROCESSORS: Mapping[str, Postprocess] = {
+    "paragraph": _escape_tables,
+    "text": _escape_text,
+    "inline": _postprocess_inline,
+}
