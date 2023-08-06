fd7d2fa7ebd8ce0754b5b5b21d23fe2744b3ee89
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:38:04 +0000

    Add package tvpy/0.0.19/tvpy-0.0.19.tar.gz

---
+++ packages/tvpy/tvpy-0.0.19.tar.gz/tvpy-0.0.19/tvpy/tv_rename.py
@@ -0,0 +1,33 @@
+
+import re
+from pathlib import Path
+
+import PTN
+
+from tvpy.console import cls
+from tvpy.tv_tmdb import load_tvpy
+from tvpy.util import done, files_r
+
+
+def file_name(tvpy, s, e):
+    title = tvpy['name']
+    title = re.sub(r'[^a-zA-Z0-9]', ' ', title)
+    title = re.sub(r' +', '.', title)
+
+    return f'{title}.S{s:02}E{e:02}'
+
+
+def tv_rename(folder):
+    folder = Path(folder)
+    tvpy = load_tvpy(folder)
+    for file in files_r(folder):
+        try:
+            info = PTN.parse(file.name)
+            name = f'{file_name(tvpy, info["season"], info["episode"])}{file.suffix.lower()}'
+            if name != file.name:
+                file.rename(folder / name)
+                cls.print(f':keyboard: [success]{name}')
+        except:
+            pass
+
+    done()
