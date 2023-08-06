385ed7951996e95370f2111f49c6f58331f38952
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:25:12 +0000

    Add package tmdb-wrapper/0.2.0/tmdb_wrapper-0.2.0.tar.gz

---
+++ packages/tmdb-wrapper/tmdb_wrapper-0.2.0.tar.gz/tmdb_wrapper-0.2.0/tmdb_wrapper/utils/constants.py
@@ -0,0 +1,10 @@
+TMDB_URL = "https://api.themoviedb.org"
+TMDB_VERSION = "3"
+
+guest_session = "guest_session"
+user_session = "user_session"
+
+url_header_encoded = {'content-type': 'application/x-www-form-urlencoded'}
+
+
+route = 'tmdb_wrapper/utils/session_id.pkl'
