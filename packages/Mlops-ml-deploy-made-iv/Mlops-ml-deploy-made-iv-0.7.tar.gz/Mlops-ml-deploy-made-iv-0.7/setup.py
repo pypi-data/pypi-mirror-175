24ce2422fd5424d6f56e74856fd93ec2d8210c74
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:15:32 +0000

    Add package Mlops-ml-deploy-made-iv/0.7/Mlops-ml-deploy-made-iv-0.7.tar.gz

---
+++ packages/Mlops-ml-deploy-made-iv/Mlops-ml-deploy-made-iv-0.7.tar.gz/Mlops-ml-deploy-made-iv-0.7/setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+
+def readme():
+    """Import the README.md Markdown file and try to convert it to RST format."""
+    try:
+        import pypandoc
+        return pypandoc.convert('README.md', 'rst')
+    except(IOError, ImportError):
+        with open('README.md') as readme_file:
+            return readme_file.read()
+
+
+def required():
+    """Create list of requires to setup function"""
+    with open('requirements.txt') as file:
+        requires = file.read().splitlines()
+    return requires
+
+
+setup(
+    name='Mlops-ml-deploy-made-iv',
+    version='0.7',
+    description='Mlops-ml-deploy-made-iv',
+    long_description=readme(),
+    url='https://github.com/Z5-05/mlops_made_2022/tree/hw01',
+    author='iverendeev',
+    install_requires=required(),
+    author_email='ilaverendeev@gmail.com',
+    license='MIT',
+    packages=['src', 'tests'],
+)
