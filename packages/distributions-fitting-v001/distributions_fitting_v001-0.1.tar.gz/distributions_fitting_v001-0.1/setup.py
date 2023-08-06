8b9cb522273c75ff2dbf138ffac0028656d1d038
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:59 +0000

    Add package distributions-fitting-v001/0.1/distributions_fitting_v001-0.1.tar.gz

---
+++ packages/distributions-fitting-v001/distributions_fitting_v001-0.1.tar.gz/distributions_fitting_v001-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions_fitting_v001',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions_fitting_v001'],
+      zip_safe=False)
