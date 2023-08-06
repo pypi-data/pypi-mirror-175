18ad5ae44f8bab32ac2c3b0a056aeb00cc8834bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:20:39 +0000

    Add package textbytesencoder/0.1.3/textbytesencoder-0.1.3-py3-none-any.whl

---
+++ packages/textbytesencoder/textbytesencoder-0.1.3-py3-none-any.whl/textbytesencoder/__init__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+"""
+Python Encoder
+~~~~~~~~~~~~~~~~~~~
+
+A basic wrapper for the Discord API.
+
+:copyright: (c) 2022-present D1ffic00lt
+:license: MIT, see LICENSE for more details.
+
+"""
+
+from encoder.textbytesencoder.textbytesencoder import *
+
+__version__ = "1.0.0"
+__title__ = 'Encoder'
+__author__ = "D1ffic00lt"
+__copyright__ = "Copyright 2013-2022 {}".format(__author__)
