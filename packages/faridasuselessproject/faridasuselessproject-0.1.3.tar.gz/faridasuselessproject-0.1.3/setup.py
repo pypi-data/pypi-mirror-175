df08c1f06f16446747585854415232298d9149b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:47 +0000

    Add package faridasuselessproject/0.1.3/faridasuselessproject-0.1.3.tar.gz

---
+++ packages/faridasuselessproject/faridasuselessproject-0.1.3.tar.gz/faridasuselessproject-0.1.3/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+setup(
+    name='faridasuselessproject',
+    version='0.1.3',    
+    description='A example Python package',
+    url='',
+    author='Farida Shafik',
+    author_email='faridashafik@gmail.com',
+    license='Dont use this',
+    packages=['faridasuselessproject'],
+    install_requires=[
+                      'numpy',                     
+                      ],
+
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',  
+        'Operating System :: POSIX :: Linux',        
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+)
