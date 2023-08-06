77976ef50576688f61bc7b2b9bca299c457dcce2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:25:13 +0000

    Add package collibra-data-classification/2.0.0.post1/collibra-data-classification-2.0.0.post1.tar.gz

---
+++ packages/collibra-data-classification/collibra-data-classification-2.0.0.post1.tar.gz/collibra-data-classification-2.0.0.post1/collibra_data_classification/apis/paths/data_classification_classification_matches.py
@@ -0,0 +1,9 @@
+from collibra_data_classification.paths.data_classification_classification_matches.get import ApiForget
+from collibra_data_classification.paths.data_classification_classification_matches.post import ApiForpost
+
+
+class DataClassificationClassificationMatches(
+    ApiForget,
+    ApiForpost,
+):
+    pass
---
+++ packages/collibra-data-classification/collibra-data-classification-2.0.0.post1.tar.gz/collibra-data-classification-2.0.0.post1/collibra_data_classification/apis/paths/data_classification_classifications_classify.py
@@ -0,0 +1,7 @@
+from collibra_data_classification.paths.data_classification_classifications_classify.post import ApiForpost
+
+
+class DataClassificationClassificationsClassify(
+    ApiForpost,
+):
+    pass
