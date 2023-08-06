72d3690c2e79ff493006af86bc7c2e7b54a2dd5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:15:44 +0000

    Add package leventools/0.1.1/leventools-0.1.1.tar.gz

---
+++ packages/leventools/leventools-0.1.1.tar.gz/leventools-0.1.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    author='Levent Soykan',
+    description='Package of helper tools for data science and python',
+    name='leventools',
+    version='0.1.1',
+    packages=find_packages(include=['leventools', 'leventools.*']),
+    python_requires='>=3.6',
+    install_requires=[
+        'numpy>=1.23'
+        'pandas>=1.5'
+        'scikit-learn>=1.1'
+        'scipy>=1.9'
+    ]
+)
