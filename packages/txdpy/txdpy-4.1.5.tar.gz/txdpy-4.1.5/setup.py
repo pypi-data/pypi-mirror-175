83335d07d58b19a057429c04d3889bfa715dd5ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:04:01 +0000

    Add package txdpy/4.1.5/txdpy-4.1.5.tar.gz

---
+++ packages/txdpy/txdpy-4.1.5.tar.gz/txdpy-4.1.5/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+packages = ['txdpy']
+
+setup(name='txdpy',
+    version='4.1.5',
+    author='唐旭东',
+    install_requires=['mmh3','pymysql','loguru','redis','lxml','colorama','requests','colorama','lxpy'],
+    packages=packages,
+    package_dir={'requests': 'requests'})
