0e48e05c1b5f1f63041fe08bfa2167d81bc6ec3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:18:10 +0000

    Add package unexport/0.3.5/unexport-0.3.5.tar.gz

---
+++ packages/unexport/unexport-0.3.5.tar.gz/unexport-0.3.5/src/unexport/__main__.py
@@ -0,0 +1,4 @@
+from unexport.main import main
+
+if __name__ == "__main__":
+    raise SystemExit(main())
---
+++ packages/unexport/unexport-0.3.5.tar.gz/unexport-0.3.5/src/unexport/relate.py
@@ -0,0 +1,28 @@
+from __future__ import annotations
+
+import ast
+from typing import Iterator
+
+__all__ = ("first_occurrence", "get_parents", "relate")
+
+
+def relate(tree: ast.AST, parent: ast.AST | None = None) -> None:
+    tree.parent = parent  # type: ignore
+    for node in ast.walk(tree):
+        for child in ast.iter_child_nodes(node):
+            child.parent = node  # type: ignore
+
+
+def get_parents(node: ast.AST) -> Iterator[ast.AST]:
+    parent = node
+    while parent:
+        if parent := parent.parent:  # type: ignore
+            yield parent
+
+
+def first_occurrence(node: ast.AST, *ancestors):
+    for parent in get_parents(node):
+        if isinstance(parent, *ancestors):
+            return parent
+    else:
+        return False
