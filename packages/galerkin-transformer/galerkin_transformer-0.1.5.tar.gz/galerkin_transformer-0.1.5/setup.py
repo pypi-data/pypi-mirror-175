004a708a52fe319b2b2cb6522bae4923fe6e42a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:40:16 +0000

    Add package galerkin-transformer/0.1.5/galerkin_transformer-0.1.5.tar.gz

---
+++ packages/galerkin-transformer/galerkin_transformer-0.1.5.tar.gz/galerkin_transformer-0.1.5/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+
+setup(
+  name='galerkin_transformer',
+  packages=find_packages('galerkin_transformer'),
+  package_dir={'': 'galerkin_transformer'},
+  version = '0.1.5',
+  license='MIT',
+  description = 'Galerkin Transformer',
+  long_description='Galerkin Transformer: a linear attention without softmax',
+  long_description_content_type="text/markdown",
+  author = 'Shuhao Cao',
+  author_email = 'scao.math@gmail.com',
+  url = 'https://github.com/scaomath/galerkin-transformer',
+  keywords = ['transformers', 'attention', 'galerkin', 'hilbert', 'pde'],
+  install_requires=[
+      'seaborn',
+      'torchinfo',
+      'numpy',
+      'torch>=1.9.0',
+      'plotly',
+      'scipy',
+      'psutil',
+      'matplotlib',
+      'tqdm',
+      'PyYAML',
+  ],
+  classifiers=[
+      'Development Status :: 4 - Beta',
+      'Intended Audience :: Science/Research',
+      'Topic :: Scientific/Engineering :: Mathematics',
+      'License :: OSI Approved :: MIT License',
+      'Programming Language :: Python :: 3.8',
+  ],
+)
