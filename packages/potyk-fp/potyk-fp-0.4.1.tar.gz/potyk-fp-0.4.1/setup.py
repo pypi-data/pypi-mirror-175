f562ea32fda323f2e309206a2924ee0cc61ae15b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:14:10 +0000

    Add package potyk-fp/0.4.1/potyk-fp-0.4.1.tar.gz

---
+++ packages/potyk-fp/potyk-fp-0.4.1.tar.gz/potyk-fp-0.4.1/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['potyk_fp']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'potyk-fp',
+    'version': '0.4.1',
+    'description': '',
+    'long_description': None,
+    'author': 'potykion',
+    'author_email': 'potykion@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
