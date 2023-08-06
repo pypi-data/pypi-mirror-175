65db55cbec527d75b2b303aea1ad9c97c0d03921
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:06 +0000

    Add package baskref/0.0.3/baskref-0.0.3-py3-none-any.whl

---
+++ packages/baskref/baskref-0.0.3-py3-none-any.whl/baskref/utils.py
@@ -0,0 +1,17 @@
+"""
+Util function for the whole package.
+
+Author: Dominik Zulovec Sajovic, May 2022
+"""
+
+
+from datetime import datetime, date
+from argparse import ArgumentTypeError
+
+
+def valid_date(str_date: str) -> date:
+    """Validates if the passed string is a valid date"""
+    try:
+        return datetime.strptime(str_date, "%Y-%m-%d").date()
+    except (ValueError, TypeError) as exc:
+        raise ArgumentTypeError(f"not a valid date: {str_date!r}") from exc
