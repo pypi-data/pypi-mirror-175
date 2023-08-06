c448fb1d71b0170ef2e11ea0d429b350d47bc702
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:24 +0000

    Add package CV-Robot/0.4.0/CV_Robot-0.4.0-py3-none-any.whl

---
+++ packages/CV-Robot/CV_Robot-0.4.0-py3-none-any.whl/CV_Robot/robot.py
@@ -0,0 +1,67 @@
+import requests
+
+try:
+    # noinspection PyShadowingBuiltins
+    from printlog import printlog as print
+except ImportError:
+    pass
+
+from CV_Robot import is_Robot, robot_URL
+
+def forward():
+    """
+    Moves robot forward at speed set by speed command.
+
+    Runs emulated code if GPIO backend not found.
+    """
+    print("Driving forward...")
+    if is_Robot:
+        requests.get('http://' + robot_URL + '/forward')
+
+
+def backward():
+    """
+    Moves robot backward at speed set by speed command.
+
+    Runs emulated code if GPIO backend not found.
+    """
+    print("Driving backward...")
+    if is_Robot:
+        requests.get('http://' + robot_URL + '/backward')
+
+def left():
+    """
+    Turns robot left at speed set by speed command.
+
+    Runs emulated code if GPIO backend not found.
+    """
+    print("Turning left...")
+    if is_Robot:
+        requests.get('http://' + robot_URL + '/left')
+
+def right():
+    """
+    Turns robot right at speed set by speed command.
+
+    Runs emulated code if GPIO backend not found.
+    """
+    print("Turning right...")
+    if is_Robot:
+        requests.get('http://' + robot_URL + '/right')
+
+def stop():
+    """
+    Sets speed of all motors to 0.
+
+    Runs emulated code if GPIO backend not found.
+    """
+    print("Stopping robot...")
+    if is_Robot:
+        requests.get('http://' + robot_URL + '/stop')
+
+def speed(val):
+    """
+    Sets driving speed to value specified
+    """
+    if is_Robot:
+        requests.get('http://' + robot_URL + '/speed?val=' + str(val))
