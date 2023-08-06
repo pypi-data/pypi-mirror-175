a3174664b8b2f6b7391d4e353f1d58c278644a57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:13:50 +0000

    Add package text-processing-nlp-tools-tianran/0.0.2/text-processing-nlp-tools-tianran-0.0.2.tar.gz

---
+++ packages/text-processing-nlp-tools-tianran/text-processing-nlp-tools-tianran-0.0.2.tar.gz/text-processing-nlp-tools-tianran-0.0.2/text-processing-nlp-tools/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# @Time    : 11/5/22 3:57 PM
+# @Author  : LIANYONGXING
+# @FileName: __init__.py
+# @Software: PyCharm
+# @Repo    : https://github.com/lianyongxing/
+from text_preprocess import yanwenzi
+from text_simple_type import text_simple_type
+
+
+__all__ = ['yanwenzi', 'text_simple_type']
