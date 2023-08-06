2ec7d6f0f79390c0ff4a01af0cf4c7f213196b06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:40:34 +0000

    Add package remotemanager/0.2.2/remotemanager-0.2.2.tar.gz

---
+++ packages/remotemanager/remotemanager-0.2.2.tar.gz/remotemanager-0.2.2/remotemanager/transport/cp.py
@@ -0,0 +1,25 @@
+"""
+Handles file transfer via `cp`
+"""
+import logging
+
+from remotemanager.transport.transport import Transport
+
+
+class cp(Transport):
+    def __init__(self, *args, **kwargs):
+
+        super().__init__(*args, **kwargs)
+
+        self._logger.info('created new cp transport')
+
+        self._cmd = 'mkdir -p {secondary} ; ' \
+                    'cp -r --preserve {primary}{files} {secondary}'
+
+    @property
+    def cmd(self):
+        base = self._cmd.format(primary='{primary}',
+                                files='{files}',
+                                secondary='{secondary}')
+        self._logger.debug(f'returning semi-formatted cmd: "{base}"')
+        return base
