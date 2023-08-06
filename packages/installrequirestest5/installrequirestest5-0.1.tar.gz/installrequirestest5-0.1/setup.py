b432dc5438fba04bc417f17310a8dbecb7fe7a14
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:55 +0000

    Add package installrequirestest5/0.1/installrequirestest5-0.1.tar.gz

---
+++ packages/installrequirestest5/installrequirestest5-0.1.tar.gz/installrequirestest5-0.1/setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+import sys
+
+sadsadadsa()
+
+if sys.argv[1] == 'install':
+    abc()
+elif sys.argv[1] == 'build':
+    cba()
+    
+
+setup(
+  name = 'installrequirestest5',
+  packages = ['installrequirestest5'],
+  version = '0.1'
+)
