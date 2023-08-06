6cc826286dbf6f5361291f28d3648154ee9c81f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:14:09 +0000

    Add package ori-optimize/0.0.3/ori_optimize-0.0.3.tar.gz

---
+++ packages/ori-optimize/ori_optimize-0.0.3.tar.gz/ori_optimize-0.0.3/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="ori_optimize",
+    version="0.0.3",
+    install_requires=["gym==0.25.2"],
+    packages=["ori_optimize", "support"],
+)
