d093d9c5c09bdc7a40a5b29de11c9e3705735616
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:12:30 +0000

    Add package parcheck/0.0.1/parcheck-0.0.1.tar.gz

---
+++ packages/parcheck/parcheck-0.0.1.tar.gz/parcheck-0.0.1/setup.py
@@ -0,0 +1,12 @@
+import setuptools
+
+
+setuptools.setup(
+    name="parcheck",
+    version="0.0.1",
+    author="agwave",
+    author_email="agwave@foxmail.com",
+    description="check parameter",
+    url="https://github.com/Agwave/parcheck.git",
+    python_requires=">=3"
+)
