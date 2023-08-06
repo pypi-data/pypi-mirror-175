3829a30a3b237ab5bc36dab9dcdd80ee5a1b1d25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:15:36 +0000

    Add package proxsuite/0.2.5/proxsuite-0.2.5-0-cp39-cp39-macosx_12_0_x86_64.whl

---
+++ packages/proxsuite/proxsuite-0.2.5-0-cp39-cp39-macosx_12_0_x86_64.whl/cmeel.prefix/include/proxsuite/helpers/optional.hpp
@@ -0,0 +1,31 @@
+//
+// Copyright (c) 2022 INRIA
+//
+/**
+ * @file optional.hpp
+ */
+
+#ifndef PROXSUITE_HELPERS_OPTIONAL_HPP
+#define PROXSUITE_HELPERS_OPTIONAL_HPP
+
+#ifdef PROXSUITE_WITH_CPP_17
+#include <optional>
+#else
+#include <proxsuite/helpers/tl-optional.hpp>
+#endif
+
+namespace proxsuite {
+#ifdef PROXSUITE_WITH_CPP_17
+template<class T>
+using optional = std::optional<T>;
+using nullopt_t = std::nullopt_t;
+inline constexpr nullopt_t nullopt = std::nullopt;
+#else
+template<class T>
+using optional = tl::optional<T>;
+using nullopt_t = tl::nullopt_t;
+inline constexpr nullopt_t nullopt = tl::nullopt;
+#endif
+} // namespace proxsuite
+
+#endif /* end of include guard PROXSUITE_HELPERS_OPTIONAL_HPP */
