86a72afa6d209627540b256ddcafefb1300eacec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:51:50 +0000

    Add package tyba-cvxpy/1.3.0/tyba_cvxpy-1.3.0-cp38-cp38-manylinux_2_17_aarch64.manylinux2014_aarch64.whl

---
+++ packages/tyba-cvxpy/tyba_cvxpy-1.3.0-cp38-cp38-manylinux_2_17_aarch64.manylinux2014_aarch64.whl/cvxpy/version.py
@@ -0,0 +1,10 @@
+
+# THIS FILE IS GENERATED FROM CVXPY SETUP.PY
+short_version = '1.3.0'
+version = '1.3.0'
+full_version = '1.3.0.dev0+442.643f5e1'
+git_revision = '643f5e1'
+commit_count = '442'
+release = False
+if not release:
+    version = full_version
