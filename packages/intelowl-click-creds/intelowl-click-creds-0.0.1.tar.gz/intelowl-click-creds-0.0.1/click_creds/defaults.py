9eefcad9acfbc1ae2ca8074b40be89bda66e813b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:13:21 +0000

    Add package intelowl-click-creds/0.0.1/intelowl-click-creds-0.0.1.tar.gz

---
+++ packages/intelowl-click-creds/intelowl-click-creds-0.0.1.tar.gz/intelowl-click-creds-0.0.1/click_creds/defaults.py
@@ -0,0 +1,9 @@
+DEFAULT_MAPPING = {
+    "login": "login",
+    "password": "password",
+    "account": "account",
+    "http_proxy": "http_proxy",
+    "https_proxy": "https_proxy"
+}
+
+MODULE_KEY = "click_creds.classes.ClickCreds"
