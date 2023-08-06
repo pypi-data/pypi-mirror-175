3155c3a77a1850cc511bc6450bb735cb042cffb1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:59:46 +0000

    Add package easyclient/0.1.1/easyclient-0.1.1.tar.gz

---
+++ packages/easyclient/easyclient-0.1.1.tar.gz/easyclient-0.1.1/easyclient/__init__.py
@@ -0,0 +1,11 @@
+import easyclient.parameters as parameters
+import easyclient.utils as utils
+from easyclient.client.base import (
+    ApiAuthNone,
+    ApiAuthKey,
+    ApiAuthOAuth2,
+)
+from easyclient.client.rest import (
+    RestApiClient,
+    AsyncRestApiClient,
+)
