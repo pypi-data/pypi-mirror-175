4a3018db10927c79b1c3d71700f6b454e96af439
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:53:36 +0000

    Add package modern-treasury/0.4.0/modern_treasury-0.4.0-py3-none-any.whl

---
+++ packages/modern-treasury/modern_treasury-0.4.0-py3-none-any.whl/modern_treasury/resources/validations.py
@@ -0,0 +1,62 @@
+# File generated from our OpenAPI spec by Stainless.
+
+from __future__ import annotations
+
+from typing import Union
+
+from .._types import NOT_GIVEN, Headers, Timeout, NotGiven
+from .._utils import maybe_transform
+from .._resource import SyncAPIResource, AsyncAPIResource
+from .._base_client import make_request_options
+from ..types.routing_number_lookup_request import RoutingNumberLookupRequest
+from ..types.validation_validate_routing_number_params import (
+    ValidationValidateRoutingNumberParams,
+)
+
+__all__ = ["Validations", "AsyncValidations"]
+
+
+class Validations(SyncAPIResource):
+    def validate_routing_number(
+        self,
+        query: ValidationValidateRoutingNumberParams,
+        *,
+        headers: Union[Headers, NotGiven] = NOT_GIVEN,
+        max_retries: Union[int, NotGiven] = NOT_GIVEN,
+        timeout: Union[float, Timeout, None, NotGiven] = NOT_GIVEN,
+    ) -> RoutingNumberLookupRequest:
+        """
+        Validates the routing number information supplied without creating a routing
+        detail
+        """
+        options = make_request_options(
+            headers, max_retries, timeout, maybe_transform(query, ValidationValidateRoutingNumberParams)
+        )
+        return self._get(
+            "/api/validations/routing_numbers",
+            options=options,
+            cast_to=RoutingNumberLookupRequest,
+        )
+
+
+class AsyncValidations(AsyncAPIResource):
+    async def validate_routing_number(
+        self,
+        query: ValidationValidateRoutingNumberParams,
+        *,
+        headers: Union[Headers, NotGiven] = NOT_GIVEN,
+        max_retries: Union[int, NotGiven] = NOT_GIVEN,
+        timeout: Union[float, Timeout, None, NotGiven] = NOT_GIVEN,
+    ) -> RoutingNumberLookupRequest:
+        """
+        Validates the routing number information supplied without creating a routing
+        detail
+        """
+        options = make_request_options(
+            headers, max_retries, timeout, maybe_transform(query, ValidationValidateRoutingNumberParams)
+        )
+        return await self._get(
+            "/api/validations/routing_numbers",
+            options=options,
+            cast_to=RoutingNumberLookupRequest,
+        )
