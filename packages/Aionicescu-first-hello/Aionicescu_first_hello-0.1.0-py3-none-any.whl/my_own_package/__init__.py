f469ecb02b9f7374a44e92dbd61323f877e519f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:22:13 +0000

    Add package Aionicescu-first-hello/0.1.0/Aionicescu_first_hello-0.1.0-py3-none-any.whl

---
+++ packages/Aionicescu-first-hello/Aionicescu_first_hello-0.1.0-py3-none-any.whl/my_own_package/__init__.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='Isabella-first-hello',
+    version='0.1.0',
+    author='AlexandruIonicescu',
+    author_email='ysss_a@yahoo.com',
+    packages=['my_own_package'],
+    package_dir={"":"src\\"},
+    include_package_data=True
+)
