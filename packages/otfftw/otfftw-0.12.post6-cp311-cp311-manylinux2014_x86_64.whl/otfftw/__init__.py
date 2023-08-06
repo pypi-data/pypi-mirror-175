26931a9b353e3ed715a1f0e90c08941a0259bef0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:19:13 +0000

    Add package otfftw/0.12.post6/otfftw-0.12.post6-cp311-cp311-manylinux2014_x86_64.whl

---
+++ packages/otfftw/otfftw-0.12.post6-cp311-cp311-manylinux2014_x86_64.whl/otfftw/__init__.py
@@ -0,0 +1,18 @@
+"""
+    otfftw --- An OpenTURNS module
+    ===================================================
+
+    Contents
+    --------
+      'otfftw' is python module for OpenTURNS
+
+"""
+
+import sys
+if sys.platform.startswith('win'):
+    # this ensures OT dll is loaded
+    import openturns
+
+from .otfftw import *
+
+__version__ = '0.12.post6'
