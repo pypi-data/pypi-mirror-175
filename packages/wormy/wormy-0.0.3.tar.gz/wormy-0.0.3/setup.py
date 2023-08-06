6526406d6e9fe126f74357d4ed0b6cd20888f1de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:16:51 +0000

    Add package wormy/0.0.3/wormy-0.0.3.tar.gz

---
+++ packages/wormy/wormy-0.0.3.tar.gz/wormy-0.0.3/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+classifiers = [
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Education',
+    'Operating System :: Microsoft :: Windows :: Windows 10',
+    'Programming Language :: Python',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3'
+]
+
+setup(
+    name='wormy',
+    version='0.0.3',
+    description='Basic Python module made by a middle schoool student for fun',
+    long_description=open('README.md').read() + '\n\n' + open('CHANGELOG.txt').read(),
+    url='',
+    author='Sahil',
+    author_email='smartluminous213@gmail.com',
+    license='MIT',
+    classifiers=classifiers,
+    keywords='',
+    packages=['wormy'],
+    install_requires=['']
+)
