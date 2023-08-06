5e0df15c831800a646b33073bc91ef93e92dbb42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:58:52 +0000

    Add package dlcm-api/2.1.1/dlcm_api-2.1.1-py3-none-any.whl

---
+++ packages/dlcm-api/dlcm_api-2.1.1-py3-none-any.whl/dlcmapi_client/api/__init__.py
@@ -0,0 +1,15 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from dlcmapi_client.api.access_api import AccessApi
+from dlcmapi_client.api.admin_api import AdminApi
+from dlcmapi_client.api.archival_storage_api import ArchivalStorageApi
+from dlcmapi_client.api.data_mgmt_api import DataMgmtApi
+from dlcmapi_client.api.dlcm_api import DlcmApi
+from dlcmapi_client.api.index_api import IndexApi
+from dlcmapi_client.api.ingest_api import IngestApi
+from dlcmapi_client.api.oai_info_api import OaiInfoApi
+from dlcmapi_client.api.preingest_api import PreingestApi
+from dlcmapi_client.api.preservation_planning_api import PreservationPlanningApi
