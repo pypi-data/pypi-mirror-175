4e7cd1c86a052877c4fe89cf256e38a0b95d051c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:21:13 +0000

    Add package fcxai-alexandria/0.2.2/fcxai_alexandria-0.2.2-py3-none-any.whl

---
+++ packages/fcxai-alexandria/fcxai_alexandria-0.2.2-py3-none-any.whl/alexandria/galib/__init__.py
@@ -0,0 +1,11 @@
+from alexandria.galib.data import ga4_report
+from alexandria.galib.data import ga4_to_df
+from alexandria.galib.data import ua_report
+from alexandria.galib.data import ua_to_df
+
+__all__ = [
+    "ga4_report",
+    "ga4_to_df",
+    "ua_report",
+    "ua_to_df",
+]
