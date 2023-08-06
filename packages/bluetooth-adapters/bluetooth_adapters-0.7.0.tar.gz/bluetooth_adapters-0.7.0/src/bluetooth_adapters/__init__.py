23bc8071c0380ce7e34c3f7a90c02986b46402c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:55 +0000

    Add package bluetooth-adapters/0.7.0/bluetooth_adapters-0.7.0.tar.gz

---
+++ packages/bluetooth-adapters/bluetooth_adapters-0.7.0.tar.gz/bluetooth_adapters-0.7.0/src/bluetooth_adapters/__init__.py
@@ -0,0 +1,46 @@
+__version__ = "0.7.0"
+
+from typing import Any
+
+from .dbus import (
+    _adapters_from_managed_objects,
+    get_bluetooth_adapter_details,
+    get_bluetooth_adapters,
+    get_dbus_managed_objects,
+)
+from .history import AdvertisementHistory, load_history_from_managed_objects
+
+__all__ = [
+    "AdvertisementHistory",
+    "BlueZDBusObjects",
+    "get_bluetooth_adapters",
+    "get_bluetooth_adapter_details",
+    "get_dbus_managed_objects",
+]
+
+
+class BlueZDBusObjects:
+    """Fetch and parse BlueZObjects."""
+
+    def __init__(self) -> None:
+        """Init the manager."""
+        self._managed_objects: dict[str, Any] = {}
+
+    async def load(self) -> None:
+        """Load from the bus."""
+        self._managed_objects = await get_dbus_managed_objects()
+
+    @property
+    def adapters(self) -> list[str]:
+        """Get adapters."""
+        return list(self.adapter_details)
+
+    @property
+    def adapter_details(self) -> dict[str, dict[str, Any]]:
+        """Get adapters."""
+        return _adapters_from_managed_objects(self._managed_objects)
+
+    @property
+    def history(self) -> dict[str, AdvertisementHistory]:
+        """Get history from managed objects."""
+        return load_history_from_managed_objects(self._managed_objects)
