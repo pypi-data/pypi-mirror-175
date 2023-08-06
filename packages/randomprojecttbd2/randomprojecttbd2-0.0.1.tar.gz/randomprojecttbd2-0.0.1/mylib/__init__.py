6ff87ffa06ebb90607d056a5f0922205cb9eb828
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:29:18 +0000

    Add package randomprojecttbd2/0.0.1/randomprojecttbd2-0.0.1.tar.gz

---
+++ packages/randomprojecttbd2/randomprojecttbd2-0.0.1.tar.gz/randomprojecttbd2-0.0.1/mylib/__init__.py
@@ -0,0 +1,1 @@
+from main_prog import examp
---
+++ packages/randomprojecttbd2/randomprojecttbd2-0.0.1.tar.gz/randomprojecttbd2-0.0.1/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+# here = os.path.abspath(os.path.dirname(__file__))
+
+# with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+#     long_description = "\n" + fh.read()
+
+VERSION = '0.0.1'
+DESCRIPTION = 'Learning to make a py package'
+LONG_DESCRIPTION = 'A package that allows to build nothing.'
+
+# Setting up
+setup(
+    name="randomprojecttbd2",
+    version=VERSION,
+    author="Noone",
+    author_email="<mail@noone.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description = LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=['numpy', 'scipy', 'tqdm'],
+    keywords=['python', 'terms-related-to-project'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
