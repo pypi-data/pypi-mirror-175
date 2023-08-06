88c5740e80d8f27533f429d254cad104af98d9e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:11:45 +0000

    Add package itables/1.3.2/itables-1.3.2.tar.gz

---
+++ packages/itables/itables-1.3.2.tar.gz/itables-1.3.2/itables/utils.py
@@ -0,0 +1,13 @@
+import os
+
+
+def find_package_file(*path):
+    """Return the full path to a file from the itables package"""
+    current_path = os.path.dirname(__file__)
+    return os.path.join(current_path, *path)
+
+
+def read_package_file(*path):
+    """Return the content of a file from the itables package"""
+    with open(find_package_file(*path), encoding="utf-8") as fp:
+        return fp.read()
