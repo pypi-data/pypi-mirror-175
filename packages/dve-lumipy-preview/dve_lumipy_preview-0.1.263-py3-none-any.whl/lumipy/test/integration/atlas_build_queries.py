3bbc4d5800eb35a082b9c10a76c9edbcdf02482c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:24:21 +0000

    Add package dve-lumipy-preview/0.1.263/dve_lumipy_preview-0.1.263-py3-none-any.whl

---
+++ packages/dve-lumipy-preview/dve_lumipy_preview-0.1.263-py3-none-any.whl/lumipy/test/integration/atlas_build_queries.py
@@ -0,0 +1,22 @@
+import pandas as pd
+
+from lumipy.atlas.utility_functions import (
+    _query_data_provider_metadata,
+    _query_direct_provider_metadata
+)
+from lumipy.test.test_infra import BaseIntTest
+
+
+class AtlasBuildQueryTests(BaseIntTest):
+
+    def test_client_atlas_query_data_providers_metadata(self):
+        df = _query_data_provider_metadata(self.client)
+        self.assertIsInstance(df, pd.DataFrame)
+        self.assertGreater(df.shape[0], 0)
+        self.assertEqual(df.shape[1], 14)
+
+    def test_client_atlas_query_direct_providers_metadata(self):
+        df = _query_direct_provider_metadata(self.client)
+        self.assertIsInstance(df, pd.DataFrame)
+        self.assertGreater(df.shape[0], 0)
+        self.assertEqual(df.shape[1], 10)
