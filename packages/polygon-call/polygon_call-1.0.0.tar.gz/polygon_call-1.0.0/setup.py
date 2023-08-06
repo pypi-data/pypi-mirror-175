c7877e7625732bb1c8ac48313439880ca3ca791a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:11:11 +0000

    Add package polygon-call/1.0.0/polygon_call-1.0.0.tar.gz

---
+++ packages/polygon-call/polygon_call-1.0.0.tar.gz/polygon_call-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='polygon_call',
+    version='1.0.0',
+    packages=[''],
+    url='',
+    license='',
+    author='Yuqi Tang',
+    author_email='yt2101@nyu.edu',
+    description='Repeatedly calls the polygon api every 1 seconds for 24 hours'
+)
