6d704a9c599b1eb6b08f89e2c7a86948ccd11f4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:28:04 +0000

    Add package beaker-py/1.11.4/beaker-py-1.11.4.tar.gz

---
+++ packages/beaker-py/beaker-py-1.11.4.tar.gz/beaker-py-1.11.4/beaker/data_model/account.py
@@ -0,0 +1,13 @@
+from typing import Optional
+
+from .base import BaseModel
+
+__all__ = ["Account"]
+
+
+class Account(BaseModel):
+    id: str
+    name: str
+    display_name: str
+    institution: Optional[str] = None
+    pronouns: Optional[str] = None
