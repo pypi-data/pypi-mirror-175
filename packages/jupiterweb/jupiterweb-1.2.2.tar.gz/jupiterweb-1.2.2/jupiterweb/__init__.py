297f7d20300b74a1bf3236fcf0ed03487e3eb058
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:28 +0000

    Add package jupiterweb/1.2.2/jupiterweb-1.2.2.tar.gz

---
+++ packages/jupiterweb/jupiterweb-1.2.2.tar.gz/jupiterweb-1.2.2/jupiterweb/__init__.py
@@ -0,0 +1,10 @@
+"""
+Criado por Érick Ghuron
+
+Jupiterweb API
+"""
+
+from .api import buscar_disciplina_por_codigo, buscar_disciplina_por_nome
+
+__author__ = 'Erick Ghuron'
+__version__ = '1.2.2'
---
+++ packages/jupiterweb/jupiterweb-1.2.2.tar.gz/jupiterweb-1.2.2/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+import jupiterweb as jup
+
+long_description = open('README.md', encoding='utf8').read()
+
+setup(
+    name = 'jupiterweb',
+    version = jup.__version__,
+    author = jup.__author__,
+    author_email = 'ghuron@usp.br',
+    packages = ['jupiterweb'],
+    description = 'Um scraper de disciplinas do jupiterweb',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url = 'https://github.com/ghurone/jupiterweb-scraper',
+    project_urls = {
+        'Código fonte': 'https://github.com/ghurone/jupiterweb-scraper',
+    },
+    install_requires=['beautifulsoup4>=4.10.0', 'lxml>=4.6.3', 'requests>=2.26.0'],
+    license = 'MIT',
+    keywords = 'jupiterweb disciplinas usp ghuron',
+    classifiers = [
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: Portuguese (Brazilian)',
+        'Operating System :: OS Independent'
+    ]
+)
