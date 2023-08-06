cd9dafdf4dcedaafa8bb911fed337a6952d60487
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:04 +0000

    Add package oleg/0.1.1/oleg-0.1.1.tar.gz

---
+++ packages/oleg/oleg-0.1.1.tar.gz/oleg-0.1.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='oleg',
+      version='0.1.1',
+      description='oleg',
+      packages=['oleg'],
+      author_email='aleksandrparnuk7@gmail.com',
+      zip_safe=False)
