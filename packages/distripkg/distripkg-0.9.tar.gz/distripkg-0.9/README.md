dd9ccee43a9c8879f4d8698761aaf3634ca79ab2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:33 +0000

    Add package distripkg/0.9/distripkg-0.9.tar.gz

---
+++ packages/distripkg/distripkg-0.9.tar.gz/distripkg-0.9/README.md
@@ -0,0 +1,9 @@
+# dand-distributions package
+
+Summary of the package
+
+# Files
+
+Explanation of files in the package
+
+# Installation
---
+++ packages/distripkg/distripkg-0.9.tar.gz/distripkg-0.9/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='distripkg',
+      version='0.9',
+      description='Gaussian and Binomial distributions from udacity',
+      packages=['distripkg'],
+      author = 'Amos Omofaiye',
+      author_email = 'shema.omofaye@gmail.com',
+      zip_safe=False)
