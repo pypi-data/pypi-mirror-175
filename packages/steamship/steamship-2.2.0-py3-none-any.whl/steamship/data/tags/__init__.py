827b9c90071cc9fbf7c257e782c47e15edaad8bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:52:21 +0000

    Add package steamship/2.2.0/steamship-2.2.0-py3-none-any.whl

---
+++ packages/steamship/steamship-2.2.0-py3-none-any.whl/steamship/data/tags/__init__.py
@@ -0,0 +1,4 @@
+from .tag import Tag
+from .tag_constants import DocTag, EmotionTag, SentimentTag, TagKind, TagValue, TokenTag
+
+__all__ = ["DocTag", "EmotionTag", "SentimentTag", "Tag", "TagKind", "TokenTag", "TagValue"]
