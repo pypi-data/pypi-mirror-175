ab7e79fc6e6586be91391c7fc77de75c7f7bf931
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:25:02 +0000

    Add package collibra-importer/2.0.0.post2/collibra-importer-2.0.0.post2.tar.gz

---
+++ packages/collibra-importer/collibra-importer-2.0.0.post2.tar.gz/collibra-importer-2.0.0.post2/collibra_importer/apis/paths/import_synchronize_synchronization_id_evict.py
@@ -0,0 +1,7 @@
+from collibra_importer.paths.import_synchronize_synchronization_id_evict.delete import ApiFordelete
+
+
+class ImportSynchronizeSynchronizationIdEvict(
+    ApiFordelete,
+):
+    pass
---
+++ packages/collibra-importer/collibra-importer-2.0.0.post2.tar.gz/collibra-importer-2.0.0.post2/collibra_importer/paths/import_results_job_id_summary/__init__.py
@@ -0,0 +1,7 @@
+# do not import all endpoints into this module because that uses a lot of memory and stack frames
+# if you need the ability to import all endpoints from this module, import them with
+# from collibra_importer.paths.import_results_job_id_summary import Api
+
+from collibra_importer.paths import PathValues
+
+path = PathValues.IMPORT_RESULTS_JOB_ID_SUMMARY
