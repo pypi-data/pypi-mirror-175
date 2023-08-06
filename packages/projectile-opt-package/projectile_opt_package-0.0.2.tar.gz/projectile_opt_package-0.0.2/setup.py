e24e01f1ccfb9a8365bb17b96e77a3d873af5016
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:21:00 +0000

    Add package projectile-opt-package/0.0.2/projectile_opt_package-0.0.2.tar.gz

---
+++ packages/projectile-opt-package/projectile_opt_package-0.0.2.tar.gz/projectile_opt_package-0.0.2/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='projectile_opt_package',
+      version='0.0.2',
+      description='Special CAD Library',
+      packages=['projectile_opt_package'],
+      include_dirs=['projectile_opt_package/compiled'],
+      author_email='denisdoronichev86@mail.ru',
+      zip_safe=False
+      )
