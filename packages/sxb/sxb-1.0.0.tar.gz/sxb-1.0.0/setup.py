0862be8a69d734eff84f69d4f555de772671fcf8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:18 +0000

    Add package sxb/1.0.0/sxb-1.0.0.tar.gz

---
+++ packages/sxb/sxb-1.0.0.tar.gz/sxb-1.0.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup 
+
+setup(name='sxb',
+      version='1.0.0',
+      description='sxb stuff',
+      packages=['sxb'],
+      zip_safe=False)
+
---
+++ packages/sxb/sxb-1.0.0.tar.gz/sxb-1.0.0/sxb/utils.py
@@ -0,0 +1,4 @@
+def show_version():
+    print("this is the show_version function inside sxb/utils.py")
+    print("version 1.0.0")
+    
