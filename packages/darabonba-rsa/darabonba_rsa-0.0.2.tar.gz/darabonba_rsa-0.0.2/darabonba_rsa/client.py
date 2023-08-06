f48dc86de4f0196730a7cfa378a7231557f2442b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:29:35 +0000

    Add package darabonba-rsa/0.0.2/darabonba_rsa-0.0.2.tar.gz

---
+++ packages/darabonba-rsa/darabonba_rsa-0.0.2.tar.gz/darabonba_rsa-0.0.2/darabonba_rsa/client.py
@@ -0,0 +1,37 @@
+from Crypto.PublicKey import RSA
+from Crypto.Cipher import PKCS1_OAEP
+
+
+class Key:
+    def __init__(self, private_key, public_key):
+        self.private_key = private_key
+        self.public_key = public_key
+
+
+class Client:
+
+    @classmethod
+    def generate(cls, size: int) -> KeyObject:
+        """
+        生成指定长度的秘钥
+        :param size:
+        :return:
+        """
+        key = RSA.generate(size)
+        private_key = key.export_key()
+        public_key = key.publickey().export_key()
+        return Key(private_key, public_key)
+
+    @classmethod
+    def encrypt(cls, public_key: str, data: str) -> bytes:
+        public_key = RSA.import_key(public_key)
+        cipher = PKCS1_OAEP.new(public_key)
+        data = cipher.encrypt(data.encode())
+        return data
+
+    @classmethod
+    def decrypt(cls, private_key: str, data: bytes) -> bytes:
+        privateKey = RSA.import_key(private_key)
+        cipher = PKCS1_OAEP.new(privateKey)
+        data = cipher.decrypt(data)
+        return data
