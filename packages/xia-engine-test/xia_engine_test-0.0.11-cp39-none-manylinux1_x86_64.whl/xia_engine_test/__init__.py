656ac8211eed54ee3a55b8462082681dc52d116a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:28:54 +0000

    Add package xia-engine-test/0.0.11/xia_engine_test-0.0.11-cp39-none-manylinux1_x86_64.whl

---
+++ packages/xia-engine-test/xia_engine_test-0.0.11-cp39-none-manylinux1_x86_64.whl/xia_engine_test/__init__.py
@@ -0,0 +1,10 @@
+from xia_engine_test.field_test import FieldTest
+from xia_engine_test.document_example import MessageHello, DocumentSimple
+
+__all__ = [
+    "FieldTest",
+    "MessageHello", "DocumentSimple"
+]
+
+
+__version__ = "0.0.11"
