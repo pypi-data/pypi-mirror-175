ecfb072eeabb4bebbf44f12cccaa17d1baf32714
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:37:59 +0000

    Add package py-taplo/0.1.2/py_taplo-0.1.2-py2.py3-none-any.whl

---
+++ packages/py-taplo/py_taplo-0.1.2-py2.py3-none-any.whl/py_taplo/config.py
@@ -0,0 +1,38 @@
+# taplo config file toml text
+
+# https://taplo.tamasfe.dev/
+
+empty_config_toml = """
+# empty config
+"""
+
+config_toml = """
+
+#include = [ "*" ]
+
+[formatting]
+align_comments        = true
+align_entries         = true
+allowed_blank_lines   = 1
+array_auto_collapse   = false
+array_auto_expand     = true
+array_trailing_comma  = true
+column_width          = 80
+compact_arrays        = false
+compact_entries       = false
+compact_inline_tables = false
+crlf                  = false
+indent_entries        = true
+indent_string         = "  "
+indent_tables         = true
+reorder_keys          = true
+trailing_newline      = true
+
+[[rule]]
+include = [ "*/pyproject.toml" ]
+keys = ["schema"]
+
+[rule.schema]
+path = "https://json.schemastore.org/pyproject.json"
+
+"""
