1b3e396b36c260eb1ad36f6ab0f77e2a77160a0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:17:39 +0000

    Add package cflashfiles/2.3/cflashfiles-2.3.tar.gz

---
+++ packages/cflashfiles/cflashfiles-2.3.tar.gz/cflashfiles-2.3/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+
+
+
+setup(name='cflashfiles',
+      version='2.3',
+      description='Flash work with files',
+      packages=['cflashfiles'],
+      install_requires=['requests', 'fsspec'],
+      url="",
+      long_description='',
+      author_email='',
+      zip_safe=False)
