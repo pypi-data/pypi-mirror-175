be673c6d992a326ca5e41528eee61c51929f4644
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:59 +0000

    Add package Sun-Weverse/1.0.0.5/Sun_Weverse-1.0.0.5-py3-none-any.whl

---
+++ packages/Sun-Weverse/Sun_Weverse-1.0.0.5-py3-none-any.whl/sunverse/__init__.py
@@ -0,0 +1,4 @@
+__title__ = 'Sun Weverse'
+__author__ = 'Anson Quek'
+__license__ = 'MIT'
+__version__ = '1.0.0.5'
---
+++ packages/Sun-Weverse/Sun_Weverse-1.0.0.5-py3-none-any.whl/sunverse/objects/__init__.py
@@ -0,0 +1,2 @@
+from .post import Postlike
+from .media import Medialike, WeverseMedia
