37d65506da1e8f7551fe00040568889d648db659
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:52:34 +0000

    Add package tracking-numbers/0.1.7/tracking_numbers-0.1.7-py3-none-any.whl

---
+++ packages/tracking-numbers/tracking_numbers-0.1.7-py3-none-any.whl/tracking_numbers/__init__.py
@@ -0,0 +1,31 @@
+import os
+from typing import List
+from typing import Optional
+
+from tracking_numbers.definition import TrackingNumberDefinition
+from tracking_numbers.types import TrackingNumber
+
+if not os.environ.get("CODE_GENERATING"):
+    from tracking_numbers._generated import DEFINITIONS
+else:
+    # When running codegen, it's very possible that the items in
+    # DEFINITIONS are out of date / can't be successfully constructed
+    # so we use an empty list so that codegen can still import utils
+    DEFINITIONS = []
+
+
+def get_tracking_number(number: str) -> Optional[TrackingNumber]:
+    for tn_definition in DEFINITIONS:
+        tracking_number = tn_definition.test(number)
+        if tracking_number and tracking_number.valid:
+            return tracking_number
+
+    return None
+
+
+def get_definition(product_name: str) -> Optional[TrackingNumberDefinition]:
+    for tn_definition in DEFINITIONS:
+        if tn_definition.product.name.lower() == product_name.lower():
+            return tn_definition
+
+    return None
---
+++ packages/tracking-numbers/tracking_numbers-0.1.7-py3-none-any.whl/tracking_numbers/types.py
@@ -0,0 +1,39 @@
+from dataclasses import dataclass
+from typing import Any
+from typing import Dict
+from typing import List
+from typing import Optional
+from typing import Tuple
+
+Spec = Dict[str, Any]
+SerialNumber = List[int]
+ValidationError = Tuple[str, str]
+
+
+@dataclass
+class Product:
+    name: str
+
+
+@dataclass
+class Courier:
+    code: str
+    name: str
+
+
+@dataclass
+class TrackingNumber:
+    number: str
+    courier: Courier
+    product: Product
+    serial_number: Optional[SerialNumber]
+    tracking_url: Optional[str]
+    validation_errors: List[ValidationError]
+
+    @property
+    def valid(self) -> bool:
+        return not self.validation_errors
+
+
+def to_int(serial_number: SerialNumber) -> int:
+    return int("".join(map(str, serial_number)))
