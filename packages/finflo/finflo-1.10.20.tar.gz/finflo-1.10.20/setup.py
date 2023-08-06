70a5be32c6aa2ba74f264ca66b422a7e69516f77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:32:38 +0000

    Add package finflo/1.10.20/finflo-1.10.20.tar.gz

---
+++ packages/finflo/finflo-1.10.20.tar.gz/finflo-1.10.20/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+
+# read from README.RST
+def readme():
+    with open('readme.md') as f:
+        return f.read()
+
+setup(
+    name='finflo',
+    long_description=readme(),
+    long_description_content_type='text/markdown'
+)
