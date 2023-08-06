61f5f5f008931709941e5a07115f3981be69bd27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:36 +0000

    Add package LarpixParser/0.0.3/LarpixParser-0.0.3.tar.gz

---
+++ packages/LarpixParser/LarpixParser-0.0.3.tar.gz/LarpixParser-0.0.3/setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+import setuptools
+
+VER = "0.0.3"
+
+setuptools.setup(
+    name="LarpixParser",
+    version=VER,
+    author="Yifan C. and others",
+    author_email="cyifan@slac.stanford.edu",
+    description="A package parsing the larpix output to hit-level",
+    url="https://github.com/YifanC/larpix_readout_parser",
+    packages=setuptools.find_packages(where="src"), #"where" is needed; "include=['LarpixParser']" is not necessary 
+    package_dir={"":"src"},
+    install_requires=["numpy", "h5py", "fire"],
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Topic :: Scientific/Engineering :: Physics"
+    ],
+    python_requires='>=3.2',
+)
