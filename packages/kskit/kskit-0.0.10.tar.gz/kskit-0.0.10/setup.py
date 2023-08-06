52397c5afbe533b7b5c8715d46a035a8fef76675
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:39 +0000

    Add package kskit/0.0.10/kskit-0.0.10.tar.gz

---
+++ packages/kskit/kskit-0.0.10.tar.gz/kskit-0.0.10/setup.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+with open('README.md') as f:
+    readme = f.read()
+
+setup(
+    name='kskit',
+    version='0.0.10',
+    description='Cancer screening analysis module build on Python',
+    long_description=readme,
+    long_description_content_type='text/markdown',
+    author='Francisco Orchard',
+    author_email='f.orchard@epiconcept.fr',
+    url='https://github.com/Epiconcept-Paris/kskit',
+    license="MIT License",
+    install_requires=[
+      "paramiko",
+      "fabric",
+      "easyocr",
+      "cryptography",
+      "opencv-python==4.5.5.64",
+      "pydicom",
+      "Numpy",
+      "matplotlib",
+      "pynetdicom",
+      "requests",
+      "xmltodict", 
+      "pandas",
+      "pyarrow",
+      "python-barcode[images]",
+      "qrcode",
+      "pyzbar[scripts]",
+      "opencv-python",
+      "pycryptodome",
+      "H5py",
+      "imageio",
+      "tqdm",
+      "pillow",
+      "gdcm"
+    ],
+    packages=find_packages(exclude=('tests', 'docs'))
+)
+
