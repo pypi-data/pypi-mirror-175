483139710a69d7728998bbcb2cab58f83a85fd55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:32:14 +0000

    Add package flow360-betdisk/0.1.3/flow360-betdisk-0.1.3.tar.gz

---
+++ packages/flow360-betdisk/flow360-betdisk-0.1.3.tar.gz/flow360-betdisk-0.1.3/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['BETDisk']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['numpy>=1.23.4,<2.0.0']
+
+setup_kwargs = {
+    'name': 'flow360-betdisk',
+    'version': '0.1.3',
+    'description': '',
+    'long_description': None,
+    'author': 'An Li',
+    'author_email': 'kidylee@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
