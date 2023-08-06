d24114e8d2c2f64d7771d822c809a0fdb6e54b1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:04:31 +0000

    Add package tigeropen/2.2.1/tigeropen-2.2.1.tar.gz

---
+++ packages/tigeropen/tigeropen-2.2.1.tar.gz/tigeropen-2.2.1/tigeropen/common/consts/push_destinations.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+QUOTE = 'quote'
+QUOTE_DEPTH = 'quotedepth'
+QUOTE_FUTURE = 'future'
+QUOTE_OPTION = 'option'
+TRADE_TICK = 'tradetick'
+
+TRADE_ASSET = 'trade/asset'
+TRADE_POSITION = 'trade/position'
+TRADE_ORDER = 'trade/order'
