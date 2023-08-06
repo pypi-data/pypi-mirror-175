bee0586c6022a4ff6eec6d3529e109f9fd0aa0ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:31:30 +0000

    Add package dissect/3.3.dev7/dissect-3.3.dev7.tar.gz

---
+++ packages/dissect/dissect-3.3.dev7.tar.gz/dissect-3.3.dev7/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+
+setup(
+    name="dissect",
+    install_requires=[
+        "dissect.cim==3.2",
+        "dissect.clfs==1.1",
+        "dissect.cstruct==3.2",
+        "dissect.esedb==3.2",
+        "dissect.etl==3.1",
+        "dissect.eventlog==3.1",
+        "dissect.evidence==3.1",
+        "dissect.extfs==3.1",
+        "dissect.fat==3.1",
+        "dissect.ffs==3.1",
+        "dissect.hypervisor==3.2",
+        "dissect.ntfs==3.1",
+        "dissect.ole==3.1",
+        "dissect.regf==3.1",
+        "dissect.shellitem==3.1",
+        "dissect.sql==3.1",
+        "dissect.target[full]==3.3",
+        "dissect.thumbcache==1.0",
+        "dissect.util==3.2",
+        "dissect.vmfs==3.1",
+        "dissect.volume==3.1",
+        "dissect.xfs==3.1",
+    ],
+)
