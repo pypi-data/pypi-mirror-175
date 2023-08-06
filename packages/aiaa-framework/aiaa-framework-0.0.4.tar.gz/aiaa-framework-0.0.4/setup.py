f829c039f958f7ab3aad6717ace2d13eb768406a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:32:46 +0000

    Add package aiaa-framework/0.0.4/aiaa-framework-0.0.4.tar.gz

---
+++ packages/aiaa-framework/aiaa-framework-0.0.4.tar.gz/aiaa-framework-0.0.4/setup.py
@@ -0,0 +1,22 @@
+
+from distutils.core import setup
+
+setup(
+    name =          'aiaa-framework',
+    version =       '0.0.4',
+    description =   'Framework for Koerber Python AI-AA team to simplify data IO and cloud usage.',
+    author =        'Körber Supply Chain Software',
+    maintainer=     'Körber Supply Chain Software',
+    copyright =     'Copyright 2022, PythonBase, Körber Supply Chain Software',
+    license =       'Apache 2.0',
+
+    install_requires = [
+        'pymssql'
+    ],
+
+    packages = [
+        'aiaa',
+        'aiaa.utils',
+        'aiaa.sql'
+    ]
+)
