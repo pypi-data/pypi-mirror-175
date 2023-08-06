bbb72e4e9d940a1247ab08b62791463bcaa30240
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:55:01 +0000

    Add package tilm/0.0.2/tilm-0.0.2.tar.gz

---
+++ packages/tilm/tilm-0.0.2.tar.gz/tilm-0.0.2/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+with open("README.md", encoding='utf-8') as fh:
+    long_description = fh.read()
+
+# with open("requirements.txt") as f:
+#     required_packages = f.read().splitlines()
+
+setup(
+    name="tilm",
+    version="0.0.2",
+    author="Qingyang Wu",
+    author_email="willqywu@gmail.com",
+    description="Text-based web interface for language model",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="",
+    packages=['tilm'],
+    # install_requires=required_packages,
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+    ],
+)
