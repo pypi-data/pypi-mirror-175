fbc54f369d4ce59d4cccca21df6850d5e05a8b50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:33:43 +0000

    Add package heakl-pycalc/0.0.1/heakl-pycalc-0.0.1.tar.gz

---
+++ packages/heakl-pycalc/heakl-pycalc-0.0.1.tar.gz/heakl-pycalc-0.0.1/src/pycalc/__main__.py
@@ -0,0 +1,16 @@
+"""This is my test main file
+
+Version
+------------------
+ - heakl-pycalc v0.0.1
+
+"""
+
+
+def main() -> None:
+    x = int(input("Enter value to print.."))
+    print(f"Here is your value {x}")
+
+
+if __name__ == "__main__":
+    main()
---
+++ packages/heakl-pycalc/heakl-pycalc-0.0.1.tar.gz/heakl-pycalc-0.0.1/src/pycalc/calcClass.py
@@ -0,0 +1,20 @@
+class Calc:
+    def add(self, a: float, b: float) -> float:
+        """Adds two numbers"""
+        return a + b
+
+    def sub(self, a: float, b: float) -> float:
+        """Subtracts two numbers"""
+        return a - b
+
+    def mul(self, a: float, b: float) -> float:
+        """Muliplies two numbers"""
+        return a * b
+
+    def div(self, a: float, b: float) -> float:
+        """Divides two numbers"""
+        return a / b
+
+    def exp(self, a: float, b: float) -> float:
+        """Returns a raised to the power of b"""
+        return a**b
