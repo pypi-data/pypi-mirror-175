a199b7057c8a0192b2b8c49623c409041d089aab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:30:45 +0000

    Add package Geode-ExplicitModeling/2.1.15/Geode_ExplicitModeling-2.1.15-cp310-cp310-manylinux2014_x86_64.whl

---
+++ packages/Geode-ExplicitModeling/Geode_ExplicitModeling-2.1.15-cp310-cp310-manylinux2014_x86_64.whl/geode_explicitmodeling/explicit_modeling3d.py
@@ -0,0 +1,10 @@
+#
+# Copyright (c) 2019 - 2022 Geode-solutions. All rights reserved.
+#
+
+import opengeode
+import geode_common
+import geode_conversion
+
+from .geode_explicitmodeling_py_explicit_modeling3d import *
+ExplicitModeling3d.initialize()
