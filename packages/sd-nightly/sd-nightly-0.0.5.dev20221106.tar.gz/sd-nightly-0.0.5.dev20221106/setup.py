89966d39411d109ddaacdda86d632b227ab8e93c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:10 +0000

    Add package sd-nightly/0.0.5.dev20221106/sd-nightly-0.0.5.dev20221106.tar.gz

---
+++ packages/sd-nightly/sd-nightly-0.0.5.dev20221106.tar.gz/sd-nightly-0.0.5.dev20221106/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+import scaledown
+import pathlib
+
+here = pathlib.Path(__file__).parent.resolve()
+
+long_description = (here / "README.md").read_text(encoding="utf-8")
+
+setuptools.setup(
+    name="sd-nightly",
+    version=scaledown.__version__,
+    author="csoham",
+    author_email="hello@csoham.com",
+    description="Scaledown is an Open Source Neural Network Optimization Framework for TinyML Devices",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/scaledown-team/scaledown",
+    project_urls={
+        "Bug Tracker": "https://github.com/scaledown-team/scaledown/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+    packages=setuptools.find_packages(exclude=['test*']),
+    python_requires=">=3.6",
+)
