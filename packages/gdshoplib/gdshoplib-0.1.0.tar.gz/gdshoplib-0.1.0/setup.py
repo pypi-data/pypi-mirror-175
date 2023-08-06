886ce9d2286ecdbe674271e08adc1853415e62c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:15:29 +0000

    Add package gdshoplib/0.1.0/gdshoplib-0.1.0.tar.gz

---
+++ packages/gdshoplib/gdshoplib-0.1.0.tar.gz/gdshoplib-0.1.0/setup.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['package',
+ 'package.apps',
+ 'package.apps.finance',
+ 'package.apps.marketing',
+ 'package.apps.products',
+ 'package.core',
+ 'package.core.manager',
+ 'package.services',
+ 'package.services.notion',
+ 'package.services.notion.models']
+
+package_data = \
+{'': ['*'], 'package.services': ['avito/*', 'ula/*', 'vk/*']}
+
+install_requires = \
+['notion-client>=1.0.0,<2.0.0',
+ 'pydantic>=1.10.2,<2.0.0',
+ 'requests>=2.28.1,<3.0.0',
+ 'twine>=4.0.1,<5.0.0']
+
+setup_kwargs = {
+    'name': 'gdshoplib',
+    'version': '0.1.0',
+    'description': '',
+    'long_description': '# Библиотека с наборами действий для использования в остальных инструментах\n\napps - Инкапсуляция технических действий в операции над доменами\n\nservice - Инкапсуляция действий над платформами\n\ncore - Инструменты, чтобы собрать приложение в единый инструмент\n\n# Roadmap\n\n[] Модуль для управления Notion\n[] Модуль для управления Avito\n[] Модуль для управления VK сообществом\n[] Получение информации о товарах из Notion\n',
+    'author': 'Nikolay Baryshnikov',
+    'author_email': 'root@k0d.ru',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'https://github.com/p141592',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.10,<4.0',
+}
+
+
+setup(**setup_kwargs)
