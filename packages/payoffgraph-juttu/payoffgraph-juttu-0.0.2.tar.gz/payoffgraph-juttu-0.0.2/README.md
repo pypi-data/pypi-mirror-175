fd34161f8fe63902aa82a3163dfc4b2292884a30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:13:35 +0000

    Add package payoffgraph-juttu/0.0.2/payoffgraph-juttu-0.0.2.tar.gz

---
+++ packages/payoffgraph-juttu/payoffgraph-juttu-0.0.2.tar.gz/payoffgraph-juttu-0.0.2/README.md
@@ -0,0 +1,10 @@
+# Get the most beautiful Payoff Chart using a single simple function
+
+
+## Usage
+
+- Make sure you have Python installed in your system.
+- Run Following command in the CMD.
+ ```
+  pip install payoffgraph-juttu
+  ```
---
+++ packages/payoffgraph-juttu/payoffgraph-juttu-0.0.2.tar.gz/payoffgraph-juttu-0.0.2/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='payoffgraph-juttu',
+    version='0.0.2',
+    description='Get the most beautiful Payoff Chart using a single simple function',
+    author= 'Juttu G Anurag',
+    # url = 'https://github.com/Spidy20/PyMusic_Player',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    keywords=['payoff graph', 'options','opstra','sensibull'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    py_modules=['payoffgraph-juttu'],
+    package_dir={'':'src'},
+    install_requires = [
+        'numpy',
+        'pandas',
+        'streamlit',
+        'plotly'
+
+    ]
+)
