b26697403a1dca6e8ba447d6a880b40d06c7f7d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:22:31 +0000

    Add package enstools-encoding/0.1.10/enstools-encoding-0.1.10.tar.gz

---
+++ packages/enstools-encoding/enstools-encoding-0.1.10.tar.gz/enstools-encoding-0.1.10/setup.py
@@ -0,0 +1,31 @@
+"""
+Setup file for enstools-encoding
+"""
+from setuptools import setup
+
+# Use the Readme file as long description.
+try:
+    with open("Readme.md", "r") as f:
+        long_description = f.read()
+except FileNotFoundError:
+    long_description = ""
+
+
+# perform the actual install operation
+setup(name="enstools-encoding",
+      version="0.1.10",
+      author="Oriol Tintó",
+      author_email="oriol.tinto@lmu.de",
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+      url="https://github.com/wavestoweather/enstools-encoding",
+      packages=["enstools.encoding", "enstools.encoding.compressors"],
+      namespace_packages=['enstools'],
+
+      install_requires=[
+          "xarray",
+          "PyYAML",
+          "h5py",
+          "h5netcdf",
+      ],
+      )
