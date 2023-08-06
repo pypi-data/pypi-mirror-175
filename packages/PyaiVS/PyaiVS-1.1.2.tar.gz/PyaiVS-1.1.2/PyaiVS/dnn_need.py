1db28491a7867b49ebd1d8b735cd53c1870b1197
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:13:41 +0000

    Add package PyaiVS/1.1.2/PyaiVS-1.1.2.tar.gz

---
+++ packages/PyaiVS/PyaiVS-1.1.2.tar.gz/PyaiVS-1.1.2/PyaiVS/dnn_need.py
@@ -0,0 +1,5 @@
+# from PyaiVS import model_bulid
+# model_bulid.running('/data/jianping/bokey/OCAICM/dataset/chembl31/chembl31.csv',out_dir='/data/jianping/bokey/OCAICM/dataset/',
+#                     split='all',model='all',FP='all', run_type='param',cpus=8)
+
+
