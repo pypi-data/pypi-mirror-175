6ce8ac66f6775db2d6810fb20b14f89ba6291b5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:13:15 +0000

    Add package files3/0.3.0/files3-0.3.0.tar.gz

---
+++ packages/files3/files3-0.3.0.tar.gz/files3-0.3.0/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="files3",
+    version="0.3.0",
+    author="eaglebaby",
+    author_email="2229066748@qq.com",
+    description="(pickle+lz4 based) save Python objects in binary to the file system and manage them.",
+
+    #url="http://iswbm.com/", 
+
+    packages=find_packages(),
+    platforms = "",
+    classifiers = [
+        'Development Status :: 3 - Alpha',
+
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+
+        'Programming Language :: Python :: 3',
+    ],
+
+    install_requires=["lz4"],
+
+    python_requires='>=3'
+
+
+)
