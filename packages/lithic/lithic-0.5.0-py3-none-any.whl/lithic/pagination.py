9ac859abaf82f7d3f37e5172e20072705d7296ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:53:09 +0000

    Add package lithic/0.5.0/lithic-0.5.0-py3-none-any.whl

---
+++ packages/lithic/lithic-0.5.0-py3-none-any.whl/lithic/pagination.py
@@ -0,0 +1,52 @@
+# File generated from our OpenAPI spec by Stainless.
+
+from typing import List, Generic, TypeVar, Optional
+from typing_extensions import TypedDict
+
+from ._types import ModelT
+from ._models import BaseModel
+from ._base_client import BasePage, PageInfo, BaseSyncPage, BaseAsyncPage
+
+__all__ = ["PageParams", "SyncPage", "AsyncPage"]
+
+_BaseModelT = TypeVar("_BaseModelT", bound=BaseModel)
+
+
+class PageParams(TypedDict, total=False):
+    page: int
+    """Page (for pagination)."""
+
+    page_size: int
+    """Page size (for pagination)."""
+
+
+class SyncPage(BaseSyncPage[ModelT], BasePage[ModelT, PageParams], Generic[ModelT]):
+    data: List[ModelT]
+    page: int
+    total_entries: int
+    total_pages: int
+
+    def _get_page_items(self) -> List[ModelT]:
+        return self.data
+
+    def next_page_info(self) -> Optional[PageInfo]:
+        current_page = self.page
+        if not current_page < self.total_pages:
+            return None
+        return PageInfo(params={"page": current_page + 1})
+
+
+class AsyncPage(BaseAsyncPage[ModelT], BasePage[ModelT, PageParams], Generic[ModelT]):
+    data: List[ModelT]
+    page: int
+    total_entries: int
+    total_pages: int
+
+    def _get_page_items(self) -> List[ModelT]:
+        return self.data
+
+    def next_page_info(self) -> Optional[PageInfo]:
+        current_page = self.page
+        if not current_page < self.total_pages:
+            return None
+        return PageInfo(params={"page": current_page + 1})
