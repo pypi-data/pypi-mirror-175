59489542a72608cf5863116c919d0dd953965003
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:15:44 +0000

    Add package SeerPPO/0.0.6/SeerPPO-0.0.6.tar.gz

---
+++ packages/SeerPPO/SeerPPO-0.0.6.tar.gz/SeerPPO-0.0.6/SeerPPO/V1/__init__.py
@@ -0,0 +1,3 @@
+from .action import SeerActionV1
+from .obs import SeerObsV1
+from .policy import SeerNetworkV1
---
+++ packages/SeerPPO/SeerPPO-0.0.6.tar.gz/SeerPPO-0.0.6/SeerPPO/V2/__init__.py
@@ -0,0 +1,3 @@
+from .action import SeerActionV2
+from .obs import SeerObsV2
+from .policy import SeerNetworkV2
