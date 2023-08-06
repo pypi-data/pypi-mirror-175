1bf2fbba14174cebaa38074f6f90cf4ab7f3916c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:59:54 +0000

    Add package helloworldbasic5message/1.0.6/helloworldbasic5message-1.0.6-py3-none-any.whl

---
+++ packages/helloworldbasic5message/helloworldbasic5message-1.0.6-py3-none-any.whl/helloworldbasic5message/__init__.py
@@ -0,0 +1,21 @@
+
+import helloworldbasic4message  as hb4
+
+from helloworldbasic4message  import MarianoList as RainbowList
+
+
+
+#import numpy as np
+
+# also recommended imports specifics
+
+
+
+#from helloworldbasic4message.utility2 import somefunction
+
+#nparray=np.array([1,2])
+#print(nparray)
+#print(help (hb4) )
+print("hello helloworldbasic5")
+
+
