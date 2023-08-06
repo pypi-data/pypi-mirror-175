dce89f23b6cf0cdc5ad5f2b5088aa2dd951bcac0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:22:14 +0000

    Add package Cornelia-First-Hello/0.1.0/Cornelia-First-Hello-0.1.0.tar.gz

---
+++ packages/Cornelia-First-Hello/Cornelia-First-Hello-0.1.0.tar.gz/Cornelia-First-Hello-0.1.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+setup(
+    name='Cornelia-First-Hello',
+    version='0.1.0',
+    author='Tacea Mariana',
+    author_email="mariana.tacea@yahoo.com",
+    packages = ['my_own_package'],
+    package_dir = {'': 'src\\'},
+    include_package_data = True
+
+)
