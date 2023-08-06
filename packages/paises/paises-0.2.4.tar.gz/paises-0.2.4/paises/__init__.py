52e30551040ad39736461109c6d8ad80b51cf241
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:33 +0000

    Add package paises/0.2.4/paises-0.2.4.tar.gz

---
+++ packages/paises/paises-0.2.4.tar.gz/paises-0.2.4/paises/__init__.py
@@ -0,0 +1,5 @@
+from paises.countries import Countries
+from paises.country import Country
+from paises.groups import Groups
+from paises.cache import *
+from paises.alias import *
---
+++ packages/paises/paises-0.2.4.tar.gz/paises-0.2.4/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['paises']
+
+package_data = \
+{'': ['*'], 'paises': ['cache/*', 'cache/backup/*', 'groups/*']}
+
+install_requires = \
+['importlib>=1.0.4,<2.0.0', 'pytest>=7.1.3,<8.0.0', 'texttable>=1.6.4,<2.0.0']
+
+setup_kwargs = {
+    'name': 'paises',
+    'version': '0.2.4',
+    'description': '',
+    'long_description': 'geo\n',
+    'author': 'Tomas',
+    'author_email': 'tomasgonz@gmail.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.11,<4.0',
+}
+
+
+setup(**setup_kwargs)
