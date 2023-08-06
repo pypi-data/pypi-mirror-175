fcc063559df9a4c17ed5cec8124baad2eac85e0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:01:07 +0000

    Add package aide-fusu-jihua/2022.11.7.0/aide-fusu-jihua-2022.11.7.0.tar.gz

---
+++ packages/aide-fusu-jihua/aide-fusu-jihua-2022.11.7.0.tar.gz/aide-fusu-jihua-2022.11.7.0/AideFusuJihua/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""aide-fusu-jihua
+https://github.com/apachecn/aide-fusu-jihua"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.11.7.0"
