8e054ed6f76c2d5e2d6c3c3787389309ac1b4d66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:24:15 +0000

    Add package maxlib-repackaged-MGGY8411-yy2205/0.1.0/maxlib-repackaged-MGGY8411-yy2205-0.1.0.tar.gz

---
+++ packages/maxlib-repackaged-MGGY8411-yy2205/maxlib-repackaged-MGGY8411-yy2205-0.1.0.tar.gz/maxlib-repackaged-MGGY8411-yy2205-0.1.0/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup
+
+from codecs import open
+from os import path
+
+HERE = path.abspath(path.dirname(__file__))
+
+with open(path.join(HERE, "README.md"), encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name="maxlib-repackaged-MGGY8411-yy2205",
+    version="0.1.0",
+    description="Max's code repackaged as a library",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Yue",
+    author_email="yy2205@nyu.edu",
+    packages=["."],
+    include_package_data=True,
+    install_requires=[
+        "wheel",
+        "pandas",
+        "matplotlib",
+        "numpy",
+        "sqlalchemy",
+        "polygon",
+        "jupyter",
+        "polygon-api-client",
+    ],
+)
