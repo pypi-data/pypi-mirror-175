9b2fbe9c35131f4134bdadb34f0a6e8e389b2df7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:26 +0000

    Add package deep_personality/0.1.3/deep_personality-0.1.3.tar.gz

---
+++ packages/deep_personality/deep_personality-0.1.3.tar.gz/deep_personality-0.1.3/setup.py
@@ -0,0 +1,19 @@
+# from distutils.core import setup
+from setuptools import setup
+
+
+with open("README", 'r') as file:
+    readme = file.read()
+
+
+setup(
+    name="deep_personality",
+    version="0.1.3",
+    packages=["dpcv"],
+    url="https://testpypi.python.org/pypi/deep_personality",
+    license="LICENSE",
+    description="an open source bench mark for automatic personality recognition",
+    long_description=readme,
+    author="LiaoRongFan, SongSiYang",
+    author_email="rongfan.liao@hotmail.com",
+)
