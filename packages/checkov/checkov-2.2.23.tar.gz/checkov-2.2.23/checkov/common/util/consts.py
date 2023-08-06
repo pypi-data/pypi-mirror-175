e3d976f101fdf6def688f5f12b948a9c4132c41e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:00:54 +0000

    Add package checkov/2.2.23/checkov-2.2.23.tar.gz

---
+++ packages/checkov/checkov-2.2.23.tar.gz/checkov-2.2.23/checkov/common/util/consts.py
@@ -0,0 +1,26 @@
+import os
+
+DEFAULT_EXTERNAL_MODULES_DIR = ".external_modules"
+RESOLVED_MODULE_ENTRY_NAME = "__resolved__"
+START_LINE = '__startline__'
+END_LINE = '__endline__'
+
+DEV_API_GET_HEADERS = {
+    'Accept': 'application/json'
+}
+
+DEV_API_POST_HEADERS = {
+    'Accept': 'application/json',
+    'Content-Type': 'application/json'
+}
+
+PRISMA_API_GET_HEADERS = {
+    'Accept': 'application/json; charset=UTF-8'
+}
+
+PARSE_ERROR_FAIL_FLAG = 'CKV_PARSE_ERROR_FAIL'
+
+PRISMA_PLATFORM = 'Prisma Cloud Code Security'
+BRIDGECREW_PLATFORM = 'Bridgecrew'
+
+MAX_IAC_FILE_SIZE = int(os.getenv('CHECKOV_MAX_IAC_FILE_SIZE', '50_000_000'))  # 50 MB is default limit
