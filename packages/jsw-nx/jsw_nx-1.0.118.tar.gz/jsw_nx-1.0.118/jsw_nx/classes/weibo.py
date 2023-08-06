14923c4ae8a663175526bac67cd3ab0f0a21763c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:11:33 +0000

    Add package jsw-nx/1.0.118/jsw_nx-1.0.118.tar.gz

---
+++ packages/jsw-nx/jsw_nx-1.0.118.tar.gz/jsw_nx-1.0.118/jsw_nx/classes/weibo.py
@@ -0,0 +1,85 @@
+import requests
+import json
+import re
+from .lc_option import LcOption
+
+# https://houbb.github.io/2019/02/25/github-09-pic-bed
+
+
+TARGET_RE = r'"pics":(.*)\}\}'
+PIC_HOST = 'https://wx1.sinaimg.cn/'
+SIZES = 'large|bmiddle|mw1024|mw690|small|square|thumb180|thumbnail'.split('|')
+API_URL = {
+    'file': 'https://picupload.weibo.com/interface/pic_upload.php',
+    'base64': 'https://picupload.weibo.com/interface/pic_upload.php?data=base64',
+}
+
+
+class Weibo:
+
+    @property
+    def headers(self):
+        return {'Cookie': f'SUB={self.token}', 'Referer': 'https://weibo.com/'}
+
+    def __init__(self):
+        lc_opt = LcOption()
+        res = lc_opt.get('60f768f6d9f1465d3b1d5c43')
+        self.token = res['value']
+
+    # Get the image url by pid and size.
+    def get(self, pid, size='large'):
+        if size not in SIZES:
+            size = 'large'
+        return PIC_HOST + size + '/' + pid
+
+    # Get all kinds of image url by pid.
+    def getall(self, pid):
+        return {
+            'large': PIC_HOST + 'large/' + pid + '.jpg',
+            'bmiddle': PIC_HOST + 'bmiddle/' + pid + '.jpg',
+            'mw1024': PIC_HOST + 'mw1024/' + pid + '.jpg',
+            'mw690': PIC_HOST + 'mw690/' + pid + '.jpg',
+            'small': PIC_HOST + 'small/' + pid + '.jpg',
+            'square': PIC_HOST + 'square/' + pid + '.jpg',
+            'thumb180': PIC_HOST + 'thumb180/' + pid + '.jpg',
+            'thumbnail': PIC_HOST + 'thumbnail/' + pid + '.jpg',
+        }
+
+    # Upload image by url.
+    def upload(self, **kwargs):
+        source = kwargs.get('source')
+        debug = kwargs.get('debug', False)
+        mode = kwargs.get('mode', 'file')
+        files = {'pic1': source, }
+        if mode == 'file':
+            filep = open(source, 'rb')
+            files = {'pic1': filep, }
+        if mode == 'base64':
+            filep = source
+            files = {'b64_data': filep}
+        res = requests.post(API_URL.get(mode, 'file'), files=files, headers=self.headers)
+        html = res.text
+
+        if debug:
+            print("Response HTML: ", html)
+
+        target_str = re.findall(TARGET_RE, html)[0]
+        json_data = json.loads(target_str)
+        picdata = json_data['pic_1']
+        pid = picdata.get('pid', None)
+        is_success = bool(pid) and picdata.get('width', 0) > 0
+
+        if is_success:
+            return {
+                'success': True,
+                'pid': pid,
+                'url': self.get(pid),
+                **picdata
+            }
+        else:
+            return {
+                'success': False,
+                'pid': None,
+                'url': None,
+                **picdata
+            }
