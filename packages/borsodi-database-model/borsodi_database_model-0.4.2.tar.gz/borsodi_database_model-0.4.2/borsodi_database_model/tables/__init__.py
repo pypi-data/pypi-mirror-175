ea776810dc466dcc4ba8444913a2002a35e28b9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:02 +0000

    Add package borsodi-database-model/0.4.2/borsodi_database_model-0.4.2.tar.gz

---
+++ packages/borsodi-database-model/borsodi_database_model-0.4.2.tar.gz/borsodi_database_model-0.4.2/borsodi_database_model/tables/__init__.py
@@ -0,0 +1,3 @@
+from .base import Base
+from .report import Report
+from .truck_tracking_log import TruckTrackingLog
---
+++ packages/borsodi-database-model/borsodi_database_model-0.4.2.tar.gz/borsodi_database_model-0.4.2/setup.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['borsodi_database_model',
+ 'borsodi_database_model.alembic',
+ 'borsodi_database_model.alembic.versions',
+ 'borsodi_database_model.tables']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['sqlalchemy==1.3.18']
+
+setup_kwargs = {
+    'name': 'borsodi-database-model',
+    'version': '0.4.2',
+    'description': 'Database table models for the Borsodi project.',
+    'long_description': None,
+    'author': 'Pintér Tamás',
+    'author_email': 'tamas.pinter@dyntell.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
