069675d676008141469ee6d335a5ae1af5af9be9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:01:44 +0000

    Add package pyjoepegs/0.0.1/pyjoepegs-0.0.1.tar.gz

---
+++ packages/pyjoepegs/pyjoepegs-0.0.1.tar.gz/pyjoepegs-0.0.1/pyjoepegs/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*-coding:utf-8 -*-
+"""
+@File    :   __init__.py
+@Time    :   2022/11/07 17:24:40
+@Author  :   ishtos
+@Version :   0.0.1
+@License :   (C)Copyright 2022 ishtos
+"""
