f221bc1636c154872662b5397e4ad2bc3cf08113
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:14:49 +0000

    Add package extractnet/2.0.7/extractnet-2.0.7-cp310-cp310-macosx_10_15_x86_64.whl

---
+++ packages/extractnet/extractnet-2.0.7-cp310-cp310-macosx_10_15_x86_64.whl/extractnet/__init__.py
@@ -0,0 +1,13 @@
+from extractnet.pipeline import Extractor
+
+__version__ = '2.0.7'
+
+
+_LOADED_MODELS = {}
+
+def extract_news(html, encoding=None, as_blocks=False):
+    if 'news_extraction' not in _LOADED_MODELS:
+        _LOADED_MODELS['news_extraction'] = Extractor()
+
+    return _LOADED_MODELS['news_extraction'].extract(html)
+
