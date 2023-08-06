b1f8b58c1eeeaed852ee17e3f64420c5bd423278
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:18:13 +0000

    Add package ZEVIT-AIH-SDK/0.5.9/ZEVIT_AIH_SDK-0.5.9-py3-none-any.whl

---
+++ packages/ZEVIT-AIH-SDK/ZEVIT_AIH_SDK-0.5.9-py3-none-any.whl/AIH_SDK/Maintenance/__init__.py
@@ -0,0 +1,24 @@
+from .Category import Category
+from .Classification import Classification, ClassificationElement
+from .PropertyDefinition import PropertyDefinition
+from .WorkItem import WorkItemActivity 
+from .Activity import Activity, Input, ActivityMediaReference
+from .Deviation import Deviation
+from .Media import Media
+from .MediaReference import MediaReference
+
+
+__all__ = [
+    'Category',
+    'Classification',
+    'ClassificationElement',
+    'PropertyDefinition',
+    'Deviation',
+    'Activity',
+    'WorkItem',
+    'Input',
+    'WorkItemActivity',
+    'Media',
+    'MediaReference',
+    'ActivityMediaReference'
+]
