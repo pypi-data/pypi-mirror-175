a59be6aedc0b25b226fbebacfcea63dacdd05ce0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:16:17 +0000

    Add package pyDABAX/0.3.1/pyDABAX-0.3.1.tar.gz

---
+++ packages/pyDABAX/pyDABAX-0.3.1.tar.gz/pyDABAX-0.3.1/src/pydabax/__init__.py
@@ -0,0 +1,21 @@
+import sys
+from pydabax.dabax import *
+from pydabax.elements import *
+get_dabax = dabax.get
+get_keys = dabax.get_keys
+get_symbols = dabax.get_symbols
+
+if sys.version_info[:2] >= (3, 8):
+    # TODO: Import directly (no need for conditional) when `python_requires = >= 3.8`
+    from importlib.metadata import PackageNotFoundError, version  # pragma: no cover
+else:
+    from importlib_metadata import PackageNotFoundError, version  # pragma: no cover
+
+try:
+    # Change here if project is renamed and does not equal the package name
+    dist_name = "pyDABAX"
+    __version__ = version(dist_name)
+except PackageNotFoundError:  # pragma: no cover
+    __version__ = "unknown"
+finally:
+    del version, PackageNotFoundError
