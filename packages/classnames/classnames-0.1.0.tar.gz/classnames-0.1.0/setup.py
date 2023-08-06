717a5ce0844b77ac44c44f4f19d85b734077266d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:33:42 +0000

    Add package classnames/0.1.0/classnames-0.1.0.tar.gz

---
+++ packages/classnames/classnames-0.1.0.tar.gz/classnames-0.1.0/setup.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+
+"""The setup script."""
+
+from setuptools import setup, find_packages
+
+with open("README.md") as readme_file:
+    readme = readme_file.read()
+
+with open("CHANGELOG.md") as history_file:
+    history = history_file.read()
+
+requirements = []
+
+test_requirements = []
+
+setup(
+    author="Luca Faggianelli",
+    author_email="luca.faggianelli@gmail.com",
+    python_requires=">=3.6",
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+    ],
+    description="Utility to create CSS class strings from a multitude of values without poking around with string templates and lengthy logic.",
+    install_requires=requirements,
+    license="MIT license",
+    long_description=readme + "\n\n" + history,
+    long_description_content_type="text/markdown",
+    include_package_data=True,
+    keywords="classnames",
+    name="classnames",
+    packages=find_packages(include=["classnames", "classnames.*"]),
+    test_suite="tests",
+    tests_require=test_requirements,
+    url="https://github.com/lucafaggianelli/python-classnames",
+    version="0.1.0",
+    zip_safe=False,
+)
