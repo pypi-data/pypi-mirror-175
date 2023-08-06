0f98c4fe901432b80d7cee8b5c7ae09c27040db4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:40:06 +0000

    Add package notanorm/2.8.3/notanorm-2.8.3-py3-none-any.whl

---
+++ packages/notanorm/notanorm-2.8.3-py3-none-any.whl/notanorm/__init__.py
@@ -0,0 +1,28 @@
+"""Simple db accessor system: not an orm."""
+
+from .base import DbRow
+
+try:
+    from .mysql import MySqlDb
+except ImportError:
+    pass
+from .sqlite import SqliteDb
+from .base import DbBase, Op
+from .model import DbType, DbCol, DbTable, DbModel, DbIndex
+from . import errors
+from .connparse import open_db
+
+__all__ = [
+    "SqliteDb",
+    "MySqlDb",
+    "DbRow",
+    "DbBase",
+    "DbType",
+    "DbCol",
+    "DbTable",
+    "DbModel",
+    "DbIndex",
+    "errors",
+    "Op",
+    "open_db",
+]
