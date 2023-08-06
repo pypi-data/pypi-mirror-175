5bd4b13d5030770f6fce088928ce7e6e585faf6c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:00:25 +0000

    Add package dm-cli/0.1.6rc1/dm-cli-0.1.6rc1.tar.gz

---
+++ packages/dm-cli/dm-cli-0.1.6rc1.tar.gz/dm-cli-0.1.6rc1/setup.py
@@ -0,0 +1,31 @@
+# https://medium.com/nerd-for-tech/how-to-build-and-distribute-a-cli-tool-with-python-537ae41d9d78
+
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+with open("requirements.txt", "r") as fh:
+    requirements = fh.read()
+
+setup(
+    name="dm-cli",
+    version="0.1.6-rc1",
+    author="",
+    author_email="",
+    license="MIT",
+    description="",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/equinor/dm-cli",
+    packages=["dm_cli"],
+    install_requires=[requirements],
+    python_requires=">=3.8",
+    classifiers=[
+        "Programming Language :: Python :: 3.8",
+        "Operating System :: OS Independent",
+    ],
+    scripts=[
+        "dm_cli/bin/dm",
+    ],
+)
