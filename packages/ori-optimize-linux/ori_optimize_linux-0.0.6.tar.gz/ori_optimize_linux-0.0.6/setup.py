e2548b2d193faf42a24ab6753b1514cd7da1bf05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:15:03 +0000

    Add package ori-optimize-linux/0.0.6/ori_optimize_linux-0.0.6.tar.gz

---
+++ packages/ori-optimize-linux/ori_optimize_linux-0.0.6.tar.gz/ori_optimize_linux-0.0.6/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="ori_optimize_linux",
+    version="0.0.6",
+    description="a environment that controls STL model rotate to reach the maximal 3D printing quality.",
+    install_requires=["gym==0.25.2", "numpy"],
+    packages=find_packages()
+)
