740e69204efe78c97e37239adf5c6f14ecca82e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:22:56 +0000

    Add package a-pandas-ex-reg2df/0.11/a_pandas_ex_reg2df-0.11.tar.gz

---
+++ packages/a-pandas-ex-reg2df/a_pandas_ex_reg2df-0.11.tar.gz/a_pandas_ex_reg2df-0.11/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+#change to dict
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(os.path.abspath(os.path.dirname(__file__)),'README.md'), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = '0.11'
+DESCRIPTION = "Never mind regedit.exe, here is reg2df"
+
+# Setting up
+setup(
+    name="a_pandas_ex_reg2df",
+    version=VERSION,
+    license='MIT',
+    url = 'https://github.com/hansalemaos/a_pandas_ex_reg2df',
+    author="Johannes Fischer",
+    author_email="<aulasparticularesdealemaosp@gmail.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    #packages=['beautifulsoup4', 'pandas'],
+    keywords=['regedit', 'windows', 'pandas'],
+    classifiers=['Development Status :: 4 - Beta', 'Programming Language :: Python :: 3 :: Only', 'Programming Language :: Python :: 3.9', 'Topic :: Scientific/Engineering :: Visualization', 'Topic :: Software Development :: Libraries :: Python Modules', 'Topic :: Text Editors :: Text Processing', 'Topic :: Text Processing :: General', 'Topic :: Text Processing :: Indexing', 'Topic :: Text Processing :: Filters', 'Topic :: Utilities'],
+    install_requires=['beautifulsoup4', 'pandas'],
+    include_package_data=True
+)
+#python setup.py sdist bdist_wheel
+#twine upload dist/*
