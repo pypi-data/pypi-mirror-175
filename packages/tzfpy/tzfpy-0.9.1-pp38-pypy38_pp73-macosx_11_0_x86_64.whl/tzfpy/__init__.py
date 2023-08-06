765b47016d9b453043196b08b85a74dd6019c895
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:23:32 +0000

    Add package tzfpy/0.9.1/tzfpy-0.9.1-pp38-pypy38_pp73-macosx_11_0_x86_64.whl

---
+++ packages/tzfpy/tzfpy-0.9.1-pp38-pypy38_pp73-macosx_11_0_x86_64.whl/tzfpy/__init__.py
@@ -0,0 +1,38 @@
+import os
+from ctypes import CDLL, POINTER, c_char_p, c_float, c_long
+from typing import List
+
+c_lib = CDLL(os.path.join(os.path.dirname(__file__), "tzf.so"))
+
+_get = c_lib.GetTZ
+_get.argtypes = [POINTER(c_float), POINTER(c_float)]
+_get.restype = c_char_p
+
+
+_names_counts = c_lib.CountTimezoneNames
+_names_counts.restype = c_long
+
+_names = c_lib.TimezoneNames
+_names.restype = POINTER(c_char_p)
+
+
+def get_tz(lng: float, lat: float) -> str:
+    return _get(c_float(lng), c_float(lat)).decode()
+
+
+def timezone_names_counts() -> int:
+    return _names_counts()
+
+
+def timezone_names() -> List[str]:
+    res = _names()
+    names = []
+    for i in range(timezone_names_counts()):
+        names.append(res[i].decode())
+    return names
+
+
+if __name__ == "__main__":
+    print(get_tz(11, 11))
+    print(get_tz(116, 39))
+    print(timezone_names())
