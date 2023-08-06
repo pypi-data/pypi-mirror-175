6d9178f83a8e2e41d585d773b361e144f7825ef2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:58 +0000

    Add package cryptofeed-werks/0.1.3/cryptofeed-werks-0.1.3.tar.gz

---
+++ packages/cryptofeed-werks/cryptofeed-werks-0.1.3.tar.gz/cryptofeed-werks-0.1.3/cryptofeed_werks/exchanges/__init__.py
@@ -0,0 +1,19 @@
+from .binance import Binance
+from .bitfinex import Bitfinex
+from .bitflyer import Bitflyer
+from .bitmex import Bitmex
+from .bybit import Bybit
+from .coinbase import Coinbase
+from .ftx import FTX
+from .upbit import Upbit
+
+__all__ = [
+    "Binance",
+    "Bitmex",
+    "Bitfinex",
+    "Bitflyer",
+    "Bybit",
+    "Coinbase",
+    "FTX",
+    "Upbit",
+]
