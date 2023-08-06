c864109cdca4a2950e929b6ec3b69e9fc327ad4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:01:00 +0000

    Add package stepwise-edx-plugin/14.0.15/stepwise-edx-plugin-14.0.15.tar.gz

---
+++ packages/stepwise-edx-plugin/stepwise-edx-plugin-14.0.15.tar.gz/stepwise-edx-plugin-14.0.15/stepwise_plugin/stepwise_oauth.py
@@ -0,0 +1,23 @@
+from oauth2_wordpress.wp_oauth import WPOpenEdxOAuth2
+
+
+class StepwiseMathWPOAuth2(WPOpenEdxOAuth2):
+
+    # This defines the backend name and identifies it during the auth process.
+    # The name is used in the URLs /login/<backend name> and /complete/<backend name>.
+    #
+    # This is the string value that will appear in the LMS Django Admin
+    # Third Party Authentication / Provider Configuration (OAuth)
+    # setup page drop-down box titled, "Backend name:", just above
+    # the "Client ID:" and "Client Secret:" fields.
+    name = "stepwisemath-oauth"
+
+    # setup oauth endpoints:
+    # authorization:    https://stepwisemath.ai/wp-json/moserver/authorize
+    # token:            https://stepwisemath.ai/wp-json/moserver/token
+    # user info:        https://stepwisemath.ai/wp-json/moserver/resource
+    BASE_URL = "https://stepwisemath.ai"
+    PATH = "wp-json/moserver"
+    AUTHORIZATION_ENDPOINT = "authorize"
+    TOKEN_ENDPOINT = "token"
+    USERINFO_ENDPOINT = "resource"
