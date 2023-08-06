e40b58bc8ea3a7ad11a6e6a572e9fd2c2301f290
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:30 +0000

    Add package tm1filetools/0.3.2/tm1filetools-0.3.2-py3-none-any.whl

---
+++ packages/tm1filetools/tm1filetools-0.3.2-py3-none-any.whl/tm1filetools/files/base.py
@@ -0,0 +1,80 @@
+import itertools
+from pathlib import Path
+
+
+class TM1File:
+    """
+    Base class for TM1 files
+    """
+
+    prefix = ""
+    control_prefix = "}"
+    is_tm1_file = True
+
+    def __init__(self, path):
+
+        self._path = Path(path)
+        self.name = self._path.name
+        self.stem = self._path.stem
+        self.is_control = self._is_control_object()
+        self.suffix = self._get_suffix()
+
+    def __str__(self):
+
+        return f"{self.__class__.__name__} ({self.name})"
+
+    def exists(self):
+
+        return self._path.exists()
+
+    def _is_control_object(self):
+
+        return self.stem[0] == self.control_prefix
+
+    def _get_suffix(self):
+
+        return self._path.suffix[1:]
+
+    def delete(self):
+
+        return self._path.unlink()
+
+    # i.e. rename the stem part, not the suffix or the parent
+    # probably most useful for renaming cubes, dims, views and subsets in advance of a restart
+    def rename(self, new_name: str):
+
+        # I feel there must be a more elegant way to do this
+        new_path = Path.joinpath(self._path.parent, f"{new_name}.{self.suffix}")
+        self._path = self._path.rename(new_path)
+
+        # This could lead to some confusion about the difference b/w stem and name :shrug:
+        # Maybe there's a better way to handle this but let's just update the attribute
+        self.stem = self._path.stem
+
+    def rename_suffix_to_lower(self):
+
+        # a way to standardise all file extensions to lower case
+        new_path = Path.joinpath(self._path.parent, f"{self._stem}.{self._path.suffix.lower()}")
+        self._path = self._path.rename(new_path)
+
+    def strip_prefix(self):
+
+        return self.stem.removeprefix(self.prefix)
+
+    @staticmethod
+    def _get_suffix_permutations(suffix: str):
+
+        lu_sequence = ((c.lower(), c.upper()) for c in suffix)
+        return ["".join(x) for x in itertools.product(*lu_sequence)]
+
+
+class NonTM1File(TM1File):
+
+    # Kind of a stupid name
+
+    def __init__(self, path):
+
+        super().__init__(path)
+
+        self.is_control = False
+        self.is_tm1_file = False
---
+++ packages/tm1filetools/tm1filetools-0.3.2-py3-none-any.whl/tm1filetools/files/text/subset.py
@@ -0,0 +1,23 @@
+from pathlib import Path
+
+from .user_owned import TM1UserFile
+
+
+class TM1SubsetFile(TM1UserFile):
+    """
+    A class representation of a tm1 subset file
+
+    """
+
+    suffix = "sub"
+    folder_suffix = "}subs"
+
+    def __init__(self, path: Path, public: bool = True):
+
+        super().__init__(path)
+
+        self.dimension = self._get_object_name()
+        self.public = public
+        self.owner = self._get_owner_name()
+        # subset_name maybe a clearer API than stem?
+        self.subset_name = self.stem
