1138f235a427d49777c6ee512f7d2c839f51a0e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:19:47 +0000

    Add package streamlit-keycloak/0.2.0/streamlit_keycloak-0.2.0-1-py3-none-any.whl

---
+++ packages/streamlit-keycloak/streamlit_keycloak-0.2.0-1-py3-none-any.whl/streamlit_keycloak/frontend/build/precache-manifest.26c484a01420e0d2d61d96e257b87036.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d99f19bc72c1da3ce570804d3674e21d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e0bb71299a653528c040",
+    "url": "./static/js/2.8c72c59c.chunk.js"
+  },
+  {
+    "revision": "9fb993bbd978ed97d00146e2dfc5286c",
+    "url": "./static/js/2.8c72c59c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3febefc320cbbe046910",
+    "url": "./static/js/main.43f84a85.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "./static/js/main.43f84a85.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "abf035dd39300f62a9ce",
+    "url": "./static/js/runtime-main.27025a60.js"
+  }
+]);
---
+++ packages/streamlit-keycloak/streamlit_keycloak-0.2.0-1-py3-none-any.whl/streamlit_keycloak/frontend/build/precache-manifest.8482bb59464c58ed2e88fe8bb0000a79.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e3c9a2df8ac9759eddcb50b89d67ce7",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e0bb71299a653528c040",
+    "url": "./static/js/2.8c72c59c.chunk.js"
+  },
+  {
+    "revision": "9fb993bbd978ed97d00146e2dfc5286c",
+    "url": "./static/js/2.8c72c59c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cecd8bd4e2c1e23deab8",
+    "url": "./static/js/main.538db693.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "./static/js/main.538db693.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "abf035dd39300f62a9ce",
+    "url": "./static/js/runtime-main.27025a60.js"
+  }
+]);
