74c5a72aaae8286c57504bbe89530c43588be732
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:30:06 +0000

    Add package stableperovskites/0.0.2/stableperovskites-0.0.2.tar.gz

---
+++ packages/stableperovskites/stableperovskites-0.0.2.tar.gz/stableperovskites-0.0.2/regression_model/train_pipeline.py
@@ -0,0 +1,29 @@
+from config.core import config
+from pipeline import formation_energy_pipe
+from processing.data_manager import load_dataset, save_pipeline
+from sklearn.model_selection import train_test_split
+
+
+def run_training() -> None:
+    """Train the model."""
+
+    # read training data
+    data = load_dataset(file_name=config.app_config.dft_calc_train)
+
+    # divide train and test
+    X_train, X_test, y_train, y_test = train_test_split(
+        data[config.model_config.input_features],
+        data[config.model_config.target],
+        test_size=config.model_config.test_size,
+        random_state=config.model_config.random_state,
+    )
+
+    # fit model
+    formation_energy_pipe.fit(X_train, y_train)
+
+    # persist trained model
+    save_pipeline(pipeline_to_persist=formation_energy_pipe)
+
+
+if __name__ == "__main__":
+    run_training()
