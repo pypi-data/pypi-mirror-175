43767e3baf1e4f6f6c7538953c9422fa0de8e5dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:10 +0000

    Add package diligence/0.0.0.0/diligence-0.0.0.0.tar.gz

---
+++ packages/diligence/diligence-0.0.0.0.tar.gz/diligence-0.0.0.0/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+from distutils.core import setup
+
+setup(
+    name='diligence',
+    version='0.0.0.0',
+    author='AXEwiges',
+    author_email='syrupcake@protonmail.com',
+    description='a package for test',
+    packages=['diligence'],
+    install_requires=[],
+    license='MIT',
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+    ],
+)
