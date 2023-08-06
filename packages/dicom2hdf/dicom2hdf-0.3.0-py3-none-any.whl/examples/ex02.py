854b2d827430f2d6d396ec4a3299f93fef1d8f39
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:23:51 +0000

    Add package dicom2hdf/0.3.0/dicom2hdf-0.3.0-py3-none-any.whl

---
+++ packages/dicom2hdf/dicom2hdf-0.3.0-py3-none-any.whl/examples/ex02.py
@@ -0,0 +1,65 @@
+"""
+    @Title:             Radiomics extraction.
+
+    @Description:       Extract radiomics from CT and PET images with a given 'Heart' contour.
+"""
+
+import env_examples  # Modifies path, DO NOT REMOVE
+
+from dicom2hdf.generators import PatientsDataGenerator
+from dicom2hdf.radiomics import RadiomicsDataset, RadiomicsFeatureExtractor
+from dicom2hdf.transforms import Compose, CopySegmentationsD, PETtoSUVD
+
+
+if __name__ == "__main__":
+    # ----------------------------------------------------------------------------------------------------------- #
+    #                                         Logs Setup (Optional)                                               #
+    # ----------------------------------------------------------------------------------------------------------- #
+    env_examples.configure_logging("logging_conf.yaml")
+
+    # ----------------------------------------------------------------------------------------------------------- #
+    #                                      Create patients data generator                                         #
+    # ----------------------------------------------------------------------------------------------------------- #
+    patients_data_generator = PatientsDataGenerator(
+        path_to_patients_folder="data/patients",
+        series_descriptions="data/radiomics_series_descriptions.json",
+        transforms=Compose(
+            [
+                PETtoSUVD(keys=["TEP"]),
+                CopySegmentationsD(segmented_image_key="CT_THORAX", unsegmented_image_key="TEP")
+            ]
+        )
+    )
+
+    # ----------------------------------------------------------------------------------------------------------- #
+    #       Extract radiomics features of the CT image from the segmentation of the heart made on the CT image    #
+    # ----------------------------------------------------------------------------------------------------------- #
+
+    CT_radiomics_dataset = RadiomicsDataset(path_to_dataset="data/CT_radiomics")
+
+    CT_radiomics_dataset.extractor = RadiomicsFeatureExtractor(
+        path_to_params="features_extractor_params_CT.yaml",
+        geometryTolerance=1e-4
+    )
+
+    CT_radiomics_dataset.create(
+        patients_data_generator=patients_data_generator,
+        organ="Heart",
+        image_name="CT_THORAX"
+    )
+
+    # ----------------------------------------------------------------------------------------------------------- #
+    #       Extract radiomics features of the PT image from the segmentation of the heart made on the CT image    #
+    # ----------------------------------------------------------------------------------------------------------- #
+    PT_radiomics_dataset = RadiomicsDataset(path_to_dataset="data/PT_radiomics")
+
+    PT_radiomics_dataset.extractor = RadiomicsFeatureExtractor(
+        path_to_params="features_extractor_params_PT.yaml",
+        geometryTolerance=1e-4
+    )
+
+    PT_radiomics_dataset.create(
+        patients_data_generator=patients_data_generator,
+        organ="Heart",
+        image_name="TEP"
+    )
---
+++ packages/dicom2hdf/dicom2hdf-0.3.0-py3-none-any.whl/examples/ex03.py
@@ -0,0 +1,58 @@
+"""
+    @Title:             Logging + database creation.
+
+    @Description:       Configure logging and create database.
+"""
+
+import env_examples  # Modifies path, DO NOT REMOVE
+
+from dicom2hdf.databases import PatientsDatabase
+from dicom2hdf.generators import PatientsDataGenerator
+from dicom2hdf.transforms import (
+    PETtoSUVD,
+    ResampleD
+)
+from monai.transforms import (
+    CenterSpatialCropD,
+    Compose,
+    ScaleIntensityD,
+    ThresholdIntensityD
+)
+
+
+if __name__ == "__main__":
+    # ----------------------------------------------------------------------------------------------------------- #
+    #                                         Logs Setup (Optional)                                               #
+    # ----------------------------------------------------------------------------------------------------------- #
+    env_examples.configure_logging("logging_conf.yaml")
+
+    # ----------------------------------------------------------------------------------------------------------- #
+    #                                      Create patients data generator                                         #
+    # ----------------------------------------------------------------------------------------------------------- #
+    patients_data_generator = PatientsDataGenerator(
+        path_to_patients_folder="data/patients",
+        series_descriptions="data/series_descriptions.json",
+        transforms=Compose(
+            [
+                ResampleD(keys=["CT_THORAX", "TEP", "Heart"], out_spacing=(1.5, 1.5, 1.5)),
+                CenterSpatialCropD(keys=["CT_THORAX", "TEP", "Heart"], roi_size=(1000, 160, 160)),
+                ThresholdIntensityD(keys=["CT_THORAX"], threshold=-250, above=True, cval=-250),
+                ThresholdIntensityD(keys=["CT_THORAX"], threshold=500, above=False, cval=500),
+                ScaleIntensityD(keys=["CT_THORAX"], minv=0, maxv=1),
+                PETtoSUVD(keys=["TEP"])
+            ]
+        )
+    )
+
+    # ----------------------------------------------------------------------------------------------------------- #
+    #                                                Create database                                              #
+    # ----------------------------------------------------------------------------------------------------------- #
+    database = PatientsDatabase(path_to_database="data/patients_database.h5")
+
+    database.create(
+        patients_data_generator=patients_data_generator,
+        tags_to_use_as_attributes=[(0x0008, 0x103E), (0x0020, 0x000E), (0x0008, 0x0060)],
+        overwrite_database=True
+    )
+
+    print("R01-005 patient :", database["R01-005"])
