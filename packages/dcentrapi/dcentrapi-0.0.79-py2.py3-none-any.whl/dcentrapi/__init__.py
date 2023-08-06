a01733e09909d13f3289e0a23f035ef2dd9c8a0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:37 +0000

    Add package dcentrapi/0.0.79/dcentrapi-0.0.79-py2.py3-none-any.whl

---
+++ packages/dcentrapi/dcentrapi-0.0.79-py2.py3-none-any.whl/dcentrapi/__init__.py
@@ -0,0 +1,4 @@
+from dcentrapi.Base import Base
+from dcentrapi.eventPolling import EventPolling
+from dcentrapi.rpcAggregation import RpcAggregation
+from dcentrapi.gasMonitor import GasMonitor
---
+++ packages/dcentrapi/dcentrapi-0.0.79-py2.py3-none-any.whl/dcentrapi/common.py
@@ -0,0 +1,5 @@
+VERSION = '0.0.79'
+
+
+def get_dapi_version():
+    return VERSION
