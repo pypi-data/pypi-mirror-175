a07a5f567fc9b16fec9c734c6bb48c994c66a5bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:57:29 +0000

    Add package minet/0.64.0/minet-0.64.0.tar.gz

---
+++ packages/minet/minet-0.64.0.tar.gz/minet-0.64.0/minet/facebook/exceptions.py
@@ -0,0 +1,17 @@
+# =============================================================================
+# Minet Facebook Exceptions
+# =============================================================================
+#
+from minet.exceptions import MinetError
+
+
+class FacebookError(MinetError):
+    pass
+
+
+class FacebookInvalidCookieError(FacebookError):
+    pass
+
+
+class FacebookInvalidTargetError(FacebookError):
+    pass
