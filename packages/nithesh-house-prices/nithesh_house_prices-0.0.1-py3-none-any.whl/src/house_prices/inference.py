d1a76ce88cad3ed1ab7728a63bad7b96018dd144
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:19:17 +0000

    Add package nithesh-house-prices/0.0.1/nithesh_house_prices-0.0.1-py3-none-any.whl

---
+++ packages/nithesh-house-prices/nithesh_house_prices-0.0.1-py3-none-any.whl/src/house_prices/inference.py
@@ -0,0 +1,51 @@
+import pandas as pd
+import joblib
+import numpy as np
+import warnings
+warnings.filterwarnings('ignore')
+
+
+def make_predictions(input_data: pd.DataFrame) -> np.ndarray:
+    df = input_data[[
+        'OverallQual',
+        'GrLivArea',
+        'GarageCars',
+        'GarageArea',
+        'TotalBsmtSF',
+        'GarageFinish',
+        'GarageType',
+        'SaleCondition'
+    ]]
+    df = df.dropna(axis=0)
+
+    # Preprocessing and Feature Engineering on this data
+    numerical_columns = [
+        cname for cname in df.columns if df[cname].dtype in [
+            'int64', 'float64'
+        ]
+    ]
+    categorical_columns = [
+        cname for cname in df.columns if df[cname].dtype == "object"
+    ]
+
+    # Loading the scaler and encoder
+    scaler = joblib.load('D:/EPITA/dsp/pw2/models/scaler.joblib')
+    encoder = joblib.load('D:/EPITA/dsp/pw2/models/encoder.joblib')
+
+    df_scaled = scaler.transform(df[numerical_columns])
+    df_scaled = pd.DataFrame(df_scaled, columns=numerical_columns)
+
+    df_encoded = encoder.transform(df[categorical_columns])
+    df_encoded = pd.DataFrame(
+        df_encoded, columns=encoder.get_feature_names(
+            categorical_columns
+        )
+    )
+
+    df = pd.concat([df_scaled, df_encoded], axis=1)
+
+    # Predicting the SalePrice for df using the model
+    model = joblib.load('D:/EPITA/dsp/pw2/models/model.joblib')
+    y_pred = model.predict(df)
+
+    return y_pred
