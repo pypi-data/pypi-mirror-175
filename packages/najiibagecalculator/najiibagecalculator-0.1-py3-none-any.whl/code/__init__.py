1bf61485e7ac1b0538daa8a9643665f4d564d590
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:49 +0000

    Add package najiibagecalculator/0.1/najiibagecalculator-0.1-py3-none-any.whl

---
+++ packages/najiibagecalculator/najiibagecalculator-0.1-py3-none-any.whl/code/__init__.py
@@ -0,0 +1,14 @@
+def calculate_age(year_born,now,name):
+    return f'You are {now-year_born} years old {name}'
+
+def sum_n(a,b):
+    return a+b
+
+def subs(a,b):
+    return a-b
+
+def multi(a,b):
+    return a*b
+
+def divide(a,b):
+    return a/b
