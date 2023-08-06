dbdd1fdad0c72a73c479ecc3454de608b1f84fd9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:22:23 +0000

    Add package DrMarcko-first-hello/0.2.0/DrMarcko-first-hello-0.2.0.tar.gz

---
+++ packages/DrMarcko-first-hello/DrMarcko-first-hello-0.2.0.tar.gz/DrMarcko-first-hello-0.2.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+setup(
+
+    name='DrMarcko-first-hello',
+    version='0.2.0',
+    author='Claudiu Pripis',
+    author_email='drmarcko22@yahoo.de',
+    pachages=['my_own_package'],
+    package_dir={'': 'src\\'},
+    include_package_data=True
+)
