888fcc2dd8f3913d2e3096e20754fb36b32bd867
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:16:31 +0000

    Add package jupyterlab-ui-profiler/0.1.1/jupyterlab_ui_profiler-0.1.1.tar.gz

---
+++ packages/jupyterlab-ui-profiler/jupyterlab_ui_profiler-0.1.1.tar.gz/jupyterlab_ui_profiler-0.1.1/src/statistics.ts
@@ -0,0 +1,26 @@
+export namespace Statistic {
+  export function min(numbers: number[]): number {
+    return Math.min(...numbers);
+  }
+
+  export function mean(numbers: number[]): number {
+    return sum(numbers) / numbers.length;
+  }
+
+  export function interQuartileMean(numbers: number[]): number {
+    const q = Math.floor(numbers.length / 4);
+    return mean(numbers.slice(q, numbers.length - q));
+  }
+
+  export function round(n: number, precision = 0): number {
+    const factor = Math.pow(10, precision);
+    return Math.round(n * factor) / factor;
+  }
+
+  export function sum(numbers: number[]): number {
+    if (numbers.length === 0) {
+      return NaN;
+    }
+    return numbers.reduce((a, b) => a + b);
+  }
+}
