3e333eb428be45366a0bb8799ca7da8f79697023
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:57:54 +0000

    Add package DE-LibUtil/0.0.37/DE_LibUtil-0.0.37.tar.gz

---
+++ packages/DE-LibUtil/DE_LibUtil-0.0.37.tar.gz/DE_LibUtil-0.0.37/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+setup(
+    name='DE_LibUtil',
+    version='0.0.37',
+    packages=[''],
+    url='https://github.com/DE-DataEng/DE_LibUtil.git',
+    license='MIT',
+    author='Almir J Gomes',
+    author_email='almir.jg@hotmail.com',
+    description='LibUtil - Biblioteca de Utilidades'
+)
