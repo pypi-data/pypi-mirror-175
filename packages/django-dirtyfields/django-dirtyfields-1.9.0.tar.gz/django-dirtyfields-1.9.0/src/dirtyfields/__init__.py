43fb15ebf82137cb0f904553ed7a6ecbb5996ef3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:22:52 +0000

    Add package django-dirtyfields/1.9.0/django-dirtyfields-1.9.0.tar.gz

---
+++ packages/django-dirtyfields/django-dirtyfields-1.9.0.tar.gz/django-dirtyfields-1.9.0/src/dirtyfields/__init__.py
@@ -0,0 +1,12 @@
+"""
+django-dirtyfields library for tracking dirty fields on a Model instance.
+
+Adapted from https://stackoverflow.com/questions/110803/dirty-fields-in-django
+"""
+
+__all__ = ['DirtyFieldsMixin']
+__version__ = "1.9.0"
+
+from dirtyfields.dirtyfields import DirtyFieldsMixin
+
+VERSION = tuple(map(int, __version__.split(".")[0:3]))
