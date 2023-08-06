65b206570e46638fd179c5aae3501378639a6592
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:24:52 +0000

    Add package flake8-forbidden-func/0.1.0/flake8_forbidden_func-0.1.0-py3-none-any.whl

---
+++ packages/flake8-forbidden-func/flake8_forbidden_func-0.1.0-py3-none-any.whl/flake8_forbidden_func/ast_tools.py
@@ -0,0 +1,68 @@
+from __future__ import annotations
+
+import ast
+
+from flake8_forbidden_func.custom_types import CallableNode
+
+
+def extract_callable_string_from(*, tree: ast.Module) -> list[CallableNode]:
+    callables = extract_callable_from(tree=tree)
+    return [
+        CallableNode(
+            callable_str=convert_callable_to_callable_string(call=node),
+            lineno=node.lineno,
+            col_offset=node.col_offset
+        )
+        for node in callables
+    ]
+
+
+def extract_callable_from(*, tree: ast.Module) -> list[ast.Call]:
+    return [n for n in ast.walk(tree) if isinstance(n, ast.Call)]
+
+
+def convert_callable_to_callable_string(call: ast.Call) -> str:
+    if not isinstance(call, ast.Call):
+        raise ValueError('Expected Call node.')
+    if isinstance(call.func, ast.Call):
+        return convert_callable_to_callable_string(call=call.func)
+    return convert_func_to_callable_string(node=call.func)
+
+
+def convert_func_to_callable_string(*, node: ast.AST) -> str:
+    if isinstance(node, ast.Subscript):
+        return convert_func_to_callable_string(node=node.value)
+
+    if not isinstance(node, (ast.Name, ast.Attribute)):
+        raise ValueError('Expected Name node or Attribute node.')
+
+    attributes = []
+    current_node = node
+    while current_node:
+        current_name, next_node = unpack_func_node(node=current_node)
+        if current_name:
+            attributes.append(current_name)
+        if next_node:
+            current_node = next_node
+        else:
+            break
+
+    return '.'.join(reversed(attributes))
+
+
+def unpack_func_node(  # noqa: CFQ004
+    *, node: ast.AST
+) -> tuple[str | None, ast.Name | ast.Attribute | None]:
+    if isinstance(node, ast.Name):
+        return node.id, None
+
+    if isinstance(node, ast.Attribute):
+        if isinstance(node.value, (ast.Name, ast.Attribute)):
+            return node.attr, node.value
+        elif isinstance(node.value, ast.Call):
+            call_name, next_node = unpack_func_node(node=node.value.func)
+            if call_name and not next_node:
+                return '.'.join([call_name, node.attr]), None
+            return node.attr, next_node
+
+    return None, None
---
+++ packages/flake8-forbidden-func/flake8_forbidden_func-0.1.0-py3-none-any.whl/tests/test_unit/test_ast_tools.py
@@ -0,0 +1,65 @@
+from __future__ import annotations
+
+import ast
+import pytest
+
+from flake8_forbidden_func.ast_tools import (
+    convert_callable_to_callable_string,
+    extract_callable_string_from
+)
+from flake8_forbidden_func.custom_types import CallableNode
+
+_python_code = """
+from bar import Model
+
+faz = Model.objects.all().annotate().filter(a=2)
+baz = bar_func()
+
+if faz == baz:
+    fuz = q.filter(a=1)
+    return fuz.bar()
+"""
+
+
+def test__extract_callable_string_from():
+    expected_result = [
+        CallableNode(callable_str='Model.objects.all', lineno=4, col_offset=6),
+        CallableNode(callable_str='Model.objects.annotate', lineno=4, col_offset=6),
+        CallableNode(callable_str='fuz.bar', lineno=9, col_offset=11),
+        CallableNode(callable_str='q.filter', lineno=8, col_offset=10),
+        CallableNode(callable_str='Model.objects.filter', lineno=4, col_offset=6),
+        CallableNode(callable_str='bar_func', lineno=5, col_offset=6)
+    ]
+    tree = ast.parse(_python_code)
+
+    result = extract_callable_string_from(tree=tree)
+
+    assert isinstance(result, list)
+    assert set(result) == set(expected_result)
+    assert len(result) == len(expected_result)
+
+
+@pytest.mark.parametrize(
+    ('code', 'expected_str'),
+    [
+        ('Model.objects.all()', 'Model.objects.all'),
+        ('Model.objects.all().annotate()', 'Model.objects.annotate'),
+        ('Model.objects.all().annotate().filter()', 'Model.objects.filter'),
+        ('Model().bar()', 'Model.bar'),
+        ('bur_func().bar_func().bar()', 'bur_func.bar_func.bar'),
+        ('bur_func().bar_func()', 'bur_func.bar_func'),
+        ('bur_func(t=1)', 'bur_func'),
+        ('q.func()', 'q.func'),
+        ('Model()', 'Model'),
+        ('Service(attr)()', 'Service'),
+        ('Foo(attr)()()()()()()', 'Foo'),
+        ('Stack[int]()', 'Stack'),
+    ]
+)
+def test__convert_callable_to_callable_string(code, expected_str):
+    tree = ast.parse(code)
+    call = [n for n in ast.walk(tree) if isinstance(n, ast.Call)][0]
+
+    result = convert_callable_to_callable_string(call=call)
+
+    assert result == expected_str
