750e6b3cdd1385f64d18cbba32c14032c037e8a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:21:49 +0000

    Add package nspyre-drivers/0.1.1/nspyre_drivers-0.1.1.tar.gz

---
+++ packages/nspyre-drivers/nspyre_drivers-0.1.1.tar.gz/nspyre_drivers-0.1.1/README.md
@@ -0,0 +1,7 @@
+# drivers
+A set of Python drivers for lab instrumentation. These drivers are associated 
+with [nspyre](https://nspyre.readthedocs.io/en/latest/), but are also suitable 
+for general usage.
+
+If you want to contribute driver code, please submit it as a 
+[pull request](http://TODO). 
---
+++ packages/nspyre-drivers/nspyre_drivers-0.1.1.tar.gz/nspyre_drivers-0.1.1/setup.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['drivers',
+ 'drivers.agiltron',
+ 'drivers.agiltron.ffsw',
+ 'drivers.arduino.arduino_gpio',
+ 'drivers.beaglebone',
+ 'drivers.beaglebone.beaglebone_gpio',
+ 'drivers.beaglebone.beaglebone_gpio.beaglebone_server',
+ 'drivers.thorlabs',
+ 'drivers.thorlabs.cld1010']
+
+package_data = \
+{'': ['*'], 'drivers.arduino.arduino_gpio': ['pin_server/*']}
+
+install_requires = \
+['pyvisa']
+
+extras_require = \
+{'agiltron': ['pyserial']}
+
+setup_kwargs = {
+    'name': 'nspyre-drivers',
+    'version': '0.1.1',
+    'description': 'A set of Python drivers for lab instrumentation.',
+    'long_description': '# drivers\nA set of Python drivers for lab instrumentation. These drivers are associated \nwith [nspyre](https://nspyre.readthedocs.io/en/latest/), but are also suitable \nfor general usage.\n\nIf you want to contribute driver code, please submit it as a \n[pull request](http://TODO). ',
+    'author': 'Jacob Feder',
+    'author_email': 'jacobsfeder@gmail.com',
+    'maintainer': 'Jacob Feder',
+    'maintainer_email': 'jacobsfeder@gmail.com',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'extras_require': extras_require,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
