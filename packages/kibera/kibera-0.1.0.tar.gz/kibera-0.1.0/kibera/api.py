36559f87150396d90d9fc811b26e4a0952597631
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:05:08 +0000

    Add package kibera/0.1.0/kibera-0.1.0.tar.gz

---
+++ packages/kibera/kibera-0.1.0.tar.gz/kibera-0.1.0/kibera/api.py
@@ -0,0 +1,96 @@
+import logging
+import re
+
+from flask import Flask, request
+from werkzeug.exceptions import BadRequest, HTTPException, NotFound, Unauthorized
+
+from kibera import keys
+from kibera.core import db
+from kibera.users import authenticate
+
+logger = logging.getLogger(__name__)
+
+app = Flask(__name__)
+
+
+@app.errorhandler(HTTPException)
+def handle_bad_request(e):
+    return {"message": e.description}, 400
+
+
+@app.route("/", methods=["GET"])
+def get_random_key():
+    key = keys.get_random_key()
+    if key is None:
+        return {}, NotFound
+    return strip_key(key), 200
+
+
+@app.route("/keys/", methods=["POST"])
+@authenticate
+def create_key(user):
+    data = validate_key_data(request.json)
+    try:
+        keys.create_key(user, data)
+    except keys.KeyExistsError:
+        raise BadRequest("Key with given host and port already exists")
+    return keys.get_key(data), 200
+
+
+@app.route("/keys/", methods=["GET"])
+@authenticate
+def list_keys(user):
+    data = validate_key_data(request.args, check_host_port=False)
+    return list(keys.list_keys(data)), 200
+
+
+@app.route("/key/<key>/", methods=["POST", "PUT"])
+@app.route("/keys/<key>/", methods=["POST", "PUT"])
+@authenticate
+def update_key(user, key):
+    data = keys.parse_key_url(key)
+    data.update(request.json)
+    data = validate_key_data(data)
+    keys.update_key(user, data)
+    return keys.get_key(data), 200
+
+
+@app.route("/key/<key>/", methods=["DELETE"])
+@app.route("/keys/<key>/", methods=["DELETE"])
+@authenticate
+def delete_key(user, key):
+    data = keys.parse_key_url(key)
+    data = validate_key_data(data)
+    keys.delete_key(user, data)
+    return {"message": "Key deleted"}, 200
+
+
+@app.route("/key/<key>/", methods=["GET"])
+@app.route("/keys/<key>/", methods=["GET"])
+@authenticate
+def get_key(user, key):
+    data = keys.parse_key_url(key)
+    data.update(request.args)
+    data = validate_key_data(data)
+
+    print(data)
+    k = keys.get_key(data)
+    if k is None:
+        raise NotFound(f"Key not found")
+    return strip_key(k), 200
+
+
+def validate_key_data(data, check_host_port=True):
+    if check_host_port:
+        for k in ["port", "host"]:
+            if not data.get(k):
+                raise BadRequest(f"Missing field '{k}' in key data")
+    if "port" in data:
+        if type(data["port"]) is str and not data["port"].isnumeric():
+            raise BadRequest(f"Invalid port {data['port']}")
+        data["port"] = int(data["port"])
+    return data
+
+
+def strip_key(key):
+    return {k: key.get(k) for k in ["host", "port", "cipher", "secret", "name"]}
---
+++ packages/kibera/kibera-0.1.0.tar.gz/kibera-0.1.0/setup.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['kibera', 'kibera.core']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['flask>=2.2.2,<3.0.0',
+ 'portforward>=0.3.0,<0.4.0',
+ 'prometheus-client>=0.15.0,<0.16.0',
+ 'pymongo>=4.3.2,<5.0.0',
+ 'redis>=3.5.3,<4.0.0',
+ 'rich>=12.6.0,<13.0.0',
+ 'tqdm>=4.60.0,<5.0.0',
+ 'waitress>=2.1.2,<3.0.0']
+
+entry_points = \
+{'console_scripts': ['kibera = kibera:main']}
+
+setup_kwargs = {
+    'name': 'kibera',
+    'version': '0.1.0',
+    'description': '',
+    'long_description': 'None',
+    'author': 'Hadi',
+    'author_email': 'hadi.zofi@gmail.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
