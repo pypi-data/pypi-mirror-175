1e1131f14a889a27eca817d51364a707c0de2155
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:54:58 +0000

    Add package otpmml/1.12.post4/otpmml-1.12.post4-cp39-cp39-win_amd64.whl

---
+++ packages/otpmml/otpmml-1.12.post4-cp39-cp39-win_amd64.whl/otpmml/__init__.py
@@ -0,0 +1,18 @@
+"""
+    otpmml --- An OpenTURNS module
+    ==================================
+
+    Contents
+    --------
+      'otpmml' is a module for OpenTURNS
+
+"""
+
+import sys
+if sys.platform.startswith('win'):
+    # this ensures OT dll is loaded
+    import openturns
+
+from .otpmml import *
+
+__version__ = '1.12.post4'
