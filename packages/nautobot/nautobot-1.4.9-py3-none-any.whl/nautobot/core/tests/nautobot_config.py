c7859792c18621e5dc12ababf57deaca48d9ac64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:51:39 +0000

    Add package nautobot/1.4.9/nautobot-1.4.9-py3-none-any.whl

---
+++ packages/nautobot/nautobot-1.4.9-py3-none-any.whl/nautobot/core/tests/nautobot_config.py
@@ -0,0 +1,45 @@
+###################################################################
+#  This file serves as a base configuration for testing purposes  #
+#  only. It is not intended for production use.                   #
+###################################################################
+
+import os
+
+from nautobot.core.settings import *  # noqa: F401,F403
+from nautobot.core.settings_funcs import parse_redis_connection
+
+# No host checks required during tests
+ALLOWED_HOSTS = ["*"]
+
+# Discover test jobs from within the Nautobot source code
+JOBS_ROOT = os.path.join(
+    os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))), "extras", "tests", "example_jobs"
+)
+
+# Enable both example plugins
+PLUGINS = [
+    "example_plugin",
+    "example_plugin_with_view_override",
+]
+
+# Hard-code the SECRET_KEY for simplicity
+SECRET_KEY = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+
+# Redis variables
+
+# Use *different* redis_databases than the ones (0 and 1) used during non-automated-testing operations.
+CACHES["default"]["LOCATION"] = parse_redis_connection(redis_database=2)  # noqa: F405
+CACHEOPS_REDIS = parse_redis_connection(redis_database=3)
+
+CACHEOPS_ENABLED = False  # TODO(john): we should revisit this, but caching has caused issues with testing
+
+# Testing storages within cli.py
+STORAGE_CONFIG = {
+    "AWS_ACCESS_KEY_ID": "ASFWDAMWWOQMEOQMWPMDA<WPDA",
+    "AWS_SECRET_ACCESS_KEY": "ASFKMWADMsacasdaw/dawrt1231541231231",
+    "AWS_STORAGE_BUCKET_NAME": "nautobot",
+    "AWS_S3_REGION_NAME": "us-west-1",
+}
+
+# Disable logging for tests
+LOGGING = {}
