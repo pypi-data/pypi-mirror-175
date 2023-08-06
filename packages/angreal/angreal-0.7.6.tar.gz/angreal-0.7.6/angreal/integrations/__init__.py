7e73a84908ee9ab3fa34afc9489475198e91630b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:06 +0000

    Add package angreal/0.7.6/angreal-0.7.6.tar.gz

---
+++ packages/angreal/angreal-0.7.6.tar.gz/angreal-0.7.6/angreal/integrations/__init__.py
@@ -0,0 +1,10 @@
+"""
+    angreal.integrations
+    ~~~~~~~~~~~~~~~~~~~~
+
+"""
+
+from angreal.integrations.git import Git
+from angreal.integrations.virtual_env import VirtualEnv, venv_required
+
+
