ed4bcc76950aa003a1c557eb6eb9a8f5b8ff940b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:53:16 +0000

    Add package strike-api/0.1.5/strike_api-0.1.5.tar.gz

---
+++ packages/strike-api/strike_api-0.1.5.tar.gz/strike_api-0.1.5/strike_api/accounts.py
@@ -0,0 +1,29 @@
+import typing
+
+from strike_api.base import call_api
+
+
+def get_account(
+    account_id: typing.Optional[str] = None,
+    handle: typing.Optional[str] = None,
+) -> dict:
+    """Fetch public account profile info by id or handle
+
+    Args:
+        account_id (typing.Optional[str]): Account id
+        handle (typing.Optional[str]): Handle attached to the account to fetch
+
+    Returns:
+        dict: account schema
+
+    """
+
+    url = ""
+    if account_id and not handle:
+        url = f"https://api.strike.me/v1/accounts/{account_id}/profile"
+    elif handle and not account_id:
+        url = f"https://api.strike.me/v1/accounts/handle/{handle}/profile"
+    else:
+        raise ValueError("use either account_id or handle")
+
+    return call_api("GET", url)
