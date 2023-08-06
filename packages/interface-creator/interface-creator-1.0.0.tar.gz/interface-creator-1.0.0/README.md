2fb80b56d8562b99fc6358365a881fa6dffa7a3f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:21:41 +0000

    Add package interface-creator/1.0.0/interface-creator-1.0.0.tar.gz

---
+++ packages/interface-creator/interface-creator-1.0.0.tar.gz/interface-creator-1.0.0/README.md
@@ -0,0 +1,14 @@
+# InterfaceCreator
+
+a simple utility to create an interface based on the source module
+
+# How To Use
+click shell help
+```
+infCreate --help
+```
+
+common usage
+```
+infCreate ./source.py ./target.py
+```
---
+++ packages/interface-creator/interface-creator-1.0.0.tar.gz/interface-creator-1.0.0/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='interface-creator',
+    version='1.0.0',
+    description='Creates interfaces from python files',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    #url='',
+    author='Zackary W',
+    # packages
+    packages=find_packages(),
+    # dependencies
+    install_requires=[
+        'click',
+    ],
+    # entry points
+    entry_points={
+        'console_scripts': [
+            'infCreate = InterfaceCreator.cli:create_interface',
+        ],
+    },
+    # classifiers
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],   
+)
