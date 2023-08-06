16e7ec20cc61bb0ba1089a2ca5fb09e0d89a757f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:20:39 +0000

    Add package textbytesencoder/0.1.3/textbytesencoder-0.1.3.tar.gz

---
+++ packages/textbytesencoder/textbytesencoder-0.1.3.tar.gz/textbytesencoder-0.1.3/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['textbytesencoder']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'textbytesencoder',
+    'version': '0.1.3',
+    'description': 'Module encoding and encrypting text by key',
+    'long_description': None,
+    'author': 'D1ffic00lt',
+    'author_email': 'dm.filinov@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
