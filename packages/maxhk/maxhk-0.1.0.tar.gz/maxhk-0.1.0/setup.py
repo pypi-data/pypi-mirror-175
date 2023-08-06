a44eb92ea6c95ea7f4e17913aca63fa56db9cba4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:20:08 +0000

    Add package maxhk/0.1.0/maxhk-0.1.0.tar.gz

---
+++ packages/maxhk/maxhk-0.1.0.tar.gz/maxhk-0.1.0/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+package_dir = \
+{'': 'src'}
+
+packages = \
+['maxhk']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'maxhk',
+    'version': '0.1.0',
+    'description': 'Set of miscelaneous utils.',
+    'long_description': '# Package with miscelaneous utils\n\n## 1 password\n\n```python\ncreds = op.get_item("pypi.org", ["username", "password"])\n# use `creds.username` or `creds.password`\n```\n\n## PyPI\n\nhttps://pypi.org/project/maxhk/\n',
+    'author': 'Max',
+    'author_email': 'None',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'https://github.com/maximehk/maxhk_pypi',
+    'package_dir': package_dir,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
