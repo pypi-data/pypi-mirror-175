aad0c74c2f5820ea3b02118f6671efe5977f0ed0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:09 +0000

    Add package kubelink/0.0.4/kubelink-0.0.4.tar.gz

---
+++ packages/kubelink/kubelink-0.0.4.tar.gz/kubelink-0.0.4/app/commands/create.py
@@ -0,0 +1,29 @@
+from app.config import Config
+from argparse import Namespace
+import logging
+
+def createCommand(config: Config, args: Namespace):
+    preset = {
+        'name': args.name,
+        'source': args.source,
+        'destination': args.destination,
+        'namespace': args.namespace,
+        'selector': args.selector,
+        'container': args.container
+    }
+
+    logging.info('Create preset')
+    logging.debug('Create preset: ' + str(preset))
+
+    isNew = True
+    if config.presetExists(preset['name']):
+        isNew = False
+
+    if not config.savePreset(preset):
+        logging.error('Failed to save a new preset')
+        exit(1)
+
+    if isNew:
+        print('Successfully added preset: ' + args.name)
+    else:
+        print('Successfully updated preset: ' + args.name)
---
+++ packages/kubelink/kubelink-0.0.4.tar.gz/kubelink-0.0.4/app/commands/watch.py
@@ -0,0 +1,43 @@
+import time
+from watchdog.observers import Observer
+from app.event_handler import EventHandler
+import logging
+from app.config import Config
+from argparse import Namespace
+from app.kubernetes import Kubernetes
+
+class WatchCommand:
+    def __init__(self, config: Config, args: Namespace) -> None:
+        self.config = config
+        self.args = args
+
+    def run(self):
+        preset = self.config.findPreset(self.args.name)        
+        if preset == None:
+            logging.error('Preset not found. First you have to run: "kubelink create" to create a preset')
+            exit(1)
+        
+        logging.debug('Found preset: ' + str(preset))
+
+        kube = Kubernetes()
+
+        pods = kube.findPods(preset['selector'], preset['namespace'], preset['container'])
+        logging.info('Found ' + str(len(pods)) + ' pods')
+
+        if len(pods) == 0:
+            print('Pods to sync not found!')
+            exit()
+
+        event_handler = EventHandler(preset, pods)
+        observer = Observer()
+        observer.schedule(event_handler, preset['source'], recursive=True)
+        observer.start()
+
+        logging.info('Watch on changes')
+
+        try:
+            while True:
+                time.sleep(1)
+        except KeyboardInterrupt:
+            observer.stop()
+        observer.join()
