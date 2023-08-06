2778e917c7fcd9885f469152032c03697f4ac6d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:15:32 +0000

    Add package Abaxador-de-arquivo/2024/Abaxador-de-arquivo-2024.tar.gz

---
+++ packages/Abaxador-de-arquivo/Abaxador-de-arquivo-2024.tar.gz/Abaxador-de-arquivo-2024/README.md
@@ -0,0 +1,7 @@
+﻿Abaxador de arquivo criando por GabrielLLPontes
+
+Sistemas:
+from Abaxador_de_arquivo import Abaixar_um_arquivo
+
+Fuçãos:
+Abaixar_um_arquivo("Site do aquivo","Nome e tipo do arquivo")
---
+++ packages/Abaxador-de-arquivo/Abaxador-de-arquivo-2024.tar.gz/Abaxador-de-arquivo-2024/src/Abaxador-de-arquivo/Abaxador_de_arquivo.py
@@ -0,0 +1,9 @@
+﻿import requests
+
+def Abaixar_um_arquivo(Site_do_arquivo, Nome_e_tipo_do_arquivo):
+    Resposta = requests.get(Site_do_arquivo)
+    if Resposta.status_code == requests.codes.OK:
+        Novo_arquivo = open(Nome_e_tipo_do_arquivo, "wb")
+        Novo_arquivo.write(Resposta.content)
+    else:
+        Resposta.raise_for_status()
