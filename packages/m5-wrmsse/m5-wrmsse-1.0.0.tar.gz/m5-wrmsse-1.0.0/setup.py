a64b699bfafc5d87e27a11d59e76e31185579881
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:20:47 +0000

    Add package m5-wrmsse/1.0.0/m5-wrmsse-1.0.0.tar.gz

---
+++ packages/m5-wrmsse/m5-wrmsse-1.0.0.tar.gz/m5-wrmsse-1.0.0/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+    
+
+with open('README.md', 'r') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+setup(
+    name = 'm5-wrmsse',
+    version = '1.0.0',
+    description = 'WRMSSE score for the M5 dataset',
+    long_description = readme,
+    long_description_content_type='text/markdown',
+    author = 'Paul Morgan',
+    author_email = '',
+    url = 'https://github.com/pmrgn/m5-wrmsse',
+    python_requires = '>=3.9',
+    install_requires = [
+        'numpy>=1.20',
+        'pandas>=1.2',
+    ],
+    license = license,
+    packages = find_packages(where='src'),
+    package_dir = {'':'src'},
+    package_data = {'m5_wrmsse': ['data/*.csv.gz']},
+)
