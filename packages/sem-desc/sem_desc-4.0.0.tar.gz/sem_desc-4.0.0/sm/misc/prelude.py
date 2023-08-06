e18b526bab6582d4827d5af40c7455b8dc88491b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:34 +0000

    Add package sem-desc/4.0.0/sem_desc-4.0.0.tar.gz

---
+++ packages/sem-desc/sem_desc-4.0.0.tar.gz/sem_desc-4.0.0/sm/misc/prelude.py
@@ -0,0 +1,11 @@
+from sm.misc.fn_cache import *
+from sm.misc.deser import *
+from sm.misc.funcs import *
+from sm.misc.exp_manager import *
+from sm.misc.logger import *
+from sm.misc.bijection import Bijection
+from sm.misc.matrix import Matrix
+
+
+class UnreachableError(Exception):
+    pass
