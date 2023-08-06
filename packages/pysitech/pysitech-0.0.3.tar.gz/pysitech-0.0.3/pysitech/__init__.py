7d9c456eabd9df08fdd910ad5044064fdec688df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:22:28 +0000

    Add package pysitech/0.0.3/pysitech-0.0.3.tar.gz

---
+++ packages/pysitech/pysitech-0.0.3.tar.gz/pysitech-0.0.3/pysitech/__init__.py
@@ -0,0 +1,1 @@
+from pysitech.Calc import calc
---
+++ packages/pysitech/pysitech-0.0.3.tar.gz/pysitech-0.0.3/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+VERSION = '0.0.3'
+DESCRIPTION = 'Dependecy Confusion POC'
+
+# Setting up
+setup(
+    name="pysitech",
+    version=VERSION,
+    author="Zeyad Abulaban",
+    author_email="zeyad@sitech.me",
+    description=DESCRIPTION,
+    packages=find_packages(),
+    install_requires=['requests'],
+    classifiers= [
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Education",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+        ]
+)
+
