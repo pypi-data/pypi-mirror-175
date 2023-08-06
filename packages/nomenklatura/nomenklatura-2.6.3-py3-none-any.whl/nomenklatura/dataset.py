e2c80bd928f6d2caf7565ca8b4e25f407b7d48db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:28:23 +0000

    Add package nomenklatura/2.6.3/nomenklatura-2.6.3-py3-none-any.whl

---
+++ packages/nomenklatura/nomenklatura-2.6.3-py3-none-any.whl/nomenklatura/dataset.py
@@ -0,0 +1,29 @@
+from typing import Any, Dict, TypeVar
+
+DS = TypeVar("DS", bound="Dataset")
+
+
+class Dataset(object):
+    """A unit of entities. A dataset is a set of data, sez W3C."""
+
+    def __init__(self, name: str, title: str) -> None:
+        self.name = name
+        self.title = title
+
+    def __eq__(self, other: Any) -> bool:
+        try:
+            return not not self.name == other.name
+        except AttributeError:
+            return False
+
+    def __lt__(self, other: "Dataset") -> bool:
+        return self.name.__lt__(other.name)
+
+    def __hash__(self) -> int:
+        return hash((self.__class__.__name__, self.name))
+
+    def __repr__(self) -> str:
+        return f"<{self.__class__.__name__}({self.name!r})>"
+
+
+DatasetIndex = Dict[str, Dataset]
