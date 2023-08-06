0a8a51a6e42034a3e02bf63009dadbfbd79a3f9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:22:15 +0000

    Add package Cezar-first-hello/0.1.0/Cezar-first-hello-0.1.0.tar.gz

---
+++ packages/Cezar-first-hello/Cezar-first-hello-0.1.0.tar.gz/Cezar-first-hello-0.1.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Cezar-first-hello',
+    version='0.1.0',
+    author='Horoiu Cezar',
+    author_email='horoiucezar@yahoo.com',
+    packages=['my_own_package'],
+    package_dir={'':'src\\'},
+    include_package_data=True
+
+)
