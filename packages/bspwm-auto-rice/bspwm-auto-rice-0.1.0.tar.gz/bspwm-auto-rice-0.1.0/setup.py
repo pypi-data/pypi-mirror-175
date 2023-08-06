38d0a7908bb9bec5f62ffeb3816d7d8c756da3e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:29:07 +0000

    Add package bspwm-auto-rice/0.1.0/bspwm-auto-rice-0.1.0.tar.gz

---
+++ packages/bspwm-auto-rice/bspwm-auto-rice-0.1.0.tar.gz/bspwm-auto-rice-0.1.0/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['bspwm_auto_rice']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['pywal>=3.3.0,<4.0.0', 'rich>=12.6.0,<13.0.0', 'typer>=0.7.0,<0.8.0']
+
+entry_points = \
+{'console_scripts': ['bspar = bspwm_auto_rice.bspar:cli']}
+
+setup_kwargs = {
+    'name': 'bspwm-auto-rice',
+    'version': '0.1.0',
+    'description': '',
+    'long_description': '',
+    'author': 'AbdelrhmanNile',
+    'author_email': 'abdelrhmannile@gmail.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.10,<4.0',
+}
+
+
+setup(**setup_kwargs)
