04e38075b7f6564f114189b7c0739087567e5ac2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:54:58 +0000

    Add package otmorris/0.13.post4/otmorris-0.13.post4-cp39-cp39-win_amd64.whl

---
+++ packages/otmorris/otmorris-0.13.post4-cp39-cp39-win_amd64.whl/otmorris/__init__.py
@@ -0,0 +1,19 @@
+"""
+    otmorris --- An OpenTURNS module
+    ================================
+
+    Contents
+    --------
+      'otmorris' is a module for OpenTURNS, which enables to compute the
+      elementary effects using the screening Morris method
+
+"""
+
+import sys
+if sys.platform.startswith('win'):
+    # this ensures OT dll is loaded
+    import openturns
+
+from .otmorris import *
+
+__version__ = '0.13.post4'
