bad2a6d2cea87bd5b535691c6527d256cb35602b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:21:26 +0000

    Add package Bino-gau/0.1/Bino_gau-0.1.tar.gz

---
+++ packages/Bino-gau/Bino_gau-0.1.tar.gz/Bino_gau-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='Bino_gau',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['Bino_Gau'],
+      author = "Mudit Painuli",
+      author_email = "muditpainuli@gmail.com",
+      zip_safe=False)
