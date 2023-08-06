3b96e1cc387dbaa7cfe80c4632838e8d721c8ed3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:46 +0000

    Add package reshadowcode/0.0.3/reshadowcode-0.0.3.tar.gz

---
+++ packages/reshadowcode/reshadowcode-0.0.3.tar.gz/reshadowcode-0.0.3/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with open(os.path.join(here, 'README.md')) as f:
+    long_description = f.read()
+
+setup(
+    name='reshadowcode',
+    version='0.0.3',
+    description='For Shadow Copy research in Windows',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/alicangnll/pyshadow',
+    author='Ali Can Gönüllü',
+    license='MIT',
+    keywords=['Windows', 'VSS', 'win32', 'Shadow', 'Copy'],
+    py_modules=['subprocess', 'pywin32', 're'],
+    install_requires=['pywin32'],
+)
