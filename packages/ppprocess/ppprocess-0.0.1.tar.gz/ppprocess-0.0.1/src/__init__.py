b7fe778e2103eecab81b984ff55ac24203cdefae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:06:08 +0000

    Add package ppprocess/0.0.1/ppprocess-0.0.1.tar.gz

---
+++ packages/ppprocess/ppprocess-0.0.1.tar.gz/ppprocess-0.0.1/src/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# @Organization  : TMT
+# @Author        : Cuong Tran
+# @Time          : 11/7/2022
+
+from .data import folder_dataset_split
+from .plot import plot_cm
+from .calc import calc_euclidean
