cbd8908c08bc75581ba243c642f9cc35d9fb0842
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:25 +0000

    Add package acdata/0.0.1/acdata-0.0.1.tar.gz

---
+++ packages/acdata/acdata-0.0.1.tar.gz/acdata-0.0.1/setup.py
@@ -0,0 +1,8 @@
+import setuptools
+
+setuptools.setup(
+	name = 'acdata',
+	version = '0.0.1',
+	author = 'Cole Hagen',
+	description = 'A python module to format a dataframe for plotting Andrews Curves',
+	packages=['acdata'])
