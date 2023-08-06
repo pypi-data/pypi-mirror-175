ab5d29b5a44d8cd2fc7ce7bb127bfb464abc5a22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:51:37 +0000

    Add package balconyapp/0.0.39/balconyapp-0.0.39.tar.gz

---
+++ packages/balconyapp/balconyapp-0.0.39.tar.gz/balconyapp-0.0.39/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['balconyapp', 'balconyapp.app']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'balconyapp',
+    'version': '0.0.39',
+    'description': 'Balcony App template package',
+    'long_description': '# balcony-app-template\nA template repository for creating new Balcony Apps.\n',
+    'author': 'Oguzhan Yilmaz',
+    'author_email': 'oguzhanylmz271@gmail.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
