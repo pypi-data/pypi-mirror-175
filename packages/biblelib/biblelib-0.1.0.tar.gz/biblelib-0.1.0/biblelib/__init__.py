bd6250bcaf61531b4c1b6442ad9b09255ab19f7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:01:02 +0000

    Add package biblelib/0.1.0/biblelib-0.1.0.tar.gz

---
+++ packages/biblelib/biblelib-0.1.0.tar.gz/biblelib-0.1.0/biblelib/__init__.py
@@ -0,0 +1,13 @@
+"""Biblelib provides utilities for working with Bible books, references, pericopes, and other units.
+
+More documentation here.
+"""
+
+from .books import Book, Books
+from .words import
+
+__all__ = [
+    # books
+    "Book",
+    "Books",
+]
