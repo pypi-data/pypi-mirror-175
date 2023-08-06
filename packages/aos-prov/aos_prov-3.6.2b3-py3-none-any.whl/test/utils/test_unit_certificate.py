78ee4e51f91084d1afb68edb6b2bc3c87305363c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:51:28 +0000

    Add package aos-prov/3.6.2b3/aos_prov-3.6.2b3-py3-none-any.whl

---
+++ packages/aos-prov/aos_prov-3.6.2b3-py3-none-any.whl/test/utils/test_unit_certificate.py
@@ -0,0 +1,21 @@
+import unittest
+
+from aos_prov.utils.unit_certificate import UnitCertificate
+
+
+class TestUnitCertificate(unittest.TestCase):
+    def test_object_can_be_created(self):
+        uc = UnitCertificate()
+        self.assertIsInstance(uc, UnitCertificate)
+
+    def test_attributes(self):
+        uc = UnitCertificate()
+
+        uc.cert_type = 'type1'
+        self.assertEqual(uc.cert_type, 'type1')
+
+        uc.certificate = 'some cert 1'
+        self.assertEqual(uc.certificate, 'some cert 1')
+
+        uc.csr = 'csr'
+        self.assertEqual(uc.csr, 'csr')
