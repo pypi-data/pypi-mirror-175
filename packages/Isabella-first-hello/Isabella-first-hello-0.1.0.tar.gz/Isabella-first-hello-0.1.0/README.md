3c26ebaf44517a7389bd7237ea4112004482cc23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:22:12 +0000

    Add package Isabella-first-hello/0.1.0/Isabella-first-hello-0.1.0.tar.gz

---
+++ packages/Isabella-first-hello/Isabella-first-hello-0.1.0.tar.gz/Isabella-first-hello-0.1.0/README.md
@@ -0,0 +1,3 @@
+# My First Package Isabella 
+
+This is my first package.
---
+++ packages/Isabella-first-hello/Isabella-first-hello-0.1.0.tar.gz/Isabella-first-hello-0.1.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Isabella-first-hello',
+    version='0.1.0',
+    author='Isabella Elena Daraban',
+    author_email='ysss_a@yahoo.com',
+    packages=['my_own_package'],
+    package_dir={'': 'src/'},
+    include_package_data=True
+)
+
