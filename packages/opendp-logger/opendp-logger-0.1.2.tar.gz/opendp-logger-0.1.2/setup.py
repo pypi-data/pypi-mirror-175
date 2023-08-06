d2b6c91bda37c304c19308e433f4cb575ad8f09e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:23:21 +0000

    Add package opendp-logger/0.1.2/opendp-logger-0.1.2.tar.gz

---
+++ packages/opendp-logger/opendp-logger-0.1.2.tar.gz/opendp-logger-0.1.2/setup.py
@@ -0,0 +1,29 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="opendp-logger",
+    version="0.1.2",
+    description="A logger wrapper for OpenDP to keep track of, import, export the AST",
+    long_description = "file: README.md",
+    long_description_content_type = "text/markdown",
+    url="https://github.com/opendp/opendp_logger",
+    author='Oblivious',
+    author_email='hello@oblivious.ai',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ], 
+    keywords='opendp logger ast',
+    packages=find_packages(),
+    python_requires=">=3.8, <4",
+    install_requires=[
+        "opendp >= 0.6.0",
+        "PyYAML >= 6.0"
+    ],
+    package_data={"oblv": ["py.typed"]},
+)
