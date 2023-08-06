0fddff927be460711aace6ba84be83025fd7bf9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:19:03 +0000

    Add package python-AsciiLib/0.0.1/python-AsciiLib-0.0.1.tar.gz

---
+++ packages/python-AsciiLib/python-AsciiLib-0.0.1.tar.gz/python-AsciiLib-0.0.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.1'
+DESCRIPTION = 'An ascii art package'
+LONG_DESCRIPTION = 'A package that makes it easy to convert images and videos to ascii art'
+
+setup(
+    name="python-AsciiLib",
+    version=VERSION,
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    author="Alexander J.",
+    author_email="alexandergrahambell707@gmail.com",
+    license='MIT',
+    packages=find_packages('C:\\Users\\alexa\\Documents\\VSCode\\AsciiLib Package\\AsciiLib'),
+    install_requires=['os', 'numpy', 'pathlib', 'PIL', 'cv2'],
+    keywords='ascii',
+    classifiers= [
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        'License :: OSI Approved :: MIT License',
+        "Programming Language :: Python :: 3",
+    ]
+)
