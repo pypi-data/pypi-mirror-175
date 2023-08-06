1a13d295b8b7fa2fc88b7e509648a1a04d7c476c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:21:59 +0000

    Add package chalkpy/1.8.7/chalkpy-1.8.7.tar.gz

---
+++ packages/chalkpy/chalkpy-1.8.7.tar.gz/chalkpy-1.8.7/tests/streams/test_stream.py
@@ -0,0 +1,31 @@
+from pydantic import BaseModel
+
+from chalk.features import Features, features
+from chalk.streams import KafkaSource, stream
+
+
+@features
+class StreamFeatures:
+    scalar_feature: str
+
+
+class KafkaMessage(BaseModel):
+    val_a: str
+
+
+s = KafkaSource(broker=[], topic=[], message_model=KafkaMessage)
+
+
+@stream(source=s)
+def fn(message: KafkaMessage) -> Features[StreamFeatures.scalar_feature]:
+    return StreamFeatures(
+        scalar_feature=message.val_a,
+    )
+
+
+def test_callable():
+    assert fn(KafkaMessage(val_a="hello")) == StreamFeatures(scalar_feature="hello")
+
+
+def test_parsed_source():
+    assert fn.source == s
