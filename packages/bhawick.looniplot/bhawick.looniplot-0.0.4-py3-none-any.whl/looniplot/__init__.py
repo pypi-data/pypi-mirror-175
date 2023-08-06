8d1d8a3a750e407f3d9756e7917a0471e0130fd1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:32:47 +0000

    Add package bhawick.looniplot/0.0.4/bhawick.looniplot-0.0.4-py3-none-any.whl

---
+++ packages/bhawick.looniplot/bhawick.looniplot-0.0.4-py3-none-any.whl/looniplot/__init__.py
@@ -0,0 +1,3 @@
+# Add a shortcut such that users of the library can write `from looniplot import plot`
+# instead of `from looniplot.looniplot import plot`.
+from looniplot.looniplot import plot, plot_to_string, histogram
