3e32c963cfce69bca61ec1680f1b260d63adf1fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:37 +0000

    Add package ms2ml/0.0.15/ms2ml-0.0.15.tar.gz

---
+++ packages/ms2ml/ms2ml-0.0.15.tar.gz/ms2ml-0.0.15/ms2ml/data/adapters/__init__.py
@@ -0,0 +1,13 @@
+"""Provides the data adapters for the ms2ml package.
+
+The data adapters are used to load the data from the different data sources
+into a common format. The data adapters use the parsers to load the
+data into the common series of objects.
+"""
+
+from .base import BaseAdapter
+from .msp import MSPAdapter
+from .mzml import MZMLAdapter
+from .spectronaut import SpectronautAdapter
+
+__all__ = ["BaseAdapter", "MSPAdapter", "SpectronautAdapter", "MZMLAdapter"]
