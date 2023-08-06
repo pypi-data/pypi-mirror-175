9a60ad417ecdd4e5e9e03cac0c2f5aa900bcc6df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:00:58 +0000

    Add package phyco/0.0/phyco-0.0.tar.gz

---
+++ packages/phyco/phyco-0.0.tar.gz/phyco-0.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='phyco',
+    version='0.0',
+    description='',
+    long_description='',
+    url='',
+    author='LyQuid',
+    author_email='lyquidpersonal@gmail.com',
+    packages=[],
+    classifiers=['Development Status :: 1 - Planning'],
+)
