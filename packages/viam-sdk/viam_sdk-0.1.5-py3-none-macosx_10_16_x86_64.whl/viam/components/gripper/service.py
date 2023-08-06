53d30208ed0421c03d50d34c5b10673c4641669b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:29:12 +0000

    Add package viam-sdk/0.1.5/viam_sdk-0.1.5-py3-none-macosx_10_16_x86_64.whl

---
+++ packages/viam-sdk/viam_sdk-0.1.5-py3-none-macosx_10_16_x86_64.whl/viam/components/gripper/service.py
@@ -0,0 +1,60 @@
+from grpclib.server import Stream
+
+from viam.components.service_base import ComponentServiceBase
+from viam.errors import ComponentNotFoundError
+from viam.proto.component.gripper import (
+    GrabRequest,
+    GrabResponse,
+    GripperServiceBase,
+    OpenRequest,
+    OpenResponse,
+    StopRequest,
+    StopResponse,
+)
+
+from .gripper import Gripper
+
+
+class GripperService(GripperServiceBase, ComponentServiceBase[Gripper]):
+    """
+    gRPC Service for a Gripper
+    """
+
+    RESOURCE_TYPE = Gripper
+
+    async def Open(self, stream: Stream[OpenRequest, OpenResponse]) -> None:
+        request = await stream.recv_message()
+        assert request is not None
+        name = request.name
+        try:
+            gripper = self.get_component(name)
+        except ComponentNotFoundError as e:
+            raise e.grpc_error
+        timeout = stream.deadline.time_remaining() if stream.deadline else None
+        await gripper.open(timeout=timeout)
+        response = OpenResponse()
+        await stream.send_message(response)
+
+    async def Grab(self, stream: Stream[GrabRequest, GrabResponse]) -> None:
+        request = await stream.recv_message()
+        assert request is not None
+        name = request.name
+        try:
+            gripper = self.get_component(name)
+        except ComponentNotFoundError as e:
+            raise e.grpc_error
+        timeout = stream.deadline.time_remaining() if stream.deadline else None
+        grabbed = await gripper.grab(timeout=timeout)
+        response = GrabResponse(success=grabbed)
+        await stream.send_message(response)
+
+    async def Stop(self, stream: Stream[StopRequest, StopResponse]) -> None:
+        request = await stream.recv_message()
+        assert request is not None
+        try:
+            gripper = self.get_component(request.name)
+        except ComponentNotFoundError as e:
+            raise e.grpc_error
+        timeout = stream.deadline.time_remaining() if stream.deadline else None
+        await gripper.stop(timeout=timeout)
+        await stream.send_message(StopResponse())
