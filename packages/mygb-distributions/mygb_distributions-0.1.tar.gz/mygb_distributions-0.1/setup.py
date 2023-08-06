4c76d6a878edf98824698c32169cdf93cee1fbaa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:02:37 +0000

    Add package mygb-distributions/0.1/mygb_distributions-0.1.tar.gz

---
+++ packages/mygb-distributions/mygb_distributions-0.1.tar.gz/mygb_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='mygb_distributions',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['mygb_distributions'],
+      author = 'Claudine Hernandez',
+      author_email = 'ch@email.com',
+      zip_safe = False)
