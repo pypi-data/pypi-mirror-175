89a1a3b9e15776c1ce78795c19c3bb42e48b596b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:19:08 +0000

    Add package scanoss/1.2.0/scanoss-1.2.0.tar.gz

---
+++ packages/scanoss/scanoss-1.2.0.tar.gz/scanoss-1.2.0/setup.py
@@ -0,0 +1,39 @@
+import codecs
+import os
+from setuptools import setup
+
+
+def read(rel_path):
+    here = os.path.abspath(os.path.dirname(__file__))
+    with codecs.open(os.path.join(here, rel_path), 'r') as fp:
+        return fp.read()
+
+
+def get_version(rel_path):
+    for line in read(rel_path).splitlines():
+        if line.startswith('__version__'):
+            delim = '"' if '"' in line else "'"
+            return line.split(delim)[1]
+    else:
+        raise RuntimeError("Unable to find version string.")
+
+
+setup(
+    name="scanoss",
+    version=get_version("src/scanoss/__init__.py"),
+    author="SCANOSS",
+    author_email="info@scanoss.com",
+    license='MIT',
+    description='Simple Python library to use the SCANOSS APIs.',
+    long_description=read("PACKAGE.md"),
+    long_description_content_type='text/markdown',
+    install_requires=["requests", "crc32c>=2.2", "binaryornot", "progress", "grpcio<=1.42.0", "protobuf>=3.16.0,<=3.19.1"],
+    include_package_data=True,
+    package_data={'': ['data/*.json', 'data/*.txt']},
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent"
+    ],
+    python_requires='>=3.7'
+)
