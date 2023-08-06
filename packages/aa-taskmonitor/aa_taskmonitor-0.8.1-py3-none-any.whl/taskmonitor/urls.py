1cabbc991ee3d1ce7784a6318134c50949eef054
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:51:34 +0000

    Add package aa-taskmonitor/0.8.1/aa_taskmonitor-0.8.1-py3-none-any.whl

---
+++ packages/aa-taskmonitor/aa_taskmonitor-0.8.1-py3-none-any.whl/taskmonitor/urls.py
@@ -0,0 +1,33 @@
+from django.urls import path
+
+from . import views
+
+app_name = "taskmonitor"
+
+urlpatterns = [
+    path(
+        "admin_taskmonitor_download_csv",
+        views.admin_taskmonitor_download_csv,
+        name="admin_taskmonitor_download_csv",
+    ),
+    path(
+        "admin_taskmonitor_reports",
+        views.admin_taskmonitor_reports,
+        name="admin_taskmonitor_reports",
+    ),
+    path(
+        "admin_taskmonitor_reports_clear_cache",
+        views.admin_taskmonitor_reports_clear_cache,
+        name="admin_taskmonitor_reports_clear_cache",
+    ),
+    path(
+        "admin_taskmonitor_reports_recalculation",
+        views.admin_taskmonitor_reports_recalculation,
+        name="admin_taskmonitor_reports_recalculation",
+    ),
+    path(
+        "admin_taskmonitor_report_data/<str:report_name>",
+        views.admin_taskmonitor_report_data,
+        name="admin_taskmonitor_report_data",
+    ),
+]
