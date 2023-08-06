04a97746008cc1f17b0397e9a55275505ce51d34
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:14:47 +0000

    Add package bclib/3.19.5/bclib-3.19.5-py3-none-any.whl

---
+++ packages/bclib/bclib-3.19.5-py3-none-any.whl/bclib/context/web_context.py
@@ -0,0 +1,20 @@
+from typing import Any, TYPE_CHECKING
+
+from bclib.utility import HttpMimeTypes
+from bclib.listener.http_listener import HttpBaseDataName, HttpBaseDataType
+from ..context.request_context import RequestContext
+
+if TYPE_CHECKING:
+    from .. import dispatcher
+
+
+class WebContext(RequestContext):
+    def __init__(self, cms_object: dict,  dispatcher: 'dispatcher.IDispatcher') -> None:
+        super().__init__(cms_object, dispatcher)
+        self.process_async = True
+        self.mime = HttpMimeTypes.HTML
+
+    def generate_response(self, result: Any) -> dict:
+        ret_val = super().generate_response(result)
+        ret_val[HttpBaseDataType.CMS][HttpBaseDataName.CONTENT] = result
+        return ret_val
