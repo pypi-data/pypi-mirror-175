9ea7d10b260a980db9f428256b8abcbe36e6862f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:05:27 +0000

    Add package ori-optimize/0.2.0/ori_optimize-0.2.0.tar.gz

---
+++ packages/ori-optimize/ori_optimize-0.2.0.tar.gz/ori_optimize-0.2.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="ori_optimize",
+    version="0.2.0",
+    description="a environment that controls STL model rotate to reach the maximal 3D printing quality.",
+    install_requires=["gym", "numpy"],
+    packages=find_packages()
+)
