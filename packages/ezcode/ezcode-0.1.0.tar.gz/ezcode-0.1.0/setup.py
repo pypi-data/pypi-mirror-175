d8a759becac1b7c20cc6f0796b93d939ff161c66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:13:58 +0000

    Add package ezcode/0.1.0/ezcode-0.1.0.tar.gz

---
+++ packages/ezcode/ezcode-0.1.0.tar.gz/ezcode-0.1.0/setup.py
@@ -0,0 +1,27 @@
+from pathlib import Path
+from setuptools import setup, find_packages
+
+
+readme = "readme.md"
+
+setup(
+    name="ezcode",
+    version="0.1.0",
+    author="Zheng Gao",
+    author_email="mail.zheng.gao@gmail.com",
+    description="Easy Algorithm & Data Structure",
+    url="https://github.com/zheng-gao/ez_code",
+    project_urls={"Bug Tracker": "https://github.com/zheng-gao/ez_code/issues"},
+    long_description=(Path(__file__).parent/readme).read_text(encoding="utf-8"),
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],                                            # Information to filter the project on PyPi website
+    python_requires=">=3.7",                      # Minimum version requirement of the package
+    py_modules=["ezcode"],                        # Name of the python package
+    package_dir={"": "src"},                      # Directory of the source code of the package
+    packages=find_packages(where="src"),          # List of all python modules to be installed
+    install_requires=[]                           # Install other dependencies if any
+)
