292dc8b20adfa99859ec0f16c01f9568b6b8c5c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:15:48 +0000

    Add package calc-heakl-package/0.1/calc_heakl_package-0.1.tar.gz

---
+++ packages/calc-heakl-package/calc_heakl_package-0.1.tar.gz/calc_heakl_package-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="calc_heakl_package",
+    version="0.1",
+    author="Ahmed",
+    author_email="ahned.heakl@ejust.edu.eg",
+    description="Simple calculator app",
+    packages=find_packages("py_calc"),
+    python_requires=">=3.6",
+)
