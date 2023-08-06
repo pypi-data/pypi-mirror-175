8579c1c5b35005fe19393a81323093239e70cc4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:19:41 +0000

    Add package weldx/0.6.2/weldx-0.6.2-py3-none-any.whl

---
+++ packages/weldx/weldx-0.6.2-py3-none-any.whl/weldx/tags/core/__init__.py
@@ -0,0 +1,12 @@
+from . import (
+    common_types,
+    data_array,
+    dataset,
+    file,
+    generic_series,
+    geometry,
+    mathematical_expression,
+    time_series,
+    transformations,
+)
+from .graph import DiEdge, DiEdgeConverter, DiGraphConverter, DiNode
---
+++ packages/weldx/weldx-0.6.2-py3-none-any.whl/weldx/tags/core/geometry/spatial_data.py
@@ -0,0 +1,33 @@
+from copy import copy
+
+from weldx.asdf.types import WeldxConverter
+from weldx.asdf.util import get_highest_tag_version
+from weldx.geometry import SpatialData
+
+__all__ = ["SpatialDataConverter"]
+
+
+class SpatialDataConverter(WeldxConverter):
+    """Converter for SpatialData."""
+
+    tags = ["asdf://weldx.bam.de/weldx/tags/core/geometry/spatial_data-0.1.*"]
+    types = [SpatialData]
+
+    def to_yaml_tree(self, obj: SpatialData, tag: str, ctx) -> dict:
+        """Serialize into tree."""
+        tree = copy(obj.__dict__)  # shallow copy so we dont change original object
+        if tree["coordinates"].ndim <= 2:
+            tree["coordinates"] = tree["coordinates"].data
+        return tree
+
+    def from_yaml_tree(self, node: dict, tag: str, ctx) -> SpatialData:
+        """Reconstruct from yaml node."""
+        from weldx.constants import Q_
+
+        if tag == "asdf://weldx.bam.de/weldx/tags/core/geometry/spatial_data-0.1.0":
+            node["coordinates"] = Q_(node["coordinates"], "mm")  # legacy
+        return SpatialData(**node)
+
+    def select_tag(self, obj, tags, ctx) -> str:
+        """Determine the output tag for serialization."""
+        return get_highest_tag_version(self.tags)
