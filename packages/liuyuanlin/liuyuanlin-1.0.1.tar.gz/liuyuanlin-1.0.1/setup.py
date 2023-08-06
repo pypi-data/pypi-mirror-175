4f601604ae5cfeca753d828ee377bbf923b27d4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:19:41 +0000

    Add package liuyuanlin/1.0.1/liuyuanlin-1.0.1.tar.gz

---
+++ packages/liuyuanlin/liuyuanlin-1.0.1.tar.gz/liuyuanlin-1.0.1/setup.py
@@ -0,0 +1,30 @@
+from time import time
+import setuptools
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+setuptools.setup(
+    name="liuyuanlin",
+    version="1.0.1",
+    author="刘沅林",
+    author_email="liuyuanlins@outlook.com",
+    description="A test package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/MisakaMikoto128/liuyuanlin",
+    project_urls={
+        "Bug Tracker": "https://github.com/MisakaMikoto128/liuyuanlin/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    entry_points={
+        'console_scripts': [
+            'liuyuanlin = liuyuanlin:liuyuanlin',
+        ]
+    },
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+)
