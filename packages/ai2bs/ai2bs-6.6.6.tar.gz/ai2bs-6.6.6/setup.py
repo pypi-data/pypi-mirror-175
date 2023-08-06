b861f5a1abb7f309cb484e4ac79ee978a2c2496b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:30:07 +0000

    Add package ai2bs/6.6.6/ai2bs-6.6.6.tar.gz

---
+++ packages/ai2bs/ai2bs-6.6.6.tar.gz/ai2bs-6.6.6/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+VERSION = '6.6.6'
+DESCRIPTION = 'None of your business'
+
+# Setting up
+setup(
+    name="ai2bs",
+    version=VERSION,
+    author="locopolo",
+    author_email="",
+    description=DESCRIPTION,
+    packages=find_packages(),
+    install_requires=[],
+    keywords=[],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
