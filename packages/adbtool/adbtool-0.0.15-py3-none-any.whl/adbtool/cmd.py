19576166278f085a1f858f32418a33f722b7b089
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:58 +0000

    Add package adbtool/0.0.15/adbtool-0.0.15-py3-none-any.whl

---
+++ packages/adbtool/adbtool-0.0.15-py3-none-any.whl/adbtool/cmd.py
@@ -0,0 +1,142 @@
+import os
+import shlex
+import subprocess
+import sys
+
+from semantic_version import Version
+
+from .errors import raise_error
+
+
+# return (output, isOk)
+def call(cmd: str, printOutput: bool = False) -> tuple[str, bool]:
+    # print(f"{printOutput = }, {cmd = }")
+    if sys.platform == "win32":
+        args = cmd
+    else:
+        # linux must split arguments
+        args = shlex.split(cmd)
+    try:
+        if printOutput:
+            isOk = subprocess.call(args) == 0
+            return "", isOk
+
+        data = subprocess.check_output(args)
+        # python3 output is bytes
+        output = data.decode("utf-8")
+        return output, True
+    except subprocess.CalledProcessError as callerr:
+        print(f"cmd = {cmd}, callerr.output = {callerr.output}", file=sys.stderr)
+        return (callerr.output, False)
+    except IOError as ioerr:
+        print(f"cmd = {cmd}, ioerr = {ioerr}", file=sys.stderr)
+        return "", False
+
+
+def getAdb() -> str:
+    androidHome = os.getenv("ANDROID_HOME")
+    if androidHome is None:
+        androidHome = os.getenv("ANDROID_SDK")
+    if androidHome is None:
+        androidHome = os.getenv("ANDROID_SDK_ROOT")
+    if androidHome is None:
+        # print('can not found ANDROID_HOME/ANDROID_SDK in environment value')
+        return "adb"
+
+    return os.path.join(androidHome, "platform-tools/adb")
+
+
+def getAapt() -> str:
+    androidHome = os.getenv("ANDROID_HOME")
+    if androidHome is None:
+        androidHome = os.getenv("ANDROID_SDK")
+    if androidHome is None:
+        androidHome = os.getenv("ANDROID_SDK_ROOT")
+    if androidHome is None:
+        print("can not found ANDROID_HOME/ANDROID_SDK in environment value")
+        return "aapt"
+
+    aaptname = "aapt.exe" if sys.platform == "win32" else "aapt"
+
+    buildtools = os.path.join(androidHome, "build-tools")
+    if os.path.isdir(buildtools):
+        vers = list(map(lambda v: Version(v), os.listdir(buildtools)))
+        vers.sort(reverse=True)
+        for ver in vers:
+            filename = os.path.join(buildtools, str(ver), aaptname)
+            if os.path.isfile(filename):
+                return filename
+
+    raise_error("can not found aapt in ANDROID_HOME/ANDROID_SDK")
+    return ""
+
+def getZipalign() -> str:
+    androidHome = os.getenv("ANDROID_HOME")
+    if androidHome is None:
+        androidHome = os.getenv("ANDROID_SDK")
+    if androidHome is None:
+        androidHome = os.getenv("ANDROID_SDK_ROOT")
+    if androidHome is None:
+        print("can not found ANDROID_HOME/ANDROID_SDK in environment value")
+        return "zipalign"
+
+    # aaptname = "apksigner.bat" if sys.platform == "win32" else "apksigner.bat"
+    aaptname = "zipalign.exe"
+
+    buildtools = os.path.join(androidHome, "build-tools")
+    if os.path.isdir(buildtools):
+        vers = list(map(lambda v: Version(v), os.listdir(buildtools)))
+        vers.sort(reverse=True)
+        for ver in vers:
+            filename = os.path.join(buildtools, str(ver), aaptname)
+            if os.path.isfile(filename):
+                return filename
+
+    raise_error("can not found aapt in ANDROID_HOME/ANDROID_SDK")
+    return ""
+
+def getApksigner() -> str:
+    androidHome = os.getenv("ANDROID_HOME")
+    if androidHome is None:
+        androidHome = os.getenv("ANDROID_SDK")
+    if androidHome is None:
+        androidHome = os.getenv("ANDROID_SDK_ROOT")
+    if androidHome is None:
+        print("can not found ANDROID_HOME/ANDROID_SDK in environment value")
+        return "apksigner"
+
+    # aaptname = "apksigner.bat" if sys.platform == "win32" else "apksigner.bat"
+    aaptname = "apksigner.bat"
+
+    buildtools = os.path.join(androidHome, "build-tools")
+    if os.path.isdir(buildtools):
+        vers = list(map(lambda v: Version(v), os.listdir(buildtools)))
+        vers.sort(reverse=True)
+        for ver in vers:
+            filename = os.path.join(buildtools, str(ver), aaptname)
+            if os.path.isfile(filename):
+                return filename
+
+    raise_error("can not found aapt in ANDROID_HOME/ANDROID_SDK")
+    return ""
+
+def get_unity_editor_dir(editor_dir):
+    def is_editor_dir(dir):
+        if dir is None:
+            return False
+        if os.path.isdir(dir):
+            if os.path.isfile(os.path.join(dir, 'Unity.exe')):
+                return True
+        return False
+    if is_editor_dir(editor_dir):
+        return editor_dir
+    editor_dir = os.getenv("UNITY_EDITOR_ROOT")
+    if is_editor_dir(editor_dir):
+        return editor_dir
+    raise_error('can not found unity editor')
+
+def get_unity_binary2text(unity_editor_dir):
+    return os.path.join(get_unity_editor_dir(unity_editor_dir), 'Data/Tools/binary2text.exe')
+
+def get_unity_webextract(unity_editor_dir):
+    return os.path.join(get_unity_editor_dir(unity_editor_dir), 'Data/Tools/WebExtract.exe')
---
+++ packages/adbtool/adbtool-0.0.15-py3-none-any.whl/adbtool/subcommands/assetbundleinfo.py
@@ -0,0 +1,85 @@
+import argparse
+from genericpath import isdir, isfile
+import os
+from posixpath import abspath
+import re
+import tempfile
+import shutil
+from litefeel.pycommon.io import makedirs
+
+from ..cmd import call, get_unity_binary2text, get_unity_webextract
+from ..config import Config
+from ..errors import raise_error
+from . import adbdevice
+
+INFO_EXT = ".txt"
+
+
+def bine2text(file, unity_editor_dir):
+    binary2text = get_unity_binary2text(unity_editor_dir)
+    cmd = f'"{binary2text}" "{file}"'
+    output, isOk = call(cmd)
+    if isOk:
+        return file +".txt"
+
+def extract(file, unity_editor_dir):
+    webextract = get_unity_webextract(unity_editor_dir)
+    cmd = f'"{webextract}" "{file}"'
+    output, isOk = call(cmd)
+    if isOk:
+        for root, dirs, files in os.walk(file+"_data"):
+            for f in files:
+                if not f.endswith('.resS'):
+                    return os.path.join(root, f)
+                    
+def do_file(input_file, output_file, unity_editor_dir):
+    if not os.path.isfile(input_file):
+        raise_error(f'file not fond:{input_file}')
+
+    with tempfile.TemporaryDirectory() as tmpdirname:
+        tmpunityfile = os.path.join(tmpdirname, 'tmp.unity3d')
+        shutil.copy(input_file, tmpunityfile)
+        outfile = extract(tmpunityfile, unity_editor_dir)
+        assert outfile
+        outfile = bine2text(outfile, unity_editor_dir)
+        assert outfile
+        makedirs(output_file, True)
+        shutil.copy(outfile, output_file)
+
+def do_folder(input_dir:str, output_dir:str, ext:str, unity_editor_dir:str) -> None:
+    for root, dir, files in os.walk(input_dir):
+        for f in files:
+            if ext and not f.endswith(ext):
+                continue
+            input_file = os.path.join(root, f)
+            rel_name = os.path.relpath(input_file, input_dir)
+            output_file = os.path.join(output_dir, rel_name + INFO_EXT)
+            do_file(input_file, output_file, unity_editor_dir)
+
+
+def docommand(args: argparse.Namespace, cfg: Config) -> None:
+    output = args.output
+    abpath = args.abpath
+    unity_editor_dir = args.unityeditordir
+    if os.path.isfile(abpath):
+        if output is None:
+            output = abpath + INFO_EXT
+        elif os.path.isdir(output):
+            output = os.path.join(output, os.path.basename(abpath) + INFO_EXT)
+        do_file(abpath, output, unity_editor_dir)
+    elif os.path.isdir(abpath):
+        if output is None:
+            output = abpath
+        elif os.path.isfile(output):
+            raise_error("output cannot be file when abpath is folder")
+        do_folder(abpath, output, args.ext, unity_editor_dir)
+    else:
+        raise_error(f"abpath is not exits:{abpath}")
+
+
+def addcommand(parser: argparse.ArgumentParser) -> None:
+    parser.add_argument("-o", "--output", help="output file or folder")
+    parser.add_argument("-e", "--ext", nargs='?', const='', default=".unity3d", help="assetbundle extions name")
+    parser.add_argument('-u', '--unityeditordir',nargs='?', help='unity editor folder')
+    parser.add_argument("abpath", help="assetbundle file or folder")
+    adbdevice.addArgumentParser(parser)
