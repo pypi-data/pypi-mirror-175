b1bc16b6561ee0699d8a55c6652b0a54ba4b9db1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:23:23 +0000

    Add package lmlib/2.1.1/lmlib-2.1.1.tar.gz

---
+++ packages/lmlib/lmlib-2.1.1.tar.gz/lmlib-2.1.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='lmLib',
+    version='0.0.0',
+    description='Model Based Signal Processing Library',
+    long_description='',
+    url='https://www.lmlib.ch',
+    author='Reto Wildhaber, Frédéric Waldmann',
+    author_email='frederic.waldmann@gmail.com',
+    packages=[],
+    classifiers=['Development Status :: 1 - Planning']
+)
