a47ac6322a2691e2d2ee120eac63f95102fffff0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:21:59 +0000

    Add package personal-logger/0.1.2/personal-logger-0.1.2.tar.gz

---
+++ packages/personal-logger/personal-logger-0.1.2.tar.gz/personal-logger-0.1.2/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['personal_logger']
+
+package_data = \
+{'': ['*']}
+
+entry_points = \
+{'console_scripts': ['plog = personal_logger.cli:main']}
+
+setup_kwargs = {
+    'name': 'personal-logger',
+    'version': '0.1.2',
+    'description': '',
+    'long_description': '',
+    'author': 'Teddy Xinyuan Chen',
+    'author_email': '45612704+tddschn@users.noreply.github.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'https://github.com/tddschn/personal-logger',
+    'packages': packages,
+    'package_data': package_data,
+    'entry_points': entry_points,
+    'python_requires': '>=3.10,<4.0',
+}
+
+
+setup(**setup_kwargs)
