82ef62572dc9bf2aed4e6462aea800e4c34fa4f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:24:38 +0000

    Add package distkv-knx/0.11.2/distkv_knx-0.11.2.tar.gz

---
+++ packages/distkv-knx/distkv_knx-0.11.2.tar.gz/distkv_knx-0.11.2/tests/conftest.py
@@ -0,0 +1,5 @@
+import logging
+
+from pytest_trio.enable_trio_mode import *  # noqa: F403,F401,E501 pylint:disable=wildcard-import,unused-wildcard-import
+
+logging.basicConfig(level=logging.DEBUG)
