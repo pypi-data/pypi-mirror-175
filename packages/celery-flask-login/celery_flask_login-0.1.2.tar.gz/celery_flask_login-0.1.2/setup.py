0c48783152f50b170c3f3c68084df7fb2f8f9726
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:18:42 +0000

    Add package celery-flask-login/0.1.2/celery_flask_login-0.1.2.tar.gz

---
+++ packages/celery-flask-login/celery_flask_login-0.1.2.tar.gz/celery_flask_login-0.1.2/setup.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['celery_flask_login']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['Flask-Login>=0.6.2,<0.7.0',
+ 'Flask>=2.2.2,<3.0.0',
+ 'celery>=5.2.7,<6.0.0',
+ 'pytest>=7.2.0,<8.0.0']
+
+setup_kwargs = {
+    'name': 'celery-flask-login',
+    'version': '0.1.2',
+    'description': '',
+    'long_description': 'None',
+    'author': 'David Sternlicht',
+    'author_email': 'd1618033@gmail.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
