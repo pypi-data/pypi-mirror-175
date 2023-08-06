6607fb59de80801020e690c484ebc4436f6712b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:58:57 +0000

    Add package IpfsML/1.0.2/IpfsML-1.0.2.tar.gz

---
+++ packages/IpfsML/IpfsML-1.0.2.tar.gz/IpfsML-1.0.2/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+requirements = ['requests']
+setup(
+    name='IpfsML',
+    version='1.0.2',
+    packages=find_packages(exclude=['tests*']),
+    license='GNU General Public License v3.0',
+    description='A python wrapper for the nft-storage',
+    long_description=open('README.md').read(),
+    keywords=['nft-storage', 'ipfs SDK'],
+    install_requires=requirements,
+    url='https://github.com/kbm9696/IpfsML',
+    author='Balamurugan',
+    author_email='kbala007.1996@gmail.com',
+    long_description_content_type='text/markdown'
+)
