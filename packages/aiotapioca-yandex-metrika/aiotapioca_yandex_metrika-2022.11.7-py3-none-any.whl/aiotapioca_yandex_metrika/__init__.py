43cb553de6d72b147d274fdc57e83f435ef2f5a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:05:16 +0000

    Add package aiotapioca-yandex-metrika/2022.11.7/aiotapioca_yandex_metrika-2022.11.7-py3-none-any.whl

---
+++ packages/aiotapioca-yandex-metrika/aiotapioca_yandex_metrika-2022.11.7-py3-none-any.whl/aiotapioca_yandex_metrika/__init__.py
@@ -0,0 +1,12 @@
+from .aiotapioca_yandex_metrika import (
+    YandexMetrikaLogsAPI,
+    YandexMetrikaManagementAPI,
+    YandexMetrikaReportsAPI,
+)
+
+
+__all__ = (
+    "YandexMetrikaLogsAPI",
+    "YandexMetrikaManagementAPI",
+    "YandexMetrikaReportsAPI",
+)
