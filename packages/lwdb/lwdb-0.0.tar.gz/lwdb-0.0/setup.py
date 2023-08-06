73629325ed3ca544c4c73ff6fa4fe18297899d2c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:28:14 +0000

    Add package lwdb/0.0/lwdb-0.0.tar.gz

---
+++ packages/lwdb/lwdb-0.0.tar.gz/lwdb-0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(name='lwdb',
+      version='0.0',
+      description='LWDB',
+      author='Rick Wierenga',
+      author_email='rick_wierenga@icloud.com',
+      url='https://www.github.com/rickwierenga/lwdb/',
+      packages=['lwdb'],
+)
+
