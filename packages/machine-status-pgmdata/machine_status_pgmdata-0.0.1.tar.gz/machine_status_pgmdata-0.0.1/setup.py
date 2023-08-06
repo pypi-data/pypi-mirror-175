d6c7d4b645e9cac84dfd089885a3348843b3f602
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:21:13 +0000

    Add package machine-status-pgmdata/0.0.1/machine_status_pgmdata-0.0.1.tar.gz

---
+++ packages/machine-status-pgmdata/machine_status_pgmdata-0.0.1.tar.gz/machine_status_pgmdata-0.0.1/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.1' 
+DESCRIPTION = 'My first Python package'
+LONG_DESCRIPTION = 'My first Python package with a slightly longer description'
+
+# Setting up
+setup(
+       # the name must match the folder name 'verysimplemodule'
+        name="machine_status_pgmdata", 
+        version=VERSION,
+        author="Supriya",
+        author_email="<supriyasuhas058@email.com>",
+        description=DESCRIPTION,
+        long_description=LONG_DESCRIPTION,
+        packages=find_packages(),
+
+       install_requires=['pandas','numpy','matplotlib','paho-mqtt','numpy','pandas','scipy','scikit-learn','matplotlib'
+
+                        ],
+
+
+        # add any additional packages that 
+        # needs to be installed along with your package. Eg: 'caer'
+        
+        keywords=['python', 'first package'],
+        )
