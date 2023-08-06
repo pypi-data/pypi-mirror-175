c31936dcb7dfdacad5650f231154c341f6d9a046
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:52:58 +0000

    Add package polygon-data/0.1.0/polygon-data-0.1.0.tar.gz

---
+++ packages/polygon-data/polygon-data-0.1.0.tar.gz/polygon-data-0.1.0/setup.py
@@ -0,0 +1,40 @@
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+
+# To use a consistent encoding
+from codecs import open
+from os import path
+
+# The directory containing this file
+HERE = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(HERE, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+# This call to setup() does all the work
+setup(
+    name="polygon-data",
+    version="0.1.0",
+    description="Demo library",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://sarthakgagdani.ml",
+    author="Sarthak Gagdani",
+    author_email="sarthak.gagdani@nyu.edu",
+    license="MIT",
+    classifiers=[
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Operating System :: OS Independent"
+    ],
+    packages=["polygon_data"],
+    include_package_data=True,
+    install_requires=["polygon-api-client", "sqlalchemy", "time", "datetime", "math"]
+)
