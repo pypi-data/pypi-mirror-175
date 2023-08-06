7102be7b3ddc2cffd8ea52a739ef577da7e41751
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:19:29 +0000

    Add package ct-module/0.3/ct-module-0.3.tar.gz

---
+++ packages/ct-module/ct-module-0.3.tar.gz/ct-module-0.3/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup 
+setup(name="ct-module",
+version="0.3",
+description="This a package for creatring a list of iam user from an environmet",
+long_description="",
+author="Kuldip Sahdeo",
+packages=['ct-module'],
+install_requires=['boto3','datetime','pandas']
+)
