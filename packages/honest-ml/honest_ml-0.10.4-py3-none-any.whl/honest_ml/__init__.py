33f1a764760edf5e9b17ce3ac195e1ecef897ad9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:17 +0000

    Add package honest-ml/0.10.4/honest_ml-0.10.4-py3-none-any.whl

---
+++ packages/honest-ml/honest_ml-0.10.4-py3-none-any.whl/honest_ml/__init__.py
@@ -0,0 +1,6 @@
+__version__ = '0.10.4'
+
+from .trainer import trainer
+
+
+__all__ = ["trainer"]
---
+++ packages/honest-ml/honest_ml-0.10.4-py3-none-any.whl/honest_ml/trainer/__init__.py
@@ -0,0 +1,17 @@
+from .trainer import (
+    EvaluateModel, BaseTrainer,
+    RegressionTrainer, ClassificationTrainer,
+    GradientBoostingRegressor,
+    EvaluateModelAndHyperParameters,
+    GeneticAlgorithm
+)
+
+__all__ = [
+    "EvaluateModel",
+    "BaseTrainer",
+    "RegressionTrainer",
+    "ClassificationTrainer",
+    "GradientBoostingRegressor",
+    "EvaluateModelAndHyperParameters",
+    "GeneticAlgorithm"
+]
