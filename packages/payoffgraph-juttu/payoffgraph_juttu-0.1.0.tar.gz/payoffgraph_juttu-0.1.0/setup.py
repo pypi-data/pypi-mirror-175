75b8fff02d70e2620a79c9ce5783fdad38050b8b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:13 +0000

    Add package payoffgraph-juttu/0.1.0/payoffgraph_juttu-0.1.0.tar.gz

---
+++ packages/payoffgraph-juttu/payoffgraph_juttu-0.1.0.tar.gz/payoffgraph_juttu-0.1.0/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='payoffgraph_juttu',
+    version='0.1.0',
+    description='Get the most beautiful Payoff Chart using a single simple function',
+    author= 'Juttu G Anurag',
+    url = 'https://github.com/Juttu/ss',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    keywords=['payoff graph', 'options','opstra','sensibull'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    py_modules=['payoffgraph_juttu'],
+    package_dir={'':'src'},
+    install_requires = [
+        'numpy',
+        'pandas',
+        'streamlit',
+        'plotly',
+        'typing >= 3.7.4'
+
+    ]
+)
